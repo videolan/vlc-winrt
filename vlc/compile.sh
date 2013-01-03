505ea6ab7ec8f0fa3f8102c258ab482ed456d0b0
Jean-Baptiste Kempf <jb@videolan.org>
2013-01-03 13:15:35 +0100

    Compilation script for VLC

---
+++ vlc/compile.sh
@@ -0,0 +1,74 @@
+#! /bin/sh
+
+# 1/ libvlc, libvlccore and its plugins
+TESTED_HASH=aaf991f
+if [ ! -d "vlc" ]; then
+    echo "VLC source not found, cloning"
+    git clone git://git.videolan.org/vlc.git vlc
+    cd vlc
+    #git am -3 ../patches/*.patch
+    #if [ $? -ne 0 ]; then
+    #    git am --abort
+    #    echo "Applying the patches failed, aborting git-am"
+    #    exit 1
+    #fi
+else
+    echo "VLC source found"
+    cd vlc
+    if ! git cat-file -e ${TESTED_HASH}; then
+        cat << EOF
+***
+*** Error: Your vlc checkout does not contain the latest tested commit ***
+***
+
+EOF
+        exit 1
+    fi
+fi
+
+TARGET_TUPLE=i686-w64-mingw32
+
+echo "Building the contribs"
+mkdir -p contrib/winrt
+cd contrib/winrt
+../bootstrap --host=${TARGET_TUPLE} --disable-disc --disable-sout \
+    --disable-sdl \
+    --disable-SDL_image \
+    --disable-fontconfig \
+    --disable-zvbi \
+    --disable-kate \
+    --disable-caca \
+    --disable-gettext \
+    --disable-mpcdec \
+    --disable-upnp \
+    --disable-gme \
+    --disable-tremor \
+    --disable-vorbis \
+    --disable-sidplay2 \
+    --disable-samplerate \
+    --disable-faad2 \
+    --disable-harfbuzz \
+    --enable-iconv \
+    --disable-goom \
+    --disable-flac \
+    --disable-fontconfig \
+    --disable-gcrypt \
+    --disable-gpg-error \
+    --disable-gnutls \
+    --disable-projectM \
+    --disable-ass \
+    --disable-gpl
+
+make fetch
+make
+
+cd ../.. && mkdir -p winrt && cd winrt
+
+echo "Bootstraping"
+../bootstrap
+
+echo "Configuring"
+../extras/package/win32/configure.sh --host=${TARGET_TUPLE}
+
+echo "Building"
+make
