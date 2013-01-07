2da559f6ace155ab3f7ad916ec271787aae3fed9
Edward Wang <edward.c.wang@compdigitec.com>
2013-01-06 19:33:01 -0500

    Add some metadata

--- app/ui/Properties/AssemblyInfo.cs
+++ app/ui/Properties/AssemblyInfo.cs
@@ -1,17 +1,17 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VLC_WINRT")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VLC for WinRT")]
+[assembly: AssemblyDescription("VLC for WinRT™ is a port of VLC media player, the popular open source media player. The WinRT version can read most files and network streams.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VLC_WINRT")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("VideoLAN")]
+[assembly: AssemblyProduct("VLC for WinRT")]
+[assembly: AssemblyCopyright("Copyright © 2013 VideoLAN and VLC Authors")]
+[assembly: AssemblyTrademark("VLC media player, VideoLAN and x264 are registered trademarks of VideoLAN.")]
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
 [assembly: ComVisible(false)]
