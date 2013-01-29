a46e6508c3f923470a1e309e013e91dec1515418
Edward Wang <edward.c.wang@compdigitec.com>
2013-01-29 17:24:34 -0500

    VLC_WRAPPER: Factorise init

--- wrapper/VLC_Wrapper/Player.cpp
+++ wrapper/VLC_Wrapper/Player.cpp
@@ -1,32 +1,38 @@
 #include "pch.h"
 #include "Player.h"
 
+#include <exception>
+
 using namespace VLC_Wrapper;
 using namespace Platform;
 using namespace Windows::UI::Xaml::Media::Imaging;
 
-
-
 Player::Player(void)
 {
-	libvlc_instance_t *inst;
+	/* Don't add any invalid options, otherwise it causes LibVLC to fail */
+	static const char *argv[] = {
+		"-I", "dummy",
+		"--no-osd",
+		"--verbose=2",
+		"--no-video-title-show",
+		"--no-stats",
+		"--no-drop-late-frames",
+        //"--avcodec-fast"
+    };
+    p_instance = libvlc_new(sizeof(argv) / sizeof(*argv), argv);
+	if(!p_instance) {
+		throw new std::exception("Could not initialise libvlc!",1);
+		return;
+	}
+}
+
+
+void Player::TestMedia() {
 	libvlc_media_player_t *mp;
 	libvlc_media_t *m;
 
-	/* Don't add any invalid options, otherwise it causes LibVLC to fail */
-	static const char *argv[] = {
-        "-I", "dummy",
-        "--no-osd",
-        "--verbose=2",
-        "--no-video-title-show",
-        "--no-stats",
-        "--no-drop-late-frames",
-        //"--avcodec-fast"
-    };
-    inst = libvlc_new(sizeof(argv) / sizeof(*argv), argv);
-
 	// add a hard-coded http source for videos to play
-	m = libvlc_media_new_location(inst, "http://media.ch9.ms/ch9/e869/9d2a7276-5398-4fdd-9639-263a3a08e869/4-103.mp4");
+	m = libvlc_media_new_location(this->p_instance, "http://media.ch9.ms/ch9/e869/9d2a7276-5398-4fdd-9639-263a3a08e869/4-103.mp4");
 	mp = libvlc_media_player_new_from_media(m);
 
 
--- wrapper/VLC_Wrapper/Player.h
+++ wrapper/VLC_Wrapper/Player.h
@@ -1,11 +1,14 @@
 #pragma once
-#include "vlc\vlc.h"
+#include "vlc/vlc.h"
 
 namespace VLC_Wrapper {
 	public ref class Player sealed
 	{
 	public:
 		Player(void);
+		void TestMedia();
+	private:
+		libvlc_instance_t* p_instance;
 	};
 }
 
