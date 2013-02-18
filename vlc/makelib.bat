68f9b578b309ae737110d43ee992ace710586477
Rafaël Carré <funman@videolan.org>
2013-02-18 12:42:50 +0100

    Add a batch helper to generate libvlc.lib

---
+++ vlc/makelib.bat
@@ -0,0 +1,6 @@
+cls
+del libvlc.def
+echo EXPORTS > libvlc.def
+for /f "usebackq tokens=4,* delims=_ " %%i in (`dumpbin /exports libvlc.dll`) do @if %%i==libvlc echo %%i_%%j >> libvlc.def
+del libvlc.lib
+lib /def:"libvlc.def" /out:"libvlc.lib" /machine:x86
