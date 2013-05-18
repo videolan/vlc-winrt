9c532115034e2b431c8bdfc8c37ebba544ca2865
Rafaël Carré <funman@videolan.org>
2013-05-18 13:09:55 +0200

    new script to set AppContainer flag

--- vlc/compile.sh
+++ vlc/compile.sh
@@ -158,7 +158,7 @@ do
 done
 rm `find tmp/plugins -name 'lib*plugin.dll' | grep -E "lib(${regexp})_plugin.dll"`
 
-find tmp \( -name "*.dll" -o -name "*.exe" \) -exec ../extras/package/win32/peflags.pl -AppContainer {} \;
+find tmp \( -name "*.dll" -o -name "*.exe" \) -exec ../../appcontainer.pl {} \;
 
 cp lib/.libs/libvlc.dll.a tmp/libvlc.lib
 
