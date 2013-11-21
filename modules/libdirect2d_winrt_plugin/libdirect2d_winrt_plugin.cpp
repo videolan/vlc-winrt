4d12517031bb536835847bc52d10a4c59d4bf91a
Kellen Sunderland <kellen.sunderland@gmail.com>
2013-11-21 16:45:44 +0100

    prepare frame pool

--- modules/libdirect2d_winrt_plugin/libdirect2d_winrt_plugin.cpp
+++ modules/libdirect2d_winrt_plugin/libdirect2d_winrt_plugin.cpp
@@ -181,7 +181,7 @@ static picture_pool_t *Pool(vout_display_t *vd, unsigned count)
 	vout_display_sys_t *sys = vd->sys;
 
 	if (!sys->pool) {
-		//sys->pool = picture_pool_NewFromFormat(&vd->fmt, count);
+		sys->pool = picture_pool_NewFromFormat(&vd->fmt, count);
 	}
 
 	return sys->pool;
