d58b7329163202d9b0b5de859470dd0197a7e378
Kellen Sunderland <kellen.sunderland@gmail.com>
2013-12-08 14:44:05 +0100

    Added a network button, app bar section,  and open file call to action

--- app/ui/ViewModels/MainPage/MainPageViewModel.cs
+++ app/ui/ViewModels/MainPage/MainPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.Command;
 using VLC_WINRT.Common;
 using VLC_WINRT.Utility.Commands;
 
@@ -8,12 +9,14 @@ namespace VLC_WINRT.ViewModels.MainPage
     {
         private LastViewedViewModel _lastViewedVM;
         private LibraryViewModel _musicVM;
+        private bool _toggleNetworkAppBarVisibility;
         private PickVideoCommand _pickVideoCommand;
 
         private ObservableCollection<LibraryViewModel> _removableStorageVMs =
             new ObservableCollection<LibraryViewModel>();
 
         private LibraryViewModel _videoVM;
+        private RelayCommand _showNetworkAppBarCommand;
 
         public MainPageViewModel()
         {
@@ -22,6 +25,11 @@ namespace VLC_WINRT.ViewModels.MainPage
 
             LastViewedVM = new LastViewedViewModel();
             PickVideo = new PickVideoCommand();
+
+            _showNetworkAppBarCommand = new RelayCommand(() =>
+            {
+                this.ToggleNetworkAppBarVisibility = !this.ToggleNetworkAppBarVisibility;
+            });
         }
 
         public LibraryViewModel VideoVM
@@ -42,10 +50,22 @@ namespace VLC_WINRT.ViewModels.MainPage
             set { SetProperty(ref _lastViewedVM, value); }
         }
 
+        public bool ToggleNetworkAppBarVisibility
+        {
+            get { return _toggleNetworkAppBarVisibility; }
+            set { SetProperty(ref _toggleNetworkAppBarVisibility, value); }
+        }
+
         public PickVideoCommand PickVideo
         {
             get { return _pickVideoCommand; }
             set { SetProperty(ref _pickVideoCommand, value); }
+        }
+
+        public RelayCommand ShowNetworkAppBarCommand
+        {
+            get { return _showNetworkAppBarCommand; }
+            set { SetProperty(ref _showNetworkAppBarCommand, value); }
         }
 
         public ObservableCollection<LibraryViewModel> RemovableStorageVMs
