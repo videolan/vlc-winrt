41c0d8a47df8563d6a8cbb8b8c49587747f6ead4
Thomas Nigro <thomas.nigro@outlook.com>
2014-01-22 02:01:17 +0100

    Added a StringToUpperConverter for XAML Bindings
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ app/ui/Utility/Converters/StringToUpperConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace VLC_WINRT.Utility.Converters
+{
+    public class StringToUpperConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((string)value).ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
