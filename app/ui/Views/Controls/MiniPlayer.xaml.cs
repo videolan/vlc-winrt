d380418677e7e53588cf6940a42e3cd180c0f728
Thomas Nigro <thomas.nigro@outlook.com>
2014-01-22 22:42:00 +0100

    Add a Mini Player Class
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ app/ui/Views/Controls/MiniPlayer.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour en savoir plus sur le modèle d'élément Contrôle utilisateur, consultez la page http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace VLC_WINRT.Views.Controls
+{
+    public sealed partial class MiniPlayer : UserControl
+    {
+        public MiniPlayer()
+        {
+            this.InitializeComponent();
+        }
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private async void PlayerAppBarSection_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+        }
+
+        private void UserControl_SizeChanged_1(object sender, SizeChangedEventArgs e)
+        {
+        }
+    }
+}
