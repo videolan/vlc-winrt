123058622be471f09c81ea96374529bb6ed314bd
Thomas Nigro <thomas.nigro@outlook.com>
2014-01-22 22:45:23 +0100

    Add an animation when entering the music player page
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- app/ui/Utility/Commands/PlayAlbumCommand.cs
+++ app/ui/Utility/Commands/PlayAlbumCommand.cs
@@ -5,17 +5,32 @@ using VLC_WINRT.Common;
 using VLC_WINRT.Utility.Services.RunTime;
 using VLC_WINRT.ViewModels;
 using VLC_WINRT.ViewModels.MainPage;
+using VLC_WINRT.Views;
+using Windows.UI.Xaml.Media.Animation;
+using VLC_WINRT.Utility.Helpers;
 
 namespace VLC_WINRT.Utility.Commands
 {
     public class PlayAlbumCommand : AlwaysExecutableCommand
     {
-        public override void Execute(object parameter)
+        public async override void Execute(object parameter)
         {
             MusicLibraryViewModel.AlbumItem album = parameter as MusicLibraryViewModel.AlbumItem;
             Locator.MusicPlayerVM.TrackCollection.ResetCollection();
             Locator.MusicPlayerVM.TrackCollection.AddTrack(album.Tracks.ToList());
             Locator.MusicPlayerVM.PlayNext();
+
+            var frame = App.ApplicationFrame;
+            var page = frame.Content as MainPage;
+            if (page != null)
+            {
+                var sB = page.Resources["FadeOutPage"] as Storyboard;
+                if (sB != null)
+                {
+                    await sB.BeginAsync();
+                    NavigationService.NavigateTo(typeof(PlayMusic));
+                }
+            }
         }
     }
 }
