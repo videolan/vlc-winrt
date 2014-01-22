335af97b6267973a9a98a844144a1165b2ccee1e
Thomas Nigro <thomas.nigro@outlook.com>
2014-01-22 23:01:24 +0100

    Changes on the player code, with Animated Background
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- app/ui/ViewModels/MainPage/MusicLibraryViewModel.cs
+++ app/ui/ViewModels/MainPage/MusicLibraryViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Xml.Linq;
