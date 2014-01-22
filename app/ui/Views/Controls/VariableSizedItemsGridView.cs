013fba7dfa034a8feddbea61246c74ba2987a453
Thomas Nigro <thomas.nigro@outlook.com>
2014-01-22 01:48:38 +0100

    New control: VariableSizedItemsGridView and LittleAppBarButton like in W8.1
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ app/ui/Views/Controls/VariableSizedItemsGridView.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+using VLC_WINRT.ViewModels.MainPage;
+
+namespace VLC_WINRT.Views.Controls
+{
+    public class VariableSizedItemsGridView : GridView
+    {
+        private int i = -1;
+        protected override void PrepareContainerForItemOverride(Windows.UI.Xaml.DependencyObject element, object item)
+        {
+            var _Item = item as MediaViewModel;
+            i++;
+            if (i == 0)
+            {
+                element.SetValue(VariableSizedWrapGrid.ColumnSpanProperty, 2);
+                element.SetValue(VariableSizedWrapGrid.RowSpanProperty, 2);
+            }
+
+            base.PrepareContainerForItemOverride(element, item);
+        }
+    }
+}
