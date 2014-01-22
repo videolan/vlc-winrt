cc42cac921ad8b1557a3457b47033ff8cb364157
Thomas Nigro <thomas.nigro@outlook.com>
2014-01-22 02:15:31 +0100

    Added Animation and new layout to the MainPage
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- app/ui/Views/MainPage.xaml.cs
+++ app/ui/Views/MainPage.xaml.cs
@@ -1,5 +1,10 @@
 ﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+using VLC_WINRT.Utility.Helpers;
 using VLC_WINRT.ViewModels;
 
 namespace VLC_WINRT.Views
@@ -9,19 +14,60 @@ namespace VLC_WINRT.Views
     /// </summary>
     public sealed partial class MainPage : BasePage
     {
+        private int _currentSection;
         public MainPage()
         {
             InitializeComponent();
+            this.SizeChanged += OnSizeChanged;
+            this.Loaded += (sender, args) =>
+            {
+                FadeInPage.Begin();
+                for (int i = 0; i < SectionsGrid.Children.Count; i++)
+                {
+                    if (i == _currentSection) continue;
+                    UIAnimationHelper.FadeOut(SectionsGrid.Children[i]);
+                }
+            };
+            NavigationCacheMode = NavigationCacheMode.Enabled;
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs)
+        {
+            var width = Window.Current.Bounds.Width;
+            if (SectionsHeaderListView.ActualWidth + HeaderGrid.ActualWidth >= width)
+            {
+
+            }
         }
 
         public override void SetDataContext()
         {
-            _vm = (NavigateableViewModel) DataContext;
+            _vm = (NavigateableViewModel)DataContext;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+        private void SectionsHeaderListView_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            var i = ((Model.Panel)e.ClickedItem).Index;
+            ChangedSectionsHeadersState(i);
         }
 
-        private void SectionsHeaderListView_OnItemClick(object sender, Windows.UI.Xaml.Controls.ItemClickEventArgs e)
+        private void ChangedSectionsHeadersState(int i)
         {
+            if (i == _currentSection) return;
+            UIAnimationHelper.FadeOut(SectionsGrid.Children[_currentSection]);
+            UIAnimationHelper.FadeIn(SectionsGrid.Children[i]);
+            _currentSection = i;
+            for (int j = 0; j < SectionsHeaderListView.Items.Count; j++)
+                Locator.MainPageVM.Panels[j].Opacity = 0.4;
+            Locator.MainPageVM.Panels[i].Opacity = 1;
+        }
 
+        private void MinimizedBottomAppBar_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            BottomAppBar.IsOpen = true;
         }
     }
 }
