94f7e54592ffd71aab568db1b928439383698d19
Thomas Nigro <thomas.nigro@outlook.com>
2014-01-24 23:56:31 +0100

    Add a MainPageCommand to access the right panel

---
+++ app/ui/Utility/Commands/MainPageCommands/GoToPanelCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VLC_WINRT.Common;
+using VLC_WINRT.ViewModels;
+using Windows.UI.Core;
+
+namespace VLC_WINRT.Utility.Commands.MainPage
+{
+    public class GoToPanelCommand : AlwaysExecutableCommand
+    {
+        public async override void Execute(object parameter)
+        {
+            var frame = App.ApplicationFrame;
+            var page = frame.Content as Views.MainPage;
+            if (page != null)
+            {
+                page.ChangedSectionsHeadersState(int.Parse(parameter.ToString()));
+            }
+        }
+    }
+}
