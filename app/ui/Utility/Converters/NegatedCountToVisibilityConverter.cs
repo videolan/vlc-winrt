8041d294c5b6aaca171e7a4efc66ec7ca800969e
Thomas Nigro <thomas.nigro@outlook.com>
2014-01-28 00:31:34 +0100

    MiniPlayer is now displayed only when TrackCollection is not empty.
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- app/ui/Utility/Converters/NegatedCountToVisibilityConverter.cs
+++ app/ui/Utility/Converters/NegatedCountToVisibilityConverter.cs
@@ -4,18 +4,18 @@ using Windows.UI.Xaml.Data;
 
 namespace VLC_WINRT.Utility.Converters
 {
-    class CountToVisibilityConverter : IValueConverter
+    public class NegatedCountToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             if (value is int)
             {
-                if ((int) value == 0)
-                    return Visibility.Visible;
-                else
+                if ((int)value == 0)
                     return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
             }
-            return Visibility.Visible;
+            return Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
