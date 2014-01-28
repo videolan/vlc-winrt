293ca36688a071c2a26c68f34b0ba7d682aaaf7c
Thomas Nigro <thomas.nigro@outlook.com>
2014-01-28 22:09:10 +0100

    Modified Welcome Message and appearance.

--- app/ui/Views/Controls/MainPage/Welcome.xaml.cs
+++ app/ui/Views/Controls/MainPage/Welcome.xaml.cs
@@ -10,5 +10,16 @@ namespace VLC_WINRT.Views.Controls.MainPage
         {
             this.InitializeComponent();
         }
+
+        private void OpenAppBar_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var frame = App.ApplicationFrame;
+            var page = frame.Content as Views.MainPage;
+            if (page != null)
+            {
+                var appbar = page.FindName("BottomAppBar") as AppBar;
+                if (appbar != null) appbar.IsOpen = true;
+            }
+        }
     }
 }
