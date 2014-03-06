3b0852c36c26acb217bc954cc443578980ce165c
Jean-Baptiste Kempf <jb@videolan.org>
2014-03-06 20:43:01 +0100

    Remove tabs in Player.cpp

--- wrapper/libVLCX/Player.cpp
+++ wrapper/libVLCX/Player.cpp
@@ -26,24 +26,24 @@ using namespace Windows::Graphics::Display;
 
 Player::Player(SwapChainBackgroundPanel^ panel) :p_mp(NULL), p_instance(NULL)
 {
-	OutputDebugStringW(L"Hello, Player!");
-	p_panel = panel;
-	p_dxManager = new DirectXManger();
+    OutputDebugStringW(L"Hello, Player!");
+    p_panel = panel;
+    p_dxManager = new DirectXManger();
 
-	m_displayWidth = (float)(p_panel->ActualWidth * (float)DisplayProperties::ResolutionScale / 100.0f);
-	m_displayHeight = (float)(p_panel->ActualHeight * (float)DisplayProperties::ResolutionScale / 100.0f);
+    m_displayWidth = (float)(p_panel->ActualWidth * (float)DisplayProperties::ResolutionScale / 100.0f);
+    m_displayHeight = (float)(p_panel->ActualHeight * (float)DisplayProperties::ResolutionScale / 100.0f);
 }
 
 //Todo: don't block UI during initialization
 IAsyncAction^ Player::Initialize()
 {
-	p_dxManager->CreateSwapPanel(p_panel);
+    p_dxManager->CreateSwapPanel(p_panel);
 
-	IAsyncAction^ vlcInitTask = ThreadPool::RunAsync(ref new WorkItemHandler([=](IAsyncAction^ operation)
-	{
-		this->InitializeVLC();
-	}, Platform::CallbackContext::Any));
-	return vlcInitTask;
+    IAsyncAction^ vlcInitTask = ThreadPool::RunAsync(ref new WorkItemHandler([=](IAsyncAction^ operation)
+    {
+        this->InitializeVLC();
+    }, Platform::CallbackContext::Any));
+    return vlcInitTask;
 }
 
 size_t  Player::ToCharArray(Platform::String^ str, char *arr, size_t maxSize)
@@ -55,205 +55,207 @@ size_t  Player::ToCharArray(Platform::String^ str, char *arr, size_t maxSize)
 
 void Player::InitializeVLC()
 {
-	char ptr_d2dstring[40];
-	sprintf_s(ptr_d2dstring, "--winrt-d2dcontext=0x%p", p_dxManager->cp_d2dContext);
+    char ptr_d2dstring[40];
+    sprintf_s(ptr_d2dstring, "--winrt-d2dcontext=0x%p", p_dxManager->cp_d2dContext);
 
-	char ptr_scstring[40];
-	sprintf_s(ptr_scstring, "--winrt-swapchain=0x%p", p_dxManager->cp_swapChain);
+    char ptr_scstring[40];
+    sprintf_s(ptr_scstring, "--winrt-swapchain=0x%p", p_dxManager->cp_swapChain);
 
-	char widthstring[40];
-	sprintf_s(widthstring, "--winrt-width=%f", m_displayWidth);
+    char widthstring[40];
+    sprintf_s(widthstring, "--winrt-width=%f", m_displayWidth);
 
-	char heightstring[40];
-	sprintf_s(heightstring, "--winrt-height=%f", m_displayHeight);
+    char heightstring[40];
+    sprintf_s(heightstring, "--winrt-height=%f", m_displayHeight);
 
     char fontstring[128 + 28];
     char packagePath[128];
-   
+
     Windows::ApplicationModel::Package^ currentPackage = Windows::ApplicationModel::Package::Current;
     ToCharArray(currentPackage->InstalledLocation->Path, packagePath, 128);
     sprintf_s(fontstring, "--freetype-font=%s\\segoeui.ttf", packagePath);
 
-	/* Don't add any invalid options, otherwise it causes LibVLC to fail */
-	const char *argv[] = {
-		"-I",
-		"dummy",
-		"--no-osd",
-		"--verbose=2",
-		"--no-stats",
-		ptr_d2dstring,
-		ptr_scstring,
-		widthstring,
-		heightstring,
-		"--avcodec-fast",
-		"--no-avcodec-dr",
+    /* Don't add any invalid options, otherwise it causes LibVLC to fail */
+    const char *argv[] = {
+        "-I",
+        "dummy",
+        "--no-osd",
+        "--verbose=2",
+        "--no-stats",
+        ptr_d2dstring,
+        ptr_scstring,
+        widthstring,
+        heightstring,
+        "--avcodec-fast",
+        "--no-avcodec-dr",
         fontstring
-	};
+    };
 
-	p_instance = libvlc_new(sizeof(argv) / sizeof(*argv), argv);
-	if (!p_instance) {
-		throw new std::exception("Could not initialise libvlc!", 1);
-		return;
-	}
+    p_instance = libvlc_new(sizeof(argv) / sizeof(*argv), argv);
+    if (!p_instance) {
+        throw new std::exception("Could not initialise libvlc!", 1);
+        return;
+    }
 }
 
 void Player::UpdateSize(unsigned int x, unsigned int y)
 {
-	// Have to update the size there
+    // Have to update the size there
 }
 
 void Player::MediaEndedCall(){
-	MediaEnded();
+    MediaEnded();
 }
+
 void vlc_event_callback(const libvlc_event_t *ev, void *data)
 {
-	Player ^player = ((PlayerPointerWrapper*)data)->player;
-	if (ev->type == libvlc_MediaPlayerEndReached)
-	{
-		player->DetachEvent();
-		player->MediaEndedCall();
-	}
+    Player ^player = ((PlayerPointerWrapper*)data)->player;
+    if (ev->type == libvlc_MediaPlayerEndReached)
+    {
+        player->DetachEvent();
+        player->MediaEndedCall();
+    }
 }
+
 void Player::DetachEvent(){
-	libvlc_event_manager_t *ev = libvlc_media_player_event_manager(p_mp);
-	static const libvlc_event_type_t mp_events[] = {
-		libvlc_MediaPlayerPlaying,
-		libvlc_MediaPlayerPaused,
-		libvlc_MediaPlayerEndReached,
-		libvlc_MediaPlayerStopped,
-		libvlc_MediaPlayerVout,
-		libvlc_MediaPlayerPositionChanged,
-		libvlc_MediaPlayerEncounteredError
-	};
-	for (int i = 0; i < (sizeof(mp_events) / sizeof(*mp_events)); i++)
-		libvlc_event_detach(ev, mp_events[i], vlc_event_callback, new PlayerPointerWrapper(this));
+    libvlc_event_manager_t *ev = libvlc_media_player_event_manager(p_mp);
+    static const libvlc_event_type_t mp_events[] = {
+        libvlc_MediaPlayerPlaying,
+        libvlc_MediaPlayerPaused,
+        libvlc_MediaPlayerEndReached,
+        libvlc_MediaPlayerStopped,
+        libvlc_MediaPlayerVout,
+        libvlc_MediaPlayerPositionChanged,
+        libvlc_MediaPlayerEncounteredError
+    };
+    for (int i = 0; i < (sizeof(mp_events) / sizeof(*mp_events)); i++)
+        libvlc_event_detach(ev, mp_events[i], vlc_event_callback, new PlayerPointerWrapper(this));
 }
 
 void Player::Open(Platform::String^ mrl)
 {
-	size_t len = WideCharToMultiByte(CP_UTF8, 0, mrl->Data(), -1, NULL, 0, NULL, NULL);
-	char* p_mrl = new char[len];
-	WideCharToMultiByte(CP_UTF8, 0, mrl->Data(), -1, p_mrl, len, NULL, NULL);
+    size_t len = WideCharToMultiByte(CP_UTF8, 0, mrl->Data(), -1, NULL, 0, NULL, NULL);
+    char* p_mrl = new char[len];
+    WideCharToMultiByte(CP_UTF8, 0, mrl->Data(), -1, p_mrl, len, NULL, NULL);
 
-	if (p_instance){
-		libvlc_media_t* m = libvlc_media_new_location(this->p_instance, p_mrl);
-		p_mp = libvlc_media_player_new_from_media(m);
+    if (p_instance){
+        libvlc_media_t* m = libvlc_media_new_location(this->p_instance, p_mrl);
+        p_mp = libvlc_media_player_new_from_media(m);
 
-		/* Connect the event manager */
-		libvlc_event_manager_t *ev = libvlc_media_player_event_manager(p_mp);
-		static const libvlc_event_type_t mp_events[] = {
-			libvlc_MediaPlayerPlaying,
-			libvlc_MediaPlayerPaused,
-			libvlc_MediaPlayerEndReached,
-			libvlc_MediaPlayerStopped,
-			libvlc_MediaPlayerVout,
-			libvlc_MediaPlayerPositionChanged,
-			libvlc_MediaPlayerEncounteredError
-		};
+        /* Connect the event manager */
+        libvlc_event_manager_t *ev = libvlc_media_player_event_manager(p_mp);
+        static const libvlc_event_type_t mp_events[] = {
+            libvlc_MediaPlayerPlaying,
+            libvlc_MediaPlayerPaused,
+            libvlc_MediaPlayerEndReached,
+            libvlc_MediaPlayerStopped,
+            libvlc_MediaPlayerVout,
+            libvlc_MediaPlayerPositionChanged,
+            libvlc_MediaPlayerEncounteredError
+        };
 
-		for (int i = 0; i < (sizeof(mp_events) / sizeof(*mp_events)); i++)
-		{
-			libvlc_event_attach(ev, mp_events[i], vlc_event_callback, new PlayerPointerWrapper(this));
-		}
+        for (int i = 0; i < (sizeof(mp_events) / sizeof(*mp_events)); i++)
+        {
+            libvlc_event_attach(ev, mp_events[i], vlc_event_callback, new PlayerPointerWrapper(this));
+        }
 
-		libvlc_media_release(m);
-	}
+        libvlc_media_release(m);
+    }
 
-	delete[](p_mrl);
+    delete[](p_mrl);
 }
 
 void Player::Stop()
 {
-	if (p_mp)
-	{
-		libvlc_media_player_stop(p_mp);
-	}
-	return;
+    if (p_mp)
+    {
+        libvlc_media_player_stop(p_mp);
+    }
+    return;
 }
 
 void Player::Pause()
 {
-	if (p_mp)
-	{
-		libvlc_media_player_pause(p_mp);
-	}
-	return;
+    if (p_mp)
+    {
+        libvlc_media_player_pause(p_mp);
+    }
+    return;
 }
 
 void Player::Play()
 {
-	if (p_mp)
-	{
-		libvlc_media_player_play(p_mp);
-	}
-	return;
+    if (p_mp)
+    {
+        libvlc_media_player_play(p_mp);
+    }
+    return;
 }
 
 void Player::Seek(float position)
 {
-	if (p_mp)
-	{
-		if (libvlc_media_player_is_seekable(p_mp))
-		{
-			libvlc_media_player_set_position(p_mp, position);
-		}
-	}
+    if (p_mp)
+    {
+        if (libvlc_media_player_is_seekable(p_mp))
+        {
+            libvlc_media_player_set_position(p_mp, position);
+        }
+    }
 }
 
 float Player::GetPosition()
 {
-	float position = 0.0f;
-	if (p_mp)
-	{
-		position = libvlc_media_player_get_position(p_mp);
-	}
+    float position = 0.0f;
+    if (p_mp)
+    {
+        position = libvlc_media_player_get_position(p_mp);
+    }
 
-	return position;
+    return position;
 }
 
 int64 Player::GetLength()
 {
-	int64 length = 0;
-	if (p_mp)
-	{
-		length = libvlc_media_player_get_length(p_mp);
-	}
-	return length;
+    int64 length = 0;
+    if (p_mp)
+    {
+        length = libvlc_media_player_get_length(p_mp);
+    }
+    return length;
 }
 
 int Player::GetSubtitleCount(){
-	int subtitleTrackCount = 0;
-	if (p_mp)
-	{
-		subtitleTrackCount = libvlc_video_get_spu_count(p_mp);
-	}
-	return subtitleTrackCount;
+    int subtitleTrackCount = 0;
+    if (p_mp)
+    {
+        subtitleTrackCount = libvlc_video_get_spu_count(p_mp);
+    }
+    return subtitleTrackCount;
 }
 
 int Player::SetSubtitleTrack(int track){
-	int spuDelay = 0;
-	if (p_mp)
-	{
-		spuDelay = libvlc_video_set_spu(p_mp, track);
-	}
-	return spuDelay;
+    int spuDelay = 0;
+    if (p_mp)
+    {
+        spuDelay = libvlc_video_set_spu(p_mp, track);
+    }
+    return spuDelay;
 }
 
 
 Player::~Player()
 {
-	if (p_mp){
-		libvlc_media_player_release(p_mp);
-	}
-	if (p_instance){
-		libvlc_release(p_instance);
-	}
+    if (p_mp){
+        libvlc_media_player_release(p_mp);
+    }
+    if (p_instance){
+        libvlc_release(p_instance);
+    }
 
 
-	/*if (p_dxManager){
-		delete p_dxManager;
-		p_dxManager = nullptr;
-		}*/
+    /*if (p_dxManager){
+        delete p_dxManager;
+        p_dxManager = nullptr;
+        }*/
 }
 
 
