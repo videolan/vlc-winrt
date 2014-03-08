669c0d4745f7a6bec05e95f4fa686371f968c812
Jean-Baptiste Kempf <jb@videolan.org>
2014-03-08 13:34:17 +0100

    Remove Backer runtime error

--- app/ui/Utility/Helpers/LoadBackers.cs
+++ app/ui/Utility/Helpers/LoadBackers.cs
@@ -7,9 +7,9 @@ namespace VLC_WINRT.Utility.Helpers
     {
         public static async void Get()
         {
-            string path = @"£15.json";
+           /* string path = @"£15.json";
             StorageFolder folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
-            StorageFile file = await folder.GetFileAsync(path);
+            StorageFile file = await folder.GetFileAsync(path); */
         }
     }
 }
