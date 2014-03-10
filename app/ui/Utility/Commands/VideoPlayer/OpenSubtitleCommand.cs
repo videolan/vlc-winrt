871e368be1e2048ac71c127557fe917440cd3008
Thomas Nigro <thomas.nigro@outlook.com>
2014-03-11 00:00:49 +0100

    Forgotten file for opening subtitle file
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ app/ui/Utility/Commands/VideoPlayer/OpenSubtitleCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
+using VLC_WINRT.Common;
+using VLC_WINRT.Utility.Services.RunTime;
+using VLC_WINRT.ViewModels;
+
+namespace VLC_WINRT.Utility.Commands.VideoPlayer
+{
+    public class OpenSubtitleCommand : AlwaysExecutableCommand
+    {
+        public async override void Execute(object parameter)
+        {
+            var picker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.List,
+                SuggestedStartLocation = PickerLocationId.VideosLibrary
+            };
+            picker.FileTypeFilter.Add(".srt");
+            picker.FileTypeFilter.Add(".ass");
+
+            StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                string mru = StorageApplicationPermissions.FutureAccessList.Add(file);
+
+                string mrl = "winrt://" + mru;
+                Locator.PlayVideoVM.OpenSubtitle(mrl);
+            }
+            else
+            {
+                Debug.WriteLine("Cancelled Opening subtitle");
+            }
+        }
+    }
+}
