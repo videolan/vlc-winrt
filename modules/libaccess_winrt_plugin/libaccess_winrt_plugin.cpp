5d02fb65b53df1144c0040d2f5a88026e064e5e6
Jean-Baptiste Kempf <jb@videolan.org>
2014-03-11 17:01:51 +0100

    Access WinRT: use local values in p_sys
    And reindent

--- modules/libaccess_winrt_plugin/libaccess_winrt_plugin.cpp
+++ modules/libaccess_winrt_plugin/libaccess_winrt_plugin.cpp
@@ -1,23 +1,22 @@
 ﻿/*****************************************************************************
-* Copyright © 2013-2014 VideoLAN
-*
-* Authors: Kellen Sunderland <kellen _DOT_ sunderland _AT_ gmail _DOT_ com>
-*
-* This program is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation; either version 2.1 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program; if not, write to the Free Software Foundation,
-* Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
-*****************************************************************************/
-
+ * Copyright © 2013-2014 VideoLAN
+ *
+ * Authors: Kellen Sunderland <kellen _DOT_ sunderland _AT_ gmail _DOT_ com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 
 /*****************************************************************************
 * Preamble
@@ -27,15 +26,15 @@
 #include <ppltasks.h>
 
 #ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN /* Exclude rarely-used stuff from Windows headers */
+# define WIN32_LEAN_AND_MEAN /* Exclude rarely-used stuff from Windows headers */
 #endif
 
 #ifdef _MSC_VER /* help visual studio compile vlc headers */
-#define inline __inline
-#define strdup _strdup
-#define ssize_t SSIZE_T
-#define N_(x) x
-#define _(x) x
+# define inline __inline
+# define strdup _strdup
+# define ssize_t SSIZE_T
+# define N_(x) x
+# define _(x) x
 int poll(struct pollfd *, unsigned, int);
 #endif
 
@@ -53,11 +52,10 @@ using namespace Windows::Foundation;
 
 static int                    Open(vlc_object_t *);
 static void                   Close(vlc_object_t *);
+
 static ssize_t                Read(access_t *access, uint8_t *buffer, size_t size);
 static int                    Seek(access_t *access, uint64_t position);
 static int                    Control(access_t *access, int query, va_list args);
-static IRandomAccessStream^   readStream;
-static DataReader^            dataReader;
 
 #include "../../wrapper/libVLCX/Helpers.h"
 
@@ -66,14 +64,20 @@ static DataReader^            dataReader;
 *****************************************************************************/
 
 vlc_module_begin()
-    set_shortname(N_("WinRTInput"))
-    set_description(N_("WinRT input"))
-    set_category(CAT_INPUT)
-    set_subcategory(SUBCAT_INPUT_ACCESS)
-    set_capability("access", 80)
-    add_shortcut("winrt", "file")
-    set_callbacks(&Open, &Close)
+set_shortname(N_("WinRTInput"))
+set_description(N_("WinRT input"))
+set_category(CAT_INPUT)
+set_subcategory(SUBCAT_INPUT_ACCESS)
+set_capability("access", 80)
+add_shortcut("winrt", "file")
+set_callbacks(&Open, &Close)
 vlc_module_end()
+
+struct access_sys_t
+{
+    IRandomAccessStream^   readStream;
+    DataReader^            dataReader;
+};
 
 /*****************************************************************************
 * Local prototypes
@@ -81,23 +85,23 @@ vlc_module_end()
 
 /**
  * Handles the file opening
-*/
-static int OpenFileAsync(String^ token)
+ */
+static int OpenFileAsync(access_sys_t *p_sys, String^ token)
 {
     auto openTask = create_task(StorageApplicationPermissions::FutureAccessList->GetFileAsync(token))
-        .then([](StorageFile^ newFile){
-            create_task(newFile->OpenReadAsync())
-                .then([](task<IRandomAccessStreamWithContentType^> task){
-                    readStream = task.get();
-                    dataReader = ref new DataReader(readStream);
-                }).wait();
-        });
+        .then([p_sys](StorageFile^ newFile){
+        create_task(newFile->OpenReadAsync())
+            .then([p_sys](task<IRandomAccessStreamWithContentType^> task){
+            p_sys->readStream = task.get();
+            p_sys->dataReader = ref new DataReader(p_sys->readStream);
+        }).wait();
+    });
 
     try
     {
         openTask.wait();  /* block with wait since we're in a worker thread */
     }
-    catch(Exception^)
+    catch( Exception^ )
     {
         OutputDebugString(L"Failed to open file.");
         return VLC_EGENERIC;
@@ -113,31 +117,25 @@ static int OpenFileAsync(String^ token)
  */
 int Open(vlc_object_t *object)
 {
-    access_t *access    = (access_t *) object;
+    access_t *access = (access_t *) object;
 
     String^ futureAccesToken = GetString(access->psz_location);
-    if (!(futureAccesToken->Begin()[0] == '{' ) && (futureAccesToken->Length() > 32 ) )
+    if( !(futureAccesToken->Begin()[0] == '{') && (futureAccesToken->Length() > 32) )
         return VLC_EGENERIC;
 
-    if ( OpenFileAsync(futureAccesToken) != VLC_SUCCESS ){
+    access_sys_t *p_sys = access->p_sys = new access_sys_t();
+    if( p_sys == nullptr )
+        return VLC_EGENERIC;
+
+    if( OpenFileAsync(p_sys, futureAccesToken) != VLC_SUCCESS ){
         OutputDebugStringW(L"Error Opening File");
-
-        if (dataReader != nullptr){
-            delete dataReader;
-            dataReader = nullptr;
-        }
-
-        if (readStream != nullptr){
-            delete readStream;
-            readStream = nullptr;
-        }
-
+        Close(object);
         return VLC_EGENERIC;
     }
 
-    access->pf_read     = &Read;
-    access->pf_seek     = &Seek;
-    access->pf_control  = &Control;
+    access->pf_read = &Read;
+    access->pf_seek = &Seek;
+    access->pf_control = &Control;
 
     return VLC_SUCCESS;
 }
@@ -146,26 +144,29 @@ int Open(vlc_object_t *object)
 void Close(vlc_object_t *object)
 {
     access_t     *access = (access_t *) object;
-
-    if (dataReader != nullptr){
-        delete dataReader;
-        dataReader = nullptr;
+    access_sys_t *p_sys = access->p_sys;
+    if( p_sys->dataReader != nullptr ){
+        delete p_sys->dataReader;
+        p_sys->dataReader = nullptr;
     }
-    if (readStream != nullptr){
-        delete readStream;
-        readStream = nullptr;
+    if( p_sys->readStream != nullptr ){
+        delete p_sys->readStream;
+        p_sys->readStream = nullptr;
     }
+    delete p_sys;
 }
 
 /* */
 ssize_t Read(access_t *access, uint8_t *buffer, size_t size)
 {
+    access_sys_t *p_sys = access->p_sys;
+
     unsigned int totalRead = 0;
 
-    auto readTask = create_task(dataReader->LoadAsync(size)).then([&totalRead, &buffer](unsigned int numBytesLoaded)
+    auto readTask = create_task(p_sys->dataReader->LoadAsync(size)).then([&totalRead, &buffer, p_sys](unsigned int numBytesLoaded)
     {
         WriteOnlyArray<unsigned char, 1U>^ bufferArray = ref new Array<unsigned char, 1U>(numBytesLoaded);
-        dataReader->ReadBytes(bufferArray);
+        p_sys->dataReader->ReadBytes(bufferArray);
         memcpy(buffer, bufferArray->begin(), bufferArray->end() - bufferArray->begin());
         totalRead = numBytesLoaded;
     });
@@ -174,12 +175,12 @@ ssize_t Read(access_t *access, uint8_t *buffer, size_t size)
     {
         readTask.wait(); /* block with wait since we're in a worker thread */
     }
-    catch(Exception^ ex)
+    catch( Exception^ ex )
     {
         OutputDebugString(L"Failure while reading block");
-        if (ex->HResult == HRESULT_FROM_WIN32(ERROR_OPLOCK_HANDLE_CLOSED)){
-            if (OpenFileAsync(GetString(access->psz_location)) == VLC_SUCCESS){
-                readStream->Seek(access->info.i_pos);
+        if( ex->HResult == HRESULT_FROM_WIN32(ERROR_OPLOCK_HANDLE_CLOSED) ){
+            if( OpenFileAsync(p_sys, GetString(access->psz_location)) == VLC_SUCCESS ){
+                p_sys->readStream->Seek(access->info.i_pos);
                 return Read(access, buffer, size);
             }
             OutputDebugString(L"Failed to reopen file");
@@ -188,8 +189,8 @@ ssize_t Read(access_t *access, uint8_t *buffer, size_t size)
     }
 
     access->info.i_pos += totalRead;
-    access->info.b_eof = readStream->Position >= readStream->Size;
-    if (access->info.b_eof){
+    access->info.b_eof = p_sys->readStream->Position >= p_sys->readStream->Size;
+    if( access->info.b_eof ){
         OutputDebugString(L"End of file reached");
     }
 
@@ -199,15 +200,17 @@ ssize_t Read(access_t *access, uint8_t *buffer, size_t size)
 /* */
 int Seek(access_t *access, uint64_t position)
 {
+    access_sys_t *p_sys = access->p_sys;
+
     try
     {
-        readStream->Seek(position);
+        p_sys->readStream->Seek(position);
         access->info.i_pos = position;
-        access->info.b_eof = readStream->Position >= readStream->Size;
+        access->info.b_eof = p_sys->readStream->Position >= p_sys->readStream->Size;
     }
-    catch (int ex)
+    catch( int ex )
     {
-        Debug( L"Exception: 0x%x", ex);
+        Debug(L"Exception: 0x%x", ex);
         return VLC_EGENERIC;
     }
 
@@ -218,32 +221,31 @@ int Seek(access_t *access, uint64_t position)
 int Control(access_t *access, int query, va_list args)
 {
     VLC_UNUSED(access);
-    switch (query)
+    switch( query )
     {
     case ACCESS_CAN_FASTSEEK:
     case ACCESS_CAN_PAUSE:
     case ACCESS_CAN_SEEK:
     case ACCESS_CAN_CONTROL_PACE: {
-                                      bool *b = va_arg(args, bool*);
-                                      *b = true;
-                                      return VLC_SUCCESS;
+        bool *b = va_arg(args, bool*);
+        *b = true;
+        return VLC_SUCCESS;
     }
 
     case ACCESS_GET_PTS_DELAY: {
-                                   int64_t *d = va_arg(args, int64_t *);
-                                   *d = DEFAULT_PTS_DELAY;
-                                   return VLC_SUCCESS;
+        int64_t *d = va_arg(args, int64_t *);
+        *d = DEFAULT_PTS_DELAY;
+        return VLC_SUCCESS;
     }
 
     case ACCESS_SET_PAUSE_STATE:
         return VLC_SUCCESS;
 
     case ACCESS_GET_SIZE: {
-        *va_arg( args, uint64_t *) = readStream->Size;
+        *va_arg(args, uint64_t *) = access->p_sys->readStream->Size;
         return VLC_SUCCESS;
     }
     default:
         return VLC_EGENERIC;
     }
 }
-
