cff44855756e4df0a7fb8523c6de998d03f89a75
Jean-Baptiste Kempf <jb@videolan.org>
2014-03-16 21:11:31 +0100

    Add license headers

--- app/ui/Common/AutoFacConfiguration.cs
+++ app/ui/Common/AutoFacConfiguration.cs
@@ -1,4 +1,13 @@
-﻿using Autofac;
+﻿/**********************************************************************
+ * VLC for WinRT
+ **********************************************************************
+ * Copyright © 2013-2014 VideoLAN and Authors
+ *
+ * Licensed under GPLv2+ and MPLv2
+ * Refer to COPYING file of the official project for license
+ **********************************************************************/
+
+using Autofac;
 using VLC_WINRT.Utility.Services.Interface;
 using VLC_WINRT.Utility.Services.RunTime;
 using DesignTime = VLC_WINRT.Utility.Services.DesignTime;
