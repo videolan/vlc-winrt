6c34d204bf0f1b0bc62accc6eddc67c5e0770801
Jean-Baptiste Kempf <jb@videolan.org>
2014-03-16 22:32:03 +0100

    s/c/©/

--- app/ui/Utility/Helpers/MusicLibrary/Deezer/Artists.cs
+++ app/ui/Utility/Helpers/MusicLibrary/Deezer/Artists.cs
@@ -1,7 +1,7 @@
 /**********************************************************************
  * VLC for WinRT
  **********************************************************************
- * Copyright c 2013-2014 VideoLAN and Authors
+ * Copyright © 2013-2014 VideoLAN and Authors
  *
  * Licensed under GPLv2+ and MPLv2
  * Refer to COPYING file of the official project for license
