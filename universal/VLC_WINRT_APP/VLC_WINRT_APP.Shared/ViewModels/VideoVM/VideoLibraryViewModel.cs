d2e705457dd0c899657f255ccf2828da41efcaf1
Thomas Nigro <thomas.nigro@outlook.com>
2014-06-24 16:18:00 +0200

    Organizing VideoLibraryViewModel by regions
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/ViewModels/VideoVM/VideoLibraryViewModel.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/ViewModels/VideoVM/VideoLibraryViewModel.cs
@@ -21,9 +21,11 @@ using Windows.Storage.AccessCache;
 using Windows.Storage.Search;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
+using SQLite;
 using VLC_WINRT.Common;
 using VLC_WINRT.ViewModels.MainPage;
 using VLC_WINRT_APP.Commands;
+using VLC_WINRT_APP.DataRepository;
 using VLC_WINRT_APP.ViewModels.Settings;
 using Panel = VLC_WINRT_APP.Model.Panel;
 
@@ -31,27 +33,25 @@ namespace VLC_WINRT_APP.ViewModels.VideoVM
 {
     public class VideoLibraryVM : BindableBase
     {
+        #region private fields
+#if WINDOWS_APP
         private ObservableCollection<Panel> _panels = new ObservableCollection<Panel>();
-        private bool _hasNoMedia = true;
-        private ObservableCollection<MediaViewModel> _media;
-        private ObservableCollection<MediaViewModel> _mediaRandom;
-        private IEnumerable<IGrouping<char, MediaViewModel>> _mediaGroupedByAlphabet;
+#endif
+        private ObservableCollection<VideoVM> _videos;
+        private ObservableCollection<VideoVM> _viewedVideos;
+        private ObservableCollection<VideoVM> _newVideos;
+        private IEnumerable<IGrouping<char, VideoVM>> _mediaGroupedByAlphabet;
+        #endregion
+
+        #region private props
+        private PlayVideoCommand _openVideo;
         private PickVideoCommand _pickCommand = new PickVideoCommand();
+        private bool _hasNoMedia = true;
+        public LastVideosRepository _lastVideosRepository = new LastVideosRepository();
+        #endregion
 
-        public VideoLibraryVM()
-        {
-            Media = new ObservableCollection<MediaViewModel>();
-            MediaRandom = new ObservableCollection<MediaViewModel>();
-            Panels.Add(new Panel("ALL", 0, 1));
-            Panels.Add(new Panel("NEVER SEEN BEFORE", 1, 0.4));
-            Panels.Add(new Panel("FAVORITE", 2, 0.4));
-        }
-
-        public bool IsVideoLibraryEmpty
-        {
-            get { return Media.Any(); }
-        }
-
+        #region public fields
+#if WINDOWS_APP
         public ObservableCollection<Panel> Panels
         {
             get { return _panels; }
@@ -60,81 +60,147 @@ namespace VLC_WINRT_APP.ViewModels.VideoVM
                 SetProperty(ref _panels, value);
             }
         }
+#endif
+
+        public ObservableCollection<VideoVM> Videos
+        {
+            get { return _videos; }
+            set { SetProperty(ref _videos, value); }
+        }
+
+        public ObservableCollection<VideoVM> ViewedVideos
+        {
+            get { return _viewedVideos; }
+            set
+            {
+                SetProperty(ref _viewedVideos, value);
+            }
+        }
+
+        public ObservableCollection<VideoVM> NewVideos
+        {
+            get { return _newVideos; }
+            set { SetProperty(ref _newVideos, value); }
+        }
+
+        public IEnumerable<IGrouping<char, VideoVM>> MediaGroupedByAlphabet
+        {
+            get { return _mediaGroupedByAlphabet; }
+            set { SetProperty(ref _mediaGroupedByAlphabet, value); }
+        }
+        #endregion
+
+        #region public props
         public bool HasNoMedia
         {
             get { return _hasNoMedia; }
             set { SetProperty(ref _hasNoMedia, value); }
         }
 
-        public IEnumerable<IGrouping<char, MediaViewModel>> MediaGroupedByAlphabet
+        [Ignore]
+        public PlayVideoCommand OpenVideo
         {
-            get { return _mediaGroupedByAlphabet; }
-            set { SetProperty(ref _mediaGroupedByAlphabet, value); }
+            get { return _openVideo; }
+            set { SetProperty(ref _openVideo, value); }
         }
-        public ObservableCollection<MediaViewModel> Media
+        #endregion
+        #region contructors
+
+
+        public VideoLibraryVM()
         {
-            get { return _media; }
-            set { SetProperty(ref _media, value); }
+            OpenVideo = new PlayVideoCommand();
+            Videos = new ObservableCollection<VideoVM>();
+            ViewedVideos = new ObservableCollection<VideoVM>();
+
+            GetViewedVideos();
+#if WINDOWS_APP
+            Panels.Add(new Panel("all", 0, 1));
+            Panels.Add(new Panel("new", 1, 0.4));
+            //Panels.Add(new Panel("favorite", 2, 0.4));
+#endif
         }
 
-        public ObservableCollection<MediaViewModel> MediaRandom
+        #endregion
+
+        #region methods
+        public async Task GetViewedVideos()
         {
-            get { return _mediaRandom; }
-            set { SetProperty(ref _mediaRandom, value); }
+            DispatchHelper.Invoke(async () => ViewedVideos = await _lastVideosRepository.Load());
+            NewVideos = new ObservableCollection<VideoVM>();
+            GetVideos();
         }
 
-        public PickVideoCommand PickCommand
+        public async Task GetVideos()
         {
-            get { return _pickCommand; }
-            set { SetProperty(ref _pickCommand, value); }
-        }
-
-        public async Task GetMedia()
-        {
-            foreach (CustomFolder folder in Locator.SettingsVM.VideoFolders)
+            //#if WINDOWS_APP
+            //            foreach (CustomFolder folder in Locator.SettingsVM.VideoFolders)
+            //            {
+            //#endif
+            try
             {
-                try
+                StorageFolder customVideoFolder;
+                //                    #if WINDOWS_APP
+                //                    if (folder.Mru == "Video Library")
+                //                    {
+                //                        customVideoFolder = KnownFolders.VideosLibrary;
+                //                    }
+                //                    else
+                //                    {
+                //                        customVideoFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync(
+                //                            folder.Mru);
+                //                    }
+                //#endif
+                //#if WINDOWS_PHONE_APP                    
+                customVideoFolder = KnownFolders.VideosLibrary;
+                //#endif
+
+                IReadOnlyList<StorageFile> files =
+                    await GetMediaFromFolder(customVideoFolder, CommonFileQuery.OrderByName);
+
+                //int j = 0;
+                foreach (StorageFile storageFile in files)
                 {
-                    StorageFolder customVideoFolder;
-                    if (folder.Mru == "Video Library")
+                    var mediaVM = new VideoVM();
+                    mediaVM.Initialize(storageFile);
+                    await mediaVM.Initialize();
+
+                    if (string.IsNullOrEmpty(mediaVM.Title))
+                        continue;
+
+                    VideoVM searchVideo = ViewedVideos.FirstOrDefault(x => x.Title == mediaVM.Title);
+                    if (searchVideo != null)
                     {
-                        customVideoFolder = KnownVLCLocation.VideosLibrary;
+                        mediaVM.TimeWatched = searchVideo.TimeWatched;
                     }
                     else
                     {
-                        customVideoFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync(
-                            folder.Mru);
+                        DispatchHelper.Invoke(() => NewVideos.Add(mediaVM));
                     }
-                    IReadOnlyList<StorageFile> files =
-                        await GetMediaFromFolder(customVideoFolder, CommonFileQuery.OrderByName);
-                    
-                    int j = 0;
-                    foreach (StorageFile storageFile in files)
+
+                    // Get back to UI thread
+                    await DispatchHelper.InvokeAsync(() =>
                     {
-                        var mediaVM = new MediaViewModel();
-                        mediaVM.Initialize(storageFile);
-                        await mediaVM.Initialize();
-                        // Get back to UI thread
-                        await DispatchHelper.InvokeAsync(() =>
-                        {
-                            Media.Add(mediaVM);
-                            int i = new Random().Next(0, files.Count - 1);
-                            if (j < 5 && i <= (files.Count - 1) / 2)
-                            {
-                                MediaRandom.Add(mediaVM);
-                                j++;
-                            }
-                            MediaGroupedByAlphabet = Media.OrderBy(x => x.AlphaKey).GroupBy(x => x.AlphaKey);
-                        });
-                    }
-                }
-                catch (Exception exception)
-                {
-                    Debug.WriteLine("An error occured while indexing a video folder");
+                        Videos.Add(mediaVM);
+                        //int i = new Random().Next(0, files.Count - 1);
+                        //if (j < 5 && i <= (files.Count - 1) / 2)
+                        //{
+                        //    MediaRandom.Add(mediaVM);
+                        //    j++;
+                        //}
+                        MediaGroupedByAlphabet = Videos.OrderBy(x => x.AlphaKey).GroupBy(x => x.AlphaKey);
+                    });
                 }
             }
+            catch (Exception exception)
+            {
+                Debug.WriteLine("An error occured while indexing a video folder");
+            }
+            //#if WINDOWS_APP
+            //            }
+            //#endif
 
-            if (Media.Count > 0)
+            if (Videos.Count > 0)
             {
                 DispatchHelper.InvokeAsync(() => HasNoMedia = false);
             }
@@ -143,16 +209,14 @@ namespace VLC_WINRT_APP.ViewModels.VideoVM
                 DispatchHelper.InvokeAsync(() => HasNoMedia = true);
             }
         }
-        public void ExecuteSemanticZoom(SemanticZoom sZ, CollectionViewSource cvs)
-        {
-            (sZ.ZoomedOutView as ListViewBase).ItemsSource = cvs.View.CollectionGroups;
-        }
+
 
         private static async Task<IReadOnlyList<StorageFile>> GetMediaFromFolder(StorageFolder folder,
                                                                             CommonFileQuery query)
         {
             IReadOnlyList<StorageFile> files = null;
             StorageFileQueryResult fileQuery;
+#if WINDOWS_APP
             var queryOptions = new QueryOptions(query,
                                                new List<string>
                                                {
@@ -215,12 +279,15 @@ namespace VLC_WINRT_APP.ViewModels.VideoVM
                                                    ".wtv",
                                                    ".xesc",
                                                });
-
+#endif
             try
             {
+#if WINDOWS_APP
                 fileQuery = folder.CreateFileQueryWithOptions(queryOptions);
-
                 files = await fileQuery.GetFilesAsync();
+#endif
+
+                files = await folder.GetFilesAsync();
             }
             catch (Exception ex)
             {
@@ -236,5 +303,12 @@ namespace VLC_WINRT_APP.ViewModels.VideoVM
 
             return files;
         }
+
+        public void ExecuteSemanticZoom(SemanticZoom sZ, CollectionViewSource cvs)
+        {
+            (sZ.ZoomedOutView as ListViewBase).ItemsSource = cvs.View.CollectionGroups;
+        }
+
+        #endregion
     }
 }
