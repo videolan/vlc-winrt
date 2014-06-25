86a1d87ca2bfd62c37b58d7498e133d870fe22a9
Thomas Nigro <thomas.nigro@outlook.com>
2014-06-25 15:04:45 +0200

    ArtistPage now working.
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MusicPages/ArtistPage.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MusicPages/ArtistPage.xaml.cs
@@ -7,19 +7,8 @@
  * Refer to COPYING file of the official project for license
  **********************************************************************/
 
-using System;
-using System.Diagnostics;
-using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
-using VLC_WINRT_APP.Helpers;
-using VLC_WINRT_APP.Services.RunTime;
-using VLC_WINRT_APP.Views.MainPages;
-using WinRTXamlToolkit.AwaitableUI;
-using Album = VLC_WINRT_APP.Helpers.MusicLibrary.MusicEntities.Album;
-using Artist = VLC_WINRT_APP.Helpers.MusicLibrary.MusicEntities.Artist;
-
 namespace VLC_WINRT_APP.Views.MusicPages
 {
     /// <summary>
@@ -32,62 +21,9 @@ namespace VLC_WINRT_APP.Views.MusicPages
             this.InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        private void GoBack(object sender, RoutedEventArgs e)
         {
-            base.OnNavigatedTo(e);
-            FadeInPage.Begin();
-        }
-
-        private async void GoBack_Click(object sender, RoutedEventArgs e)
-        {
-            await FadeOutPage.BeginAsync();
-            App.ApplicationFrame.Navigate(typeof(MainPage));
-        }
-
-        private async void PopularItemGridView_OnItemClick(object sender, ItemClickEventArgs e)
-        {
-            // TODO: For snap view, send the user to the LastFM page. Otherwise, open a popup with the album info.
-            var topAlbum = e.ClickedItem as Album;
-            /* If, for whatever reason, the album clicked on is null or it does not have a url attached,
-             * return back to the view.*/
-            if (topAlbum == null)
-            {
-                Debug.WriteLine("TopAlbum is null.");
-                return;
-            }
-            if (string.IsNullOrEmpty(topAlbum.Url))
-            {
-                Debug.WriteLine("Album does not have a URL link out.");
-                return;
-            }
-            // LastFM does not append "http" to its URLs sometimes, which can cause Windows to throw an error.
-            // So let's check before if it has it or not, and if not we'll append it.
-            string appendHttp = !topAlbum.Url.Contains("http://") ? "http://" + topAlbum.Url : topAlbum.Url;
-            var launchUri = new Uri(appendHttp);
-            await Launcher.LaunchUriAsync(launchUri);
-        }
-
-        private async void SimilarArtistsGridView_OnItemClick(object sender, ItemClickEventArgs e)
-        {
-            // TODO: For snap view, send the user to the LastFM page. Otherwise, open a popup with the artist info.
-            var topArtist = e.ClickedItem as Artist;
-            /* If, for whatever reason, the album clicked on is null or it does not have a url attached,
-             * return back to the view.*/
-            if (topArtist == null)
-            {
-                Debug.WriteLine("TopArtist is null.");
-                return;
-            }
-            if (string.IsNullOrEmpty(topArtist.Url))
-            {
-                Debug.WriteLine("Artist does not have a URL link out.");
-                return;
-            }
-            // LastFM does not append "http" to its URLs sometimes, which can cause Windows to throw an error.
-            // So let's check before if it has it or not, and if not we'll append it.
-            string appendHttp = !topArtist.Url.Contains("http://") ? "http://" + topArtist.Url : topArtist.Url;
-            var launchUri = new Uri(appendHttp);
-            await Launcher.LaunchUriAsync(launchUri);
+            Frame.GoBack();
         }
     }
 }
