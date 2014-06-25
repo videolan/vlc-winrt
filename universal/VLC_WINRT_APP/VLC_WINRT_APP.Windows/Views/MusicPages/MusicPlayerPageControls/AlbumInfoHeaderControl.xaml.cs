5f87a68aec70968de4ed7610a3dae0830b3af0e5
Thomas Nigro <thomas.nigro@outlook.com>
2014-06-25 16:26:20 +0200

    Album cover size decrease in snap mode
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MusicPages/MusicPlayerPageControls/AlbumInfoHeaderControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace VLC_WINRT_APP.Views.MusicPages.MusicPlayerPageControls
+{
+    public sealed partial class AlbumInfoHeaderControl : UserControl
+    {
+        public AlbumInfoHeaderControl()
+        {
+            this.InitializeComponent();
+            Window.Current.SizeChanged += CurrentOnSizeChanged;
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Responsive();
+        }
+
+        private void CurrentOnSizeChanged(object sender, WindowSizeChangedEventArgs windowSizeChangedEventArgs)
+        {
+            Responsive();
+        }
+
+        void Responsive()
+        {
+            if (Window.Current.Bounds.Width < 400)
+            {
+                CoverImage.Height = 130;
+                CoverImage.Width = 130;
+                CoverImageColumnDefinition.Width = new GridLength(130);
+            }
+            else
+            {
+                CoverImage.Height = 200;
+                CoverImage.Width = 200;
+                CoverImageColumnDefinition.Width = new GridLength(200);
+            }
+        }
+    }
+}
