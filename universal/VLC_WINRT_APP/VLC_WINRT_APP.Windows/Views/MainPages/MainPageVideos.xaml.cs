1471fdb7fc77e450d5a4bc9d139e590f156408cb
Thomas Nigro <thomas.nigro@outlook.com>
2014-07-26 22:39:24 +0200

    Responsive design for VideoItems

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MainPages/MainPageVideos.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MainPages/MainPageVideos.xaml.cs
@@ -7,6 +7,7 @@
  * Refer to COPYING file of the official project for license
  **********************************************************************/
 
+using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
@@ -36,13 +37,64 @@ namespace VLC_WINRT_APP.Views.MainPages
         {
             if (Window.Current.Bounds.Width < 400)
             {
-                RootGrid.Margin = new Thickness(9,0,0,0);
+                RootGrid.Margin = new Thickness(9, 0, 0, 0);
                 FirstRowDefinition.Height = new GridLength(0);
             }
             else
             {
                 RootGrid.Margin = new Thickness(24, 0, 0, 0);
                 FirstRowDefinition.Height = new GridLength(42);
+            }
+        }
+
+        private void ItemsWrapGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Responsive(sender as ItemsWrapGrid);
+        }
+
+        private void ItemsWrapGrid_Loaded(object sender, SizeChangedEventArgs e)
+        {
+            Responsive(sender as ItemsWrapGrid);
+        }
+
+        void Responsive(ItemsWrapGrid itemsWrap)
+        {
+            int usefulWidth = (int)Window.Current.Bounds.Width;
+            Debug.WriteLine(usefulWidth);
+            int sidebar;
+            if (Window.Current.Bounds.Width < 400)
+            {
+                sidebar = 20;
+            }
+            else if (Window.Current.Bounds.Width < 1080)
+            {
+                sidebar = 130;
+            }
+            else
+            {
+                sidebar = 390;
+            }
+            usefulWidth -= sidebar;
+
+            if (usefulWidth < 400)
+            {
+                itemsWrap.ItemWidth = usefulWidth;
+                itemsWrap.ItemHeight = usefulWidth * 0.561;
+            }
+            else if (usefulWidth < 890)
+            {
+                itemsWrap.ItemWidth = usefulWidth / 2;
+                itemsWrap.ItemHeight = (usefulWidth / 2) * 0.561;
+            }
+            else if (usefulWidth < 1300)
+            {
+                itemsWrap.ItemWidth = usefulWidth / 3;
+                itemsWrap.ItemHeight = (usefulWidth / 3) * 0.561;
+            }
+            else
+            {
+                itemsWrap.ItemWidth = usefulWidth / 4;
+                itemsWrap.ItemHeight = (usefulWidth / 4) * 0.561;
             }
         }
     }
