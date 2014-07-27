27c2eb14920cb9aec2d4e76c19f5a5c9249dbffa
Thomas Nigro <thomas.nigro@outlook.com>
2014-07-27 17:11:08 +0200

    Add missing files
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/MainPages/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using VLC_WINRT_APP.Services.Interface;
+using VLC_WINRT_APP.Services.RunTime;
+
+namespace VLC_WINRT_APP.Views.MainPages
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly VlcService _vlcService;
+        public MainPage(VlcService vlcService, IMediaService mediaService)
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Enabled;
+            _vlcService = vlcService;
+            //(mediaService as MediaService).SetMediaElement(FoudationMediaElement);
+            Loaded += SwapPanelLoaded;
+        }
+        private async void SwapPanelLoaded(object sender, RoutedEventArgs e)
+        {
+            await _vlcService.Initialize(SwapChainPanel);
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseCoreWindow);
+        }
+    }
+}
