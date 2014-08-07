d1c7166c815d2fc68c01cee74a10d409a1f5e738
Sébastien Thevenin <cybeau@gmail.com>
2014-08-07 14:16:11 +0000

    Update windows phone projects to fix compilation errors in Win32 target

--- modules/libdirect2d_winrt_plugin.WindowsPhone/pch.h
+++ modules/libdirect2d_winrt_plugin.WindowsPhone/pch.h
@@ -2,3 +2,10 @@
 
 #include <collection.h>
 #include <ppltasks.h>
+#include <wrl/client.h>
+
+#include <DirectXMath.h>
+#include "inttypes.h"
+#include <D2d1.h>
+#include <D2d1_1.h>
+#include <DXGI1_2.h>
--- wrapper/libVLCX.WindowsPhone/pch.h
+++ wrapper/libVLCX.WindowsPhone/pch.h
@@ -1,4 +1,12 @@
 ﻿#pragma once
 
-#include <collection.h>
-#include <ppltasks.h>
+
+#include <wrl.h>
+#include <wrl/client.h>
+
+#include <dxgi.h>
+#include <dxgi1_2.h>
+#include <d3d11_1.h>
+#include <d2d1_1.h>
+
+#include "windows.ui.xaml.media.dxinterop.h"
