0c985f4f0b0ffe2d69725282e5af4d3e3c2ed5e8
Thomas Nigro <tn@thomasnigro.fr>
2014-08-22 22:54:24 +0200

    Speed slider has a tooltip
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/DoubleSpeedToUnderstandableSpeedConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace VLC_WINRT_APP.Converters
+{
+    public class DoubleSpeedToUnderstandableSpeedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return "x" + (double)value / 100;
+
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
