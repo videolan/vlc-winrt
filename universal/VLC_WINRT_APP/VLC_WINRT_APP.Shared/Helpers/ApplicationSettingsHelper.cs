e80bf91f6794274d6839f150ba938aba3dea51d2
Thomas Nigro <thomas.nigro@outlook.com>
2014-09-02 13:48:43 +0200

    Add ApplicationSettings helper
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Helpers/ApplicationSettingsHelper.cs
@@ -0,0 +1,53 @@
+﻿﻿/**********************************************************************
+ * VLC for WinRT
+ **********************************************************************
+ * Copyright © 2013-2014 VideoLAN and Authors
+ *
+ * Licensed under GPLv2 and MPLv2
+ * Refer to COPYING file of the official project for license
+ **********************************************************************/
+
+using System.Diagnostics;
+using Windows.Storage;
+
+namespace VLC_WINRT_APP.Helpers
+{
+    static class ApplicationSettingsHelper
+    {
+        /// <summary>
+        /// Function to read a setting value and clear it after reading it
+        /// </summary>
+        public static object ReadResetSettingsValue(string key)
+        {
+            Debug.WriteLine(key);
+            if (!ApplicationData.Current.LocalSettings.Values.ContainsKey(key))
+            {
+                Debug.WriteLine("null returned");
+                return null;
+            }
+            else
+            {
+                var value = ApplicationData.Current.LocalSettings.Values[key];
+                ApplicationData.Current.LocalSettings.Values.Remove(key);
+                Debug.WriteLine("value found " + value.ToString());
+                return value;
+            }
+        }
+
+        /// <summary>
+        /// Save a key value pair in settings. Create if it doesn't exist
+        /// </summary>
+        public static void SaveSettingsValue(string key, object value)
+        {
+            Debug.WriteLine(key + ":" + value.ToString());
+            if (!ApplicationData.Current.LocalSettings.Values.ContainsKey(key))
+            {
+                ApplicationData.Current.LocalSettings.Values.Add(key, value);
+            }
+            else
+            {
+                ApplicationData.Current.LocalSettings.Values[key] = value;
+            }
+        }
+    }
+}
