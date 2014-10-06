73601d2a85074c8a2bde003ed866089987bb639f
Thomas Nigro <thomas.nigro@outlook.com>
2014-10-06 16:09:26 +0200

    Better navigation in MusicPage
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MainPages/MainPageMusic.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MainPages/MainPageMusic.xaml.cs
@@ -19,6 +19,7 @@ using Windows.UI.Xaml.Navigation;
 using VLC_WINRT_APP.Helpers;
 using VLC_WINRT_APP.Model;
 using VLC_WINRT_APP.ViewModels;
+using VLC_WINRT_APP.Views.MainPages.MusicPanes;
 
 namespace VLC_WINRT_APP.Views.MainPages
 {
@@ -54,24 +55,8 @@ namespace VLC_WINRT_APP.Views.MainPages
             }
         }
 
-        private void SemanticZoom_OnViewChangeCompletedArtistByName(object sender, SemanticZoomViewChangedEventArgs e)
-        {
-            Locator.MusicLibraryVM.ExecuteSemanticZoom(SemanticZoomAlbumsByArtist, AlbumsGroupedByName);
-        }
-
-        private void SemanticZoom_OnViewChangeCompletedCurrentArtistAlbum(object sender, SemanticZoomViewChangedEventArgs e)
-        {
-            if (e.IsSourceZoomedInView == false)
-            {
-                e.DestinationItem.Item = e.SourceItem.Item;
-            }
-        }
 
 
-        private void Header_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            SemanticZoomAlbumsByArtist.IsZoomedInViewActive = false;
-        }
 
         private void Panels_OnItemClick(object sender, ItemClickEventArgs e)
         {
@@ -82,54 +67,33 @@ namespace VLC_WINRT_APP.Views.MainPages
             }
             panel.Color = App.Current.Resources["MainColor"] as SolidColorBrush;
 
-            if (!ArtistListView.Items.Any()) 
-                return;
             switch (panel.Index)
             {
                 case 0:
-                    SemanticZoomAlbumsByArtist.Visibility = Visibility.Visible;
-                    ArtistsGrid.Visibility = Visibility.Collapsed;
-                    RadDataGrid.Visibility = Visibility.Collapsed;
+                    if(MusicPanesFrame.CurrentSourcePageType != typeof(ArtistsSemanticZoom))
+                        MusicPanesFrame.Navigate(typeof (ArtistsSemanticZoom));
                     FavoriteAlbumsGridView.Visibility = Visibility.Collapsed;
                     break;
                 case 1:
-                    SemanticZoomAlbumsByArtist.Visibility = Visibility.Collapsed;
-                    ArtistsGrid.Visibility = Visibility.Visible;
-                    RadDataGrid.Visibility = Visibility.Collapsed;
+                    if (MusicPanesFrame.CurrentSourcePageType != typeof(ArtistAlbumsTwoPanes))
+                        MusicPanesFrame.Navigate(typeof(ArtistAlbumsTwoPanes));
                     FavoriteAlbumsGridView.Visibility = Visibility.Collapsed;
-                    if (CapabilitiesHelper.IsTouchCapable)
-                    {
-                        Locator.MusicLibraryVM.IsMainPageMusicArtistAlbumsSemanticZoomViewedIn = false;
-                    }
-                    if (Locator.MusicPlayerVM.IsPlaying
-                        && Locator.MusicPlayerVM.PlayingType == PlayingType.Music
-                        && Locator.MusicPlayerVM.CurrentPlayingArtist != null)
-                    {
-                        ArtistListView.SelectedItem =
-                            Locator.MusicLibraryVM.Artists.FirstOrDefault(
-                                x => x.Name == Locator.MusicPlayerVM.CurrentPlayingArtist.Name);
-                        ArtistListView.ScrollIntoView(ArtistListView.SelectedItem);
-                    }
-                    if (ArtistListView.SelectedIndex == -1
-                        && Window.Current.Bounds.Width > 800)
-                    {
-                        ArtistListView.SelectedIndex = 0;
-                    }
-                    App.RootPage.ColumnGrid.MinimizeSidebar();
                     break;
                 case 2:
-                    SemanticZoomAlbumsByArtist.Visibility = Visibility.Collapsed;
-                    ArtistsGrid.Visibility = Visibility.Collapsed;
-                    RadDataGrid.Visibility = Visibility.Visible;
+                    if (MusicPanesFrame.CurrentSourcePageType != typeof(SongsListView))
+                        MusicPanesFrame.Navigate(typeof(SongsListView));
                     FavoriteAlbumsGridView.Visibility = Visibility.Collapsed;
                     break;
                 case 3:
-                    SemanticZoomAlbumsByArtist.Visibility = Visibility.Collapsed;
-                    ArtistsGrid.Visibility = Visibility.Collapsed;
-                    RadDataGrid.Visibility = Visibility.Collapsed;
                     FavoriteAlbumsGridView.Visibility = Visibility.Visible;
                     break;
             }
+        }
+
+        private void MusicPanesFrame_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if(MusicPanesFrame.CurrentSourcePageType == null)
+                MusicPanesFrame.Navigate(typeof (ArtistsSemanticZoom));
         }
     }
 }
---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MainPages/MusicPanes/ArtistAlbumsTwoPanes.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VLC_WINRT_APP.Helpers;
+using VLC_WINRT_APP.Model;
+using VLC_WINRT_APP.ViewModels;
+
+namespace VLC_WINRT_APP.Views.MainPages.MusicPanes
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class ArtistAlbumsTwoPanes : Page
+    {
+        public ArtistAlbumsTwoPanes()
+        {
+            this.InitializeComponent();
+        }
+        private void SemanticZoom_OnViewChangeCompletedCurrentArtistAlbum(object sender, SemanticZoomViewChangedEventArgs e)
+        {
+            if (e.IsSourceZoomedInView == false)
+            {
+                e.DestinationItem.Item = e.SourceItem.Item;
+            }
+        }
+        protected override void OnNavigatedTo(Windows.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (CapabilitiesHelper.IsTouchCapable)
+            {
+                Locator.MusicLibraryVM.IsMainPageMusicArtistAlbumsSemanticZoomViewedIn = false;
+            }
+            if (Locator.MusicPlayerVM.IsPlaying
+                && Locator.MusicPlayerVM.PlayingType == PlayingType.Music
+                && Locator.MusicPlayerVM.CurrentPlayingArtist != null)
+            {
+                ArtistListView.SelectedItem =
+                    Locator.MusicLibraryVM.Artists.FirstOrDefault(
+                        x => x.Name == Locator.MusicPlayerVM.CurrentPlayingArtist.Name);
+                ArtistListView.ScrollIntoView(ArtistListView.SelectedItem);
+            }
+            if (ArtistListView.SelectedIndex == -1
+                && Window.Current.Bounds.Width > 800)
+            {
+                if(ArtistListView.Items != null && ArtistListView.Items.Count > 0)
+                    ArtistListView.SelectedIndex = 0;
+            }
+            App.RootPage.ColumnGrid.MinimizeSidebar();
+        }
+    }
+}
