2b9cf1c8072a229cf7c96a60b69990ca5f81090c
Thomas Nigro <thomas.nigro@outlook.com>
2014-10-13 14:32:47 +0200

    Add music pages
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/MusicPages/ArtistsPivotItem.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace VLC_WINRT_APP.Views.MusicPages
+{
+    public sealed partial class ArtistsPivotItem : UserControl
+    {
+        public ArtistsPivotItem()
+        {
+            this.InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            this.Unloaded += OnUnloaded;
+            this.SizeChanged += OnSizeChanged;
+            Responsive();
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs)
+        {
+            Responsive();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            this.SizeChanged -= OnSizeChanged;
+        }
+
+        void Responsive()
+        {
+            ArtistListViewColumnDefinition.Width = new GridLength(Window.Current.Bounds.Width, GridUnitType.Pixel);
+        }
+
+        private void SemanticZoom_OnViewChangeCompletedCurrentArtistAlbum(object sender, SemanticZoomViewChangedEventArgs e)
+        {
+            if (e.IsSourceZoomedInView == false)
+            {
+                e.DestinationItem.Item = e.SourceItem.Item;
+            }
+        }
+    }
+}
