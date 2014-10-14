22bd89fe78dbee025699f01e39a02fd4171355e7
Thomas Nigro <thomas.nigro@outlook.com>
2014-10-14 15:59:17 +0200

    AlbumPage designed, Music page currently designed. Modifications on controls for a better WP UX
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/MusicPages/AlbumPage.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/MusicPages/AlbumPage.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using Windows.Phone.UI.Input;
+using Windows.UI.Core;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using WinRTXamlToolkit.Controls.Extensions;
 
 namespace VLC_WINRT_APP.Views.MusicPages
 {
@@ -28,6 +32,18 @@ namespace VLC_WINRT_APP.Views.MusicPages
         {
             App.ApplicationFrame.GoBack();
             backPressedEventArgs.Handled = true;
+        }
+
+        private void PlaylistView_Loaded(object sender, RoutedEventArgs e)
+        {
+            ScrollViewer sV = (sender as ListView).GetFirstDescendantOfType<ScrollViewer>();
+            sV.ViewChanging += (o, args) =>
+            {
+                if (args.NextView.VerticalOffset > 50 && FadeOutHeader.GetCurrentState() == ClockState.Stopped)
+                {
+                    Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => FadeOutHeader.Begin());
+                }
+            };
         }
     }
 }
---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/TopBar.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace VLC_WINRT_APP.Views.UserControls
+{
+    public sealed partial class TopBar : UserControl
+    {
+        public TopBar()
+        {
+            this.InitializeComponent();
+            this.Loaded += (sender, args) => this.DataContext = this;
+        }
+
+        public static readonly DependencyProperty HeaderTextBlockProperty = DependencyProperty.Register(
+            "HeaderTextBlock", typeof (String), typeof (TopBar), new PropertyMetadata(default(String), HeaderTextBlockPropertyChangedCallback));
+
+        private static void HeaderTextBlockPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            (dependencyObject as TopBar).HeaderTextBlock = dependencyPropertyChangedEventArgs.NewValue.ToString();
+        }
+
+        public String HeaderTextBlock
+        {
+            get { return (String) GetValue(HeaderTextBlockProperty); }
+            set
+            {
+                SetValue(HeaderTextBlockProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty HeaderBackgroundProperty = DependencyProperty.Register(
+            "HeaderBackground", typeof (SolidColorBrush), typeof (TopBar), new PropertyMetadata(App.Current.Resources["MainColor"], HeaderBackgroundPropertyChangedCallback));
+
+        private static void HeaderBackgroundPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            (dependencyObject as TopBar).HeaderBackground = ((SolidColorBrush)dependencyPropertyChangedEventArgs.NewValue);
+        }
+
+        public SolidColorBrush HeaderBackground
+        {
+            get { return (SolidColorBrush)GetValue(HeaderBackgroundProperty); }
+            set
+            {
+                SetValue(HeaderBackgroundProperty, value);
+                
+            }
+        }
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+            App.RootPage.PanelsView.ShowSidebar();
+        }
+    }
+}
