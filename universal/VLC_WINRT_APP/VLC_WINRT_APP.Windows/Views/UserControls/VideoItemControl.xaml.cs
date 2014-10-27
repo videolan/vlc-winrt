a762a0ea8551487d9f599df0db3c8f749049f7cc
Thomas Nigro <tn@thomasnigro.fr>
2014-10-27 15:08:47 +0100

    Update Video item layout
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/UserControls/VideoItemControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+
+namespace VLC_WINRT_APP.Views.UserControls
+{
+    public sealed partial class VideoItemControl : UserControl
+    {
+        public VideoItemControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Grid_Holding(object sender, HoldingRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+
+        private void Grid_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+    }
+}
