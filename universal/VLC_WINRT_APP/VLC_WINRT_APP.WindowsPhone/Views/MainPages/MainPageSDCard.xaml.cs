ef32abbe8cc998429dbaf5c87e1fdd7ff95b78e2
Thomas Nigro <tn@thomasnigro.fr>
2014-11-18 18:55:30 +0100

    WP Add SD Card navigation
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/MainPages/MainPageSDCard.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.Phone.UI.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using VLC_WINRT_APP.ViewModels;
+
+namespace VLC_WINRT_APP.Views.MainPages
+{
+    public sealed partial class MainPageSDCard : Page
+    {
+        public MainPageSDCard()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            HardwareButtons.BackPressed += HardwareButtonsOnBackPressed;
+        }
+
+        private void HardwareButtonsOnBackPressed(object sender, BackPressedEventArgs backPressedEventArgs)
+        {
+                backPressedEventArgs.Handled = true;
+            if (Locator.ExternalStorageVM.CurrentStorageVM.CanGoBack)
+            {
+                Locator.ExternalStorageVM.CurrentStorageVM.GoBackCommand.Execute(null);
+            }
+            else
+            {
+                App.ApplicationFrame.Navigate(typeof (MainPageHome));
+            }
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            HardwareButtons.BackPressed -= HardwareButtonsOnBackPressed;
+        }
+    }
+}
