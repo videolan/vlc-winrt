4b16842e68218e07258f8b3c3475a00df9516ff1
Thomas Nigro <tn@thomasnigro.fr>
2014-11-24 18:22:13 +0100

    Add StatusBar helpers and sandwich button path in Styles
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Helpers/StatusBar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Media;
+
+namespace VLC_WINRT_APP.Helpers
+{
+#if WINDOWS_PHONE_APP
+    public static class StatusBarHelper
+    {
+        public static void Default()
+        {
+            Set(App.Current.Resources["MainColor"] as SolidColorBrush, 0, "VLC", ApplicationViewBoundsMode.UseCoreWindow);
+        }
+
+        public static void SetTransparent()
+        {
+            Set(null, 0, "VLC", ApplicationViewBoundsMode.UseCoreWindow);
+        }
+
+        static void Set(SolidColorBrush background, double opacity, string text, ApplicationViewBoundsMode boundsMode)
+        {
+            StatusBar sB = StatusBar.GetForCurrentView();
+            if (background != null) 
+                sB.BackgroundColor = background.Color;
+            sB.BackgroundOpacity = opacity;
+            sB.ProgressIndicator.ShowAsync();
+            if (text != null) sB.ProgressIndicator.Text = text;
+            var appView = ApplicationView.GetForCurrentView();
+            appView.SetDesiredBoundsMode(boundsMode);
+
+        }
+    }
+#endif
+}
