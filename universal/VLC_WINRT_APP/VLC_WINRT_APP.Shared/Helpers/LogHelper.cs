b77282b4d57d6ea405e72d15b13d0b16537dd936
Thomas Nigro <tn@thomasnigro.fr>
2014-11-24 17:44:37 +0100

    Add basic loghelper
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Helpers/LogHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace VLC_WINRT_APP.Helpers
+{
+    public static class LogHelper
+    {
+        public static void Log(object o)
+        {
+            Debug.WriteLine(o.ToString());
+        }
+    }
+}
