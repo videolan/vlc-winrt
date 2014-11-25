b6fbf247cdbfd4195b378c3bfe76443c989ebfc1
Thomas Nigro <tn@thomasnigro.fr>
2014-11-25 18:06:40 +0100

    WP MainVM allows appbarcommands bindings
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/ViewModels/MainVM.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/ViewModels/MainVM.cs
@@ -7,16 +7,20 @@
  * Refer to COPYING file of the official project for license
  **********************************************************************/
 
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Resources;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using VLC_WINRT.Common;
 using VLC_WINRT_APP.Commands.MainPageCommands;
 using VLC_WINRT_APP.Common;
 using VLC_WINRT_APP.Helpers;
-using VLC_WINRT_APP.Model;
 using VLC_WINRT_APP.Commands;
+using VLC_WINRT_APP.Views.MainPages;
+using Panel = VLC_WINRT_APP.Model.Panel;
 
 namespace VLC_WINRT_APP.ViewModels
 {
@@ -31,12 +35,26 @@ namespace VLC_WINRT_APP.ViewModels
         private GoToThanksPageCommand _goToThanksPageCommand;
         private GoToAboutPageCommand _goToAboutPageCommand;
         private AlwaysExecutableCommand _openSidebarCommand;
-
+#if WINDOWS_PHONE_APP
+        private ChangeMainPageMusicViewCommand _changeMainPageMusicViewCommand;
+#endif
         #endregion
         #region public fields
+#if WINDOWS_PHONE_APP
+        public IObservableVector<ICommandBarElement> AppBarElements
+        {
+            get
+            {
+                if (App.ApplicationFrame != null && App.ApplicationFrame.CurrentSourcePageType == typeof(MainPageHome))
+                {
+                    return ((App.ApplicationFrame.Content as MainPageHome).BottomAppBar as CommandBar).PrimaryCommands;
+                }
+                return null;
+            }
+        }
+#endif
         #endregion
         #region public props
-
         public GoToPanelCommand GoToPanelCommand
         {
             get { return _goToPanelCommand; }
@@ -61,6 +79,11 @@ namespace VLC_WINRT_APP.ViewModels
             set { SetProperty(ref _goToAboutPageCommand, value); }
         }
 #if WINDOWS_PHONE_APP
+        public ChangeMainPageMusicViewCommand ChangeMainPageMusicViewCommand
+        {
+            get { return _changeMainPageMusicViewCommand; }
+            set { SetProperty(ref _changeMainPageMusicViewCommand, value); }
+        }
         public AlwaysExecutableCommand OpenSidebarCommand
         {
             get { return _openSidebarCommand; }
@@ -81,6 +104,7 @@ namespace VLC_WINRT_APP.ViewModels
             {
                 App.RootPage.PanelsView.ShowSidebar();
             });
+            ChangeMainPageMusicViewCommand = new ChangeMainPageMusicViewCommand();
 #endif
             // TODO: For Windows 8.1 build, use ResourceLoader.GetForCurrentView(); 
 
@@ -91,10 +115,11 @@ namespace VLC_WINRT_APP.ViewModels
             string removableName = "";
 #if WINDOWS_APP
             removableName = resourceLoader.GetString("RemovableStorage");
-#else
-            removableName = resourceLoader.GetString("SdCard");
-#endif
             Panels.Add(new Panel(removableName, 3, 0.4, App.Current.Resources["RemovablesPath"].ToString()));
+#else
+            //removableName = resourceLoader.GetString("SdCard");
+            Panels.Add(new Panel("more", 3, 0.4, App.Current.Resources["HamburgerPath"].ToString()));
+#endif
 #if WINDOWS_APP
             Panels.Add(new Panel(resourceLoader.GetString("MediaServers"), 4, 0.4, App.Current.Resources["ServerPath"].ToString()));
 #endif
@@ -107,6 +132,52 @@ namespace VLC_WINRT_APP.ViewModels
                 ApplicationSettingsHelper.SaveSettingsValue("ContinueVideoPlaybackInBackground", true);
         }
 
+#if WINDOWS_PHONE_APP
+        public void UpdateAppBar(int index)
+        {
+            if (AppBarElements == null) return;
+            AppBarElements.Clear();
+            switch (index)
+            {
+                case 0:
+                    AppBarElements.Add(new AppBarButton()
+                    {
+                        Label = "search",
+                        Icon = new SymbolIcon(Symbol.Find),
+                        Command = OpenSidebarCommand
+                    });
+                    AppBarElements.Add(new AppBarButton()
+                    {
+                        Label = "open file",
+                        Icon = new SymbolIcon(Symbol.OpenFile),
+                        Command = Locator.VideoLibraryVM.OpenVideo
+                    });
+                    AppBarElements.Add(new AppBarButton()
+                    {
+                        Label = "open stream",
+                        Icon = new SymbolIcon(Symbol.World),
+                        Flyout = App.Current.Resources["PhoneOpenStreamFlyout"] as Flyout,
+                    });
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    AppBarElements.Add(new AppBarButton()
+                    {
+                        Label = "view all",
+                        Icon = new SymbolIcon(Symbol.ViewAll),
+                        Flyout = App.Current.Resources["PhoneChangeMusicViewFlyout"] as MenuFlyout,
+                    });
+                    AppBarElements.Add(new AppBarButton()
+                    {
+                        Label = "random",
+                        Icon = new SymbolIcon(Symbol.Shuffle),
+                        Command = Locator.MusicLibraryVM.PlayAllRandomCommand
+                    });
+                    break;
+            }
+        }
+#endif
         public ObservableCollection<Panel> Panels
         {
             get { return _panels; }
