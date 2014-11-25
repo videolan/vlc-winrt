767489adce7f954f066e1535d3f933c458237649
Thomas Nigro <tn@thomasnigro.fr>
2014-11-25 18:02:21 +0100

    WP Add some new methods in StatusBarHelper
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Helpers/StatusBar.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Helpers/StatusBar.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Windows.UI.ViewManagement;
+﻿using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Media;
 
 namespace VLC_WINRT_APP.Helpers
@@ -9,21 +6,31 @@ namespace VLC_WINRT_APP.Helpers
 #if WINDOWS_PHONE_APP
     public static class StatusBarHelper
     {
+        private static string title = "VLC Beta";
         public static void Default()
         {
-            Set(App.Current.Resources["MainColor"] as SolidColorBrush, 0, "VLC", ApplicationViewBoundsMode.UseCoreWindow);
+            Set(App.Current.Resources["MainColor"] as SolidColorBrush, null, 1, title, ApplicationViewBoundsMode.UseVisible);
         }
 
         public static void SetTransparent()
         {
-            Set(null, 0, "VLC", ApplicationViewBoundsMode.UseCoreWindow);
+            Set(null, null, 0, title, ApplicationViewBoundsMode.UseCoreWindow);
         }
 
-        static void Set(SolidColorBrush background, double opacity, string text, ApplicationViewBoundsMode boundsMode)
+        public static void SetForeground(SolidColorBrush color)
+        {
+            StatusBar sB = StatusBar.GetForCurrentView();
+            if (color != null) sB.ForegroundColor = color.Color;
+        }
+
+        static void Set(SolidColorBrush background, SolidColorBrush foreground, double opacity, string text, ApplicationViewBoundsMode boundsMode)
         {
             StatusBar sB = StatusBar.GetForCurrentView();
             if (background != null) 
                 sB.BackgroundColor = background.Color;
+            if (foreground != null)
+                sB.ForegroundColor = foreground.Color;
+
             sB.BackgroundOpacity = opacity;
             var _ = sB.ProgressIndicator.ShowAsync();
             if (text != null) sB.ProgressIndicator.Text = text;
