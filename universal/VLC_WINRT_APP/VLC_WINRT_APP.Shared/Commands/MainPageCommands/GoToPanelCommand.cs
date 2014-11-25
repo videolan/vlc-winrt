d230b32cb40c8eeb3eaa8598d2509f62e71bb71f
Thomas Nigro <tn@thomasnigro.fr>
2014-11-25 18:01:42 +0100

    WP Update navigation behaviour, color of selected (current) page icon, add SelectionChangedEventArgs management
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/MainPageCommands/GoToPanelCommand.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/MainPageCommands/GoToPanelCommand.cs
@@ -7,11 +7,14 @@
  * Refer to COPYING file of the official project for license
  **********************************************************************/
 
+using System;
 using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Media;
 using VLC_WINRT.Common;
+using VLC_WINRT_APP.Model;
 using VLC_WINRT_APP.ViewModels;
 using VLC_WINRT_APP.Views.MainPages;
 
@@ -21,55 +24,68 @@ namespace VLC_WINRT_APP.Commands.MainPageCommands
     public class GoToPanelCommand : AlwaysExecutableCommand
     {
 #if WINDOWS_APP
-        public static readonly SolidColorBrush SelectedColorBrush = App.Current.Resources["MainColor"] as SolidColorBrush;
+        public static readonly SolidColorBrush SelectedColorBrush =
+            App.Current.Resources["MainColor"] as SolidColorBrush;
+
         public static readonly SolidColorBrush DefaultColorBrush = new SolidColorBrush(Colors.DimGray);
 #else
-//        public static  readonly SolidColorBrush SelectedColorBrush = new SolidColorBrush(Colors.WhiteSmoke);
-//        public static readonly SolidColorBrush DefaultColorBrush = new SolidColorBrush(Color.FromArgb(110, 0, 0, 0));
+        //public static readonly SolidColorBrush SelectedColorBrush = new SolidColorBrush(Colors.WhiteSmoke);
+        //public static readonly SolidColorBrush DefaultColorBrush = new SolidColorBrush(Color.FromArgb(70, 0, 0, 0));
         public static readonly SolidColorBrush SelectedColorBrush = App.Current.Resources["MainColor"] as SolidColorBrush;
         public static readonly SolidColorBrush DefaultColorBrush = new SolidColorBrush(Colors.DimGray);
 #endif
-        public override void Execute(object parameter)
+
+        public override async void Execute(object parameter)
         {
             App.Transition.Edge = EdgeTransitionLocation.Bottom;
-            Model.Panel panel = (parameter as ItemClickEventArgs).ClickedItem as Model.Panel;
+            Model.Panel panel = null;
+            if (parameter is ItemClickEventArgs)
+            {
+                panel = (parameter as ItemClickEventArgs).ClickedItem as Model.Panel;
+            }
+            else if (parameter is SelectionChangedEventArgs)
+            {
+                panel = (parameter as SelectionChangedEventArgs).AddedItems[0] as Model.Panel;
+            }
             foreach (Model.Panel panel1 in Locator.MainVM.Panels)
                 panel1.Color = DefaultColorBrush;
             panel.Color = SelectedColorBrush;
+#if WINDOWS_APP
             switch (panel.Index)
             {
                 case 0:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof(MainPageHome))
-                        App.ApplicationFrame.Navigate(typeof(MainPageHome));
+                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageHome))
+                        App.ApplicationFrame.Navigate(typeof (MainPageHome));
                     break;
                 case 1:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof(MainPageVideos))
-                        App.ApplicationFrame.Navigate(typeof(MainPageVideos));
+                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageVideos))
+                        App.ApplicationFrame.Navigate(typeof (MainPageVideos));
                     break;
                 case 2:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof(MainPageMusic))
-                        App.ApplicationFrame.Navigate(typeof(MainPageMusic));
+                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageMusic))
+                        App.ApplicationFrame.Navigate(typeof (MainPageMusic));
                     break;
-#if WINDOWS_PHONE_APP
                 case 3:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageSDCard))
-                        App.ApplicationFrame.Navigate(typeof (MainPageSDCard));
-                    break;
-#endif
-#if WINDOWS_APP
-                case 3:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof(MainPageRemovables))
-                        App.ApplicationFrame.Navigate(typeof(MainPageRemovables));
+                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageRemovables))
+                        App.ApplicationFrame.Navigate(typeof (MainPageRemovables));
                     break;
                 case 4:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof(MainPageMediaServers))
-                        App.ApplicationFrame.Navigate(typeof(MainPageMediaServers));
+                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageMediaServers))
+                        App.ApplicationFrame.Navigate(typeof (MainPageMediaServers));
                     break;
-#endif
             }
+#else
+
+            if (App.ApplicationFrame.CurrentSourcePageType != typeof(MainPageHome))
+                App.ApplicationFrame.Navigate(typeof(MainPageHome));
+            await App.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            (App.ApplicationFrame.Content as MainPageHome).MainPivot.SelectedIndex = panel.Index);
+
+#endif
 #if WINDOWS_PHONE_APP
             App.RootPage.PanelsView.HideSidebar();
 #endif
+
         }
     }
 }
