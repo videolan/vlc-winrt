36b45e893ca891af9f21e784801350fa5897c3fc
Thomas Nigro <tn@thomasnigro.fr>
2014-11-26 16:01:10 +0100

    WP Remove useless dependencies
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/BottomMiniPlayer.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/BottomMiniPlayer.xaml.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// Pour en savoir plus sur le modèle d'élément Contrôle utilisateur, consultez la page http://go.microsoft.com/fwlink/?LinkId=234236
+using VLC_WINRT_APP.ViewModels;
 
 namespace VLC_WINRT_APP.Views.UserControls
 {
@@ -22,6 +9,11 @@ namespace VLC_WINRT_APP.Views.UserControls
         public BottomMiniPlayer()
         {
             this.InitializeComponent();
+        }
+
+        private void RootMiniPlayer_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Locator.MusicPlayerVM.GoToMusicPlayerPage.Execute(null);
         }
     }
 }
