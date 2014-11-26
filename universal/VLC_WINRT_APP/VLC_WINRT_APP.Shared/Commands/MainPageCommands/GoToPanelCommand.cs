0a949609d2da990259eef2fcfcc209dfcbbe7fbc
Thomas Nigro <tn@thomasnigro.fr>
2014-11-26 01:40:58 +0100

    WP Fix bracket closure typo
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/MainPageCommands/GoToPanelCommand.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/MainPageCommands/GoToPanelCommand.cs
@@ -58,28 +58,29 @@ namespace VLC_WINRT_APP.Commands.MainPageCommands
             {
                 panel.Color = SelectedColorBrush;
 #if WINDOWS_APP
-            switch (panel.Index)
-            {
-                case 0:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageHome))
-                        App.ApplicationFrame.Navigate(typeof (MainPageHome));
-                    break;
-                case 1:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageVideos))
-                        App.ApplicationFrame.Navigate(typeof (MainPageVideos));
-                    break;
-                case 2:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageMusic))
-                        App.ApplicationFrame.Navigate(typeof (MainPageMusic));
-                    break;
-                case 3:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageRemovables))
-                        App.ApplicationFrame.Navigate(typeof (MainPageRemovables));
-                    break;
-                case 4:
-                    if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageMediaServers))
-                        App.ApplicationFrame.Navigate(typeof (MainPageMediaServers));
-                    break;
+                switch (panel.Index)
+                {
+                    case 0:
+                        if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageHome))
+                            App.ApplicationFrame.Navigate(typeof (MainPageHome));
+                        break;
+                    case 1:
+                        if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageVideos))
+                            App.ApplicationFrame.Navigate(typeof (MainPageVideos));
+                        break;
+                    case 2:
+                        if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageMusic))
+                            App.ApplicationFrame.Navigate(typeof (MainPageMusic));
+                        break;
+                    case 3:
+                        if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageRemovables))
+                            App.ApplicationFrame.Navigate(typeof (MainPageRemovables));
+                        break;
+                    case 4:
+                        if (App.ApplicationFrame.CurrentSourcePageType != typeof (MainPageMediaServers))
+                            App.ApplicationFrame.Navigate(typeof (MainPageMediaServers));
+                        break;
+                }
             }
 #else
 
