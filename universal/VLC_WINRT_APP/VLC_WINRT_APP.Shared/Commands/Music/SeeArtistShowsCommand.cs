37c7d8f0e2995f3c4cf7932b7a891444c2f4d77a
Thomas Nigro <tn@thomasnigro.fr>
2014-12-02 18:50:25 +0100

    User can discover shows from any artist (dynamic datacontext)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/Music/SeeArtistShowsCommand.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/Music/SeeArtistShowsCommand.cs
@@ -1,4 +1,5 @@
 ﻿using VLC_WINRT.Common;
+using VLC_WINRT_APP.Model.Music;
 using VLC_WINRT_APP.Views.MusicPages.ArtistPages;
 
 namespace VLC_WINRT_APP.Commands.Music
@@ -7,7 +8,11 @@ namespace VLC_WINRT_APP.Commands.Music
     {
         public override void Execute(object parameter)
         {
-            App.ApplicationFrame.Navigate(typeof (ArtistShowsPage));
+            if (parameter is ArtistItem)
+            {
+                App.ApplicationFrame.Navigate(typeof(ArtistShowsPage));
+                (App.ApplicationFrame.Content as ArtistShowsPage).DataContext = parameter as ArtistItem;
+            }
         }
     }
 }
