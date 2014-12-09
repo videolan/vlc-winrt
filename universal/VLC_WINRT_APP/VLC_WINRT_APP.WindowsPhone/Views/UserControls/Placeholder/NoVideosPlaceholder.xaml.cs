ef6d0f9392b6a4faa2fec215467bb8d45f5a9752
Thomas Nigro <thomas.nigro@outlook.com>
2014-12-09 20:49:54 +0100

    add no videos placeholder:
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/Placeholder/NoVideosPlaceholder.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Controls;
+namespace VLC_WINRT_APP.Views.UserControls.Placeholder
+{
+    public sealed partial class NoVideosPlaceholder : UserControl
+    {
+        public NoVideosPlaceholder()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
