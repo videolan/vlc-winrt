70c5c476c667c4ee2c15c28d80c9bd885f3d0e1b
Thomas Nigro <thomas.nigro@outlook.com>
2014-12-10 23:18:14 +0100

    Add BottomMiniPlyerVisibilityConverter
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/BottomMiniPlayerVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using VLC_WINRT_APP.Views.MusicPages;
+
+namespace VLC_WINRT_APP.Converters
+{
+    public class BottomMiniPlayerVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Type && (Type) value != typeof (MusicPlayerPage))
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
