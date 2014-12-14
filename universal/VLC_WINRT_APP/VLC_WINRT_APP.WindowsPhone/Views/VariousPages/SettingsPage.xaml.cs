82be0639da063344b6dee6953d485e78b4b5158d
Thomas Nigro <tn@thomasnigro.fr>
2014-12-14 19:32:33 +0100

    Add Version number in SettingsPage
    Ref #13191
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/VariousPages/SettingsPage.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/VariousPages/SettingsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.Phone.UI.Input;
+﻿using Windows.ApplicationModel;
+using Windows.Phone.UI.Input;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -13,6 +14,12 @@ namespace VLC_WINRT_APP.Views.VariousPages
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             HardwareButtons.BackPressed += HardwareButtonsOnBackPressed;
+
+            Package thisPackage = Package.Current;
+            PackageVersion version = thisPackage.Id.Version;
+            string appVersion = string.Format("{0}.{1}.{2}.{3}",
+                version.Major, version.Minor, version.Build, version.Revision);
+            AppVersion.Text = "VLC for WP8.1 v" + appVersion;
         }
 
         private void HardwareButtonsOnBackPressed(object sender, BackPressedEventArgs backPressedEventArgs)
