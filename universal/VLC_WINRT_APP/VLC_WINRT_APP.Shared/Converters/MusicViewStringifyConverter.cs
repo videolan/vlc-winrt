20b2b9fe5f20be82fa4c1e533821b2a35606c2a0
Thomas Nigro <tn@thomasnigro.fr>
2014-12-15 01:45:22 +0100

    Add MusicView and its converter
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/MusicViewStringifyConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Data;
+using VLC_WINRT_APP.Model;
+using VLC_WINRT_APP.Model.Music;
+
+namespace VLC_WINRT_APP.Converters
+{
+    public class MusicViewStringifyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is MusicView)
+            {
+                switch ((MusicView)value)
+                {
+                    case MusicView.Albums:
+                        return "albums";
+                        break;
+                    case MusicView.Artists:
+                        return "artists";
+                        break;
+                    case MusicView.Playlists:
+                        return "playlists";
+                        break;
+                    case MusicView.Songs:
+                        return "songs"; break;
+                }
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Model/Music/MusicView.cs
@@ -0,0 +1,10 @@
+﻿namespace VLC_WINRT_APP.Model.Music
+{
+    public enum MusicView
+    {
+        Albums = 0,
+        Artists = 1,
+        Songs = 2,
+        Playlists = 3,
+    }
+}
