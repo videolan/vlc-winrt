18e1baad808f8c53f56e650e5c45c58cde8065e5
Thomas Nigro <thomas.nigro@outlook.com>
2014-12-15 12:01:31 +0100

    MiniPlayer visibility in Landscape and Hide AppBar while playing videos
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/BottomMiniPlayer.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/BottomMiniPlayer.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using VLC_WINRT_APP.ViewModels;
+using VLC_WINRT_APP.Helpers;
 
 namespace VLC_WINRT_APP.Views.UserControls
 {
@@ -9,7 +10,32 @@ namespace VLC_WINRT_APP.Views.UserControls
         public BottomMiniPlayer()
         {
             this.InitializeComponent();
+            this.Loaded += BottomMiniPlayer_Loaded;
         }
+
+        void BottomMiniPlayer_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.SizeChanged += BottomMiniPlayer_SizeChanged;
+            this.Unloaded += BottomMiniPlayer_Unloaded;
+        }
+
+        void BottomMiniPlayer_Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.SizeChanged -= BottomMiniPlayer_SizeChanged;
+        }
+
+        void BottomMiniPlayer_SizeChanged(object sender, Windows.UI.Xaml.SizeChangedEventArgs e)
+        {
+            if (DisplayHelper.IsPortrait())
+            {
+                RootGrid.Height = 80;
+            }
+            else
+            {
+                RootGrid.Height = 0;
+            }
+        }
+
 
         private void RootMiniPlayer_Tapped(object sender, TappedRoutedEventArgs e)
         {
