7a6bc0d88d06f25b508b893f665e7d10b7bcec22
Thomas Nigro <thomas.nigro@outlook.com>
2014-12-16 23:31:08 +0100

    Improve NoVideo placeholder
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/Placeholder/NoVideosPlaceholder.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/Placeholder/NoVideosPlaceholder.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VLC_WINRT_APP.Model.Video;
+using VLC_WINRT_APP.ViewModels;
+
 namespace VLC_WINRT_APP.Views.UserControls.Placeholder
 {
     public sealed partial class NoVideosPlaceholder : UserControl
@@ -6,6 +10,16 @@ namespace VLC_WINRT_APP.Views.UserControls.Placeholder
         public NoVideosPlaceholder()
         {
             this.InitializeComponent();
+        }
+
+        private void ShowsClick(object sender, RoutedEventArgs e)
+        {
+            Locator.SettingsVM.VideoView = VideoView.Shows;
+        }
+
+        private void CameraRollClick(object sender, RoutedEventArgs e)
+        {
+            Locator.SettingsVM.VideoView = VideoView.CameraRoll;
         }
     }
 }
