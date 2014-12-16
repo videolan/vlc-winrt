ff4e73a0ee92ac910eaa3d55da41e3ee0bce7de6
Thomas Nigro <thomas.nigro@outlook.com>
2014-12-16 22:25:19 +0100

    When Track Index is 0 don't display it
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/NullIntConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using Newtonsoft.Json.Schema;
+
+namespace VLC_WINRT_APP.Converters
+{
+    public class NullIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int && (int) value == 0)
+            {
+                return "";
+            }
+            if (value is string && (string) value == "0")
+            {
+                return "";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
