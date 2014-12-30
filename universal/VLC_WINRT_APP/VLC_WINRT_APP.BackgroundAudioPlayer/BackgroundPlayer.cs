c79a2ee0fd94f5bcbd626bd7e0c721fdcc2361f7
Thomas Nigro <tn@thomasnigro.fr>
2014-12-31 00:15:09 +0100

    Add BackgroundPlayer class and reference in Manifest
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.BackgroundAudioPlayer/BackgroundPlayer.cs
@@ -0,0 +1,12 @@
+﻿using Windows.ApplicationModel.Background;
+
+namespace VLC_WINRT_APP.BackgroundAudioPlayer
+{
+    public sealed class BackgroundPlayer : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            
+        }
+    }
+}
