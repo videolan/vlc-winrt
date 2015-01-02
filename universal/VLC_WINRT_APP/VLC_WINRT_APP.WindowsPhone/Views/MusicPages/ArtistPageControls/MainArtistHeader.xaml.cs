f7cba2ddfcad1738bf2621d9ebe80d95d8710169
Thomas Nigro <tn@thomasnigro.fr>
2015-01-02 18:30:19 +0100

    MainArtistHeader dedicated control

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/MusicPages/ArtistPageControls/MainArtistHeader.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace VLC_WINRT_APP.Views.MusicPages.ArtistPageControls
+{
+    public sealed partial class MainArtistHeader : UserControl
+    {
+        public MainArtistHeader()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
