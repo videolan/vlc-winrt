7dbd39f32fea5ec17df9c4f1d2b06ba412616296
Thomas Nigro <tn@thomasnigro.fr>
2015-01-02 19:15:58 +0100

    Update VideoInformationFlyout When playing video from its flyout, it closes the flyout

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/Flyouts/VideoInformationFlyout.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+namespace VLC_WINRT_APP.Views.UserControls.Flyouts
+{
+    public sealed partial class VideoInformationFlyout : UserControl
+    {
+        public VideoInformationFlyout()
+        {
+            this.InitializeComponent();
+        }
+        private void ActionButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+        }
+
+        void Hide()
+        {
+            var trackFlyout = App.Current.Resources["VideoInformationFlyout"] as Flyout;
+            trackFlyout.Hide();
+        }
+    }
+}
