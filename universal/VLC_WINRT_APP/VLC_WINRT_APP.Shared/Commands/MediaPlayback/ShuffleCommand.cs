13edf48feadb0f6facdb6ecffccebf64220c4bb1
Thomas Nigro <tn@thomasnigro.fr>
2015-01-04 23:36:20 +0100

    Cosmetics

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/MediaPlayback/ShuffleCommand.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/MediaPlayback/ShuffleCommand.cs
@@ -15,26 +15,26 @@ namespace VLC_WINRT_APP.Commands.MediaPlayback
             if (Locator.MusicPlayerVM.TrackCollection == null
                 || Locator.MusicPlayerVM.TrackCollection.Playlist == null
                 || !Locator.MusicPlayerVM.TrackCollection.Playlist.Any()) return;
-            if (Locator.MusicPlayerVM.TrackCollection.IsShuffled)
-            {
-                Locator.MusicPlayerVM.TrackCollection.Playlist = Locator.MusicPlayerVM.TrackCollection.NonShuffledPlaylist;
-                //Locator.MusicPlayerVM.TrackCollection.NonShuffledPlaylist.Clear();
-            }
-            else
-            {
-                Locator.MusicPlayerVM.TrackCollection.NonShuffledPlaylist = new ObservableCollection<TrackItem>(Locator.MusicPlayerVM.TrackCollection.Playlist);
-                Random r = new Random();
-                for (int i = 0; i < Locator.MusicPlayerVM.TrackCollection.Playlist.Count; i++)
+                if (Locator.MusicPlayerVM.TrackCollection.IsShuffled)
                 {
-                    int index1 = r.Next(Locator.MusicPlayerVM.TrackCollection.Playlist.Count);
-                    int index2 = r.Next(Locator.MusicPlayerVM.TrackCollection.Playlist.Count);
-                    Locator.MusicPlayerVM.TrackCollection.Playlist.Move(index1, index2);
-                }   
-            }
-            Locator.MusicPlayerVM.TrackCollection.IsShuffled = !Locator.MusicPlayerVM.TrackCollection.IsShuffled;
-            Locator.MusicPlayerVM.TrackCollection.CurrentTrack =
-                Locator.MusicPlayerVM.TrackCollection.Playlist.IndexOf(
-                    Locator.MusicPlayerVM.TrackCollection.Playlist.FirstOrDefault(x => x.IsCurrentPlaying == true));
+                    Locator.MusicPlayerVM.TrackCollection.Playlist = Locator.MusicPlayerVM.TrackCollection.NonShuffledPlaylist;
+                    //Locator.MusicPlayerVM.TrackCollection.NonShuffledPlaylist.Clear();
+                }
+                else
+                {
+                    Locator.MusicPlayerVM.TrackCollection.NonShuffledPlaylist = new ObservableCollection<TrackItem>(Locator.MusicPlayerVM.TrackCollection.Playlist);
+                    Random r = new Random();
+                    for (int i = 0; i < Locator.MusicPlayerVM.TrackCollection.Playlist.Count; i++)
+                    {
+                        int index1 = r.Next(Locator.MusicPlayerVM.TrackCollection.Playlist.Count);
+                        int index2 = r.Next(Locator.MusicPlayerVM.TrackCollection.Playlist.Count);
+                        Locator.MusicPlayerVM.TrackCollection.Playlist.Move(index1, index2);
+                    }   
+                }
+                Locator.MusicPlayerVM.TrackCollection.IsShuffled = !Locator.MusicPlayerVM.TrackCollection.IsShuffled;
+                Locator.MusicPlayerVM.TrackCollection.CurrentTrack =
+                    Locator.MusicPlayerVM.TrackCollection.Playlist.IndexOf(
+                        Locator.MusicPlayerVM.TrackCollection.Playlist.FirstOrDefault(x => x.IsCurrentPlaying == true));
         }
     }
 }
