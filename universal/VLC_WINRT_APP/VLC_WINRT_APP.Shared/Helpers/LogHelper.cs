68b028a2d211038b3c258426c6839a561080db70
Thomas Nigro <tn@thomasnigro.fr>
2015-01-06 01:33:46 +0100

    Save any debug info in LogFile.txt

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Helpers/LogHelper.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Helpers/LogHelper.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace VLC_WINRT_APP.Helpers
 {
     public static class LogHelper
     {
+        private static StorageFile LogFile;
+
+        static LogHelper()
+        {
+            Initialize();
+        }
+
+        static async Task Initialize()
+        {
+            LogFile = await ApplicationData.Current.LocalFolder.CreateFileAsync("LogFile.txt", CreationCollisionOption.ReplaceExisting);
+        }
         public static void Log(object o)
         {
             Debug.WriteLine(o.ToString());
+            WriteInLog(o.ToString());
+        }
+
+        static void WriteInLog(string value)
+        {
+            if (LogFile != null) FileIO.WriteTextAsync(LogFile, value);
         }
     }
 }
