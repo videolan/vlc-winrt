a9f6fec2e7923492cc9a57a3403857f3b4ee7ba8
Pavel Valach <valach.pavel@gmail.com>
2015-01-08 03:36:08 +0100

    Added localization converters for unknown (= empty) album and artist
    This should be a good starting point for having this part of app localized. I tried to include all the shared parts, but focused on Windows Phone only.
    This commit is a complement to the commit where the 'Unknown album' and 'Unknown artist' is removed from the code and replaced by string.Empty.
    No known issues with this approach so far, however, they might happen.
    
    Signed-off-by: Pavel Valach <valach.pavel@gmail.com>
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/AlbumStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace VLC_WINRT_APP.Converters
+{
+    public class AlbumStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is String && !string.IsNullOrEmpty((string)value))
+            {
+                return value;
+            }
+
+            // This might be expensive, but it won't be called too often.
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+            return loader.GetString("UnknownAlbum");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
