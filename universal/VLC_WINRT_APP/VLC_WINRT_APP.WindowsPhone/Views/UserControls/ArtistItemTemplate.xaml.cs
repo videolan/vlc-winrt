26871d7b6d5e1d4d1b6fbbd8c35157173a1b74e8
Thomas Nigro <tn@thomasnigro.fr>
2015-01-09 18:51:47 +0100

    ArtistTemplate is in a UserControl

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/ArtistItemTemplate.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VLC_WINRT_APP.Model.Music;
+
+namespace VLC_WINRT_APP.Views.UserControls
+{
+    public sealed partial class ArtistItemTemplate : UserControl
+    {
+        public ArtistItemTemplate()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Image_OnImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            var albumItem = (this.DataContext as ArtistItem);
+            Debug.WriteLine("Cover " + albumItem.Name + " failed");
+        }
+
+        private void Image_OnImageOpened(object sender, RoutedEventArgs e)
+        {
+            var albumItem = (this.DataContext as ArtistItem);
+            Debug.WriteLine("Cover " + albumItem.Name + " opened");
+        }
+    }
+}
