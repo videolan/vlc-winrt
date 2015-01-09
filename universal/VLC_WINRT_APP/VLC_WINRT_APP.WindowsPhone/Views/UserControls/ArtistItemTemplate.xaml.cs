5289caa1461d622ca2e87d15389e141bb1cc3cd5
Thomas Nigro <tn@thomasnigro.fr>
2015-01-09 20:07:24 +0100

    Remove debug event handlers

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/ArtistItemTemplate.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/ArtistItemTemplate.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.Diagnostics;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using VLC_WINRT_APP.Model.Music;
+﻿using Windows.UI.Xaml.Controls;
 
 namespace VLC_WINRT_APP.Views.UserControls
 {
@@ -10,18 +7,6 @@ namespace VLC_WINRT_APP.Views.UserControls
         public ArtistItemTemplate()
         {
             this.InitializeComponent();
-        }
-
-        private void Image_OnImageFailed(object sender, ExceptionRoutedEventArgs e)
-        {
-            var albumItem = (this.DataContext as ArtistItem);
-            Debug.WriteLine("Cover " + albumItem.Name + " failed");
-        }
-
-        private void Image_OnImageOpened(object sender, RoutedEventArgs e)
-        {
-            var albumItem = (this.DataContext as ArtistItem);
-            Debug.WriteLine("Cover " + albumItem.Name + " opened");
         }
     }
 }
