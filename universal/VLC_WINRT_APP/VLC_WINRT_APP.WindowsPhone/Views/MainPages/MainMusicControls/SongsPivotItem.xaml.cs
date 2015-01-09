021a9353f074b7c7636ea7e6296d4c4db884442f
Thomas Nigro <tn@thomasnigro.fr>
2015-01-09 18:54:19 +0100

    Fix horizontal view of Songs SemanticViewedOut view

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/MainPages/MainMusicControls/SongsPivotItem.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/MainPages/MainMusicControls/SongsPivotItem.xaml.cs
@@ -2,6 +2,7 @@
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using VLC_WINRT_APP.Helpers;
 
 namespace VLC_WINRT_APP.Views.MainPages.MainMusicControls
 {
@@ -20,9 +21,20 @@ namespace VLC_WINRT_APP.Views.MainPages.MainMusicControls
 
         private void ItemsWrapGrid_Loaded(object sender, RoutedEventArgs e)
         {
-            var width = Window.Current.Bounds.Width;
-            var grid = sender as ItemsWrapGrid;
-            grid.ItemWidth = (width - 48)/4;
+            Responsive(sender as ItemsWrapGrid);
+        }
+
+        private void ItemsWrapGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Responsive(sender as ItemsWrapGrid);
+        }
+
+        void Responsive(ItemsWrapGrid grid)
+        {
+            if (grid == null) return;
+            double width;
+            width = DisplayHelper.IsPortrait() ? Window.Current.Bounds.Width : 400;
+            grid.ItemWidth = (width - 48) / 4;
             grid.ItemHeight = grid.ItemWidth;
         }
     }
