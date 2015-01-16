b2a71873a1a92b3da28531d3443b45469a052b92
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-01-16 11:12:34 +0100

    PlayNetworkMRLCommand: Do not navigate to the player page in case of error

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/PlayNetworkMRLCommand.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/PlayNetworkMRLCommand.cs
@@ -7,7 +7,9 @@
  * Refer to COPYING file of the official project for license
  **********************************************************************/
 
+using System;
 using VLC_WINRT.Common;
+using VLC_WINRT_APP.Helpers;
 using VLC_WINRT_APP.ViewModels;
 using VLC_WINRT_APP.Views.VideoPages;
 
@@ -21,6 +23,17 @@ namespace VLC_WINRT_APP.Commands
             if (string.IsNullOrEmpty(mrl))
                 return;
 
+            //TODO: pass MRL to vlc
+            try
+            {
+                await Locator.VideoVm.SetActiveVideoInfo(null, mrl);
+            }
+            catch (Exception ex)
+            {
+                ExceptionHelper.CreateMemorizedException("PlayNetworkMRLCommand.Execute", ex);
+                return;
+            }
+
             if (App.ApplicationFrame.CurrentSourcePageType != typeof(VideoPlayerPage))
             {
                 App.ApplicationFrame.Navigate(typeof(VideoPlayerPage));
@@ -29,9 +42,6 @@ namespace VLC_WINRT_APP.Commands
 #if WINDOWS_PHONE_APP
             Locator.MainVM.CloseStreamFlyout();
 #endif
-
-            //TODO: pass MRL to vlc
-            await Locator.VideoVm.SetActiveVideoInfo(null, mrl);
         }
     }
 }
