4a4c6349513c53679dd387717436e46692673457
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-01-20 17:36:10 +0100

    direct2d: Remove dead code

--- modules/libdirect2d_winrt_plugin/libdirect2d_winrt_plugin.cpp
+++ modules/libdirect2d_winrt_plugin/libdirect2d_winrt_plugin.cpp
@@ -415,59 +415,6 @@ static void Prepare(vout_display_t *vd, picture_t *picture, subpicture_t *subpic
 
     sys->d2dContext->DrawImage(sys->yuvEffect, D2D1_INTERPOLATION_MODE_CUBIC);
 
-    #if 0
-    /* FIXME: look at the following example http://code.msdn.microsoft.com/windowsapps/Direct2D-Image-Effects-4819dc5b */
-    if (subpicture) {
-        for (subpicture_region_t *r = subpicture->p_region; r != NULL; r = r->p_next) {
-
-            D2D1_SIZE_U            size2;
-            size2.height = r->fmt.i_visible_height;
-            size2.width  = r->fmt.i_visible_width;
-
-            D2D1_BITMAP_PROPERTIES props2;
-            D2D1_PIXEL_FORMAT      pixFormat2;
-            pixFormat2.format = DXGI_FORMAT_R8G8B8A8_UNORM;
-            pixFormat2.alphaMode = D2D1_ALPHA_MODE_IGNORE;
-            props2.pixelFormat   = pixFormat2;
-            props2.dpiX = dpi;
-            props2.dpiY = dpi;
-
-            /* This is awful */
-            const int pixels_offset = r->fmt.i_y_offset * r->p_picture->p->i_pitch +
-                                      r->fmt.i_x_offset * r->p_picture->p->i_pixel_pitch;
-            for (int y = 0; y < r->fmt.i_visible_height; y++) {
-               uint8_t *row = (uint8_t*)&r->p_picture->p->p_pixels[pixels_offset + y*r->p_picture->p->i_pitch];
-               for (int x = 0; x < r->fmt.i_visible_width * 4; x += 4) {
-                   uint8_t r = row[x + 0];
-                   uint8_t g = row[x + 1];
-                   uint8_t b = row[x + 2];
-                   uint8_t a = row[x + 3];
-                   if (a != 255)
-                       r = g = b = a = 0;
-                   row[x + 0] = r;
-                   row[x + 1] = g;
-                   row[x + 2] = b;
-                   row[x + 3] = a;
-               }
-            }
-
-
-            ID2D1Bitmap                 *d2dbmp2;
-            HRESULT hr = sys->d2dContext->CreateBitmap(size2, r->p_picture->p[0].p_pixels, r->p_picture->p[0].i_pitch, props2, &d2dbmp2 );
-
-            Debug( L"D2DD 0x%x\n", hr);
-
-            ComPtr<ID2D1Effect> blendEffect;
-            sys->d2dContext->CreateEffect(CLSID_D2D1Blend, &blendEffect);
-
-            /* Incorrect if the video is rescaled*/
-            D2D1_RECT_F dst_rect = { r->i_x, r->i_y, r->i_x + r->fmt.i_visible_width, r->i_y + r->fmt.i_visible_height};
-            if(d2dbmp2)
-                vd->sys->d2dContext->DrawBitmap(d2dbmp2, dst_rect);
-        }
-    }
-#endif
-
     sys->d2dContext->PopAxisAlignedClip();
     HRESULT hr_draw = vd->sys->d2dContext->EndDraw();
 
