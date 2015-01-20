8b9e538f12148282c3579e8c21d831f55758b6fa
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-01-20 17:22:42 +0100

    MusicPlayerVM: Fix assertion

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/ViewModels/MusicVM/MusicPlayerVM.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/ViewModels/MusicVM/MusicPlayerVM.cs
@@ -230,7 +230,7 @@ namespace VLC_WINRT_APP.ViewModels.MusicVM
             bool playWithLibVlc = !VLCFileExtensions.MFSupported.Contains(currentTrackFile.FileType.ToLower()) || forceVlcLib;
             if (!playWithLibVlc)
             {
-                Debug.Assert(Locator.MusicLibraryVM.ContinueIndexing == null);
+                Debug.Assert(Locator.MusicLibraryVM.ContinueIndexing == null || Locator.MusicLibraryVM.ContinueIndexing.Task.IsCompleted);
                 Locator.MusicLibraryVM.ContinueIndexing = new TaskCompletionSource<bool>();
                 App.BackgroundAudioHelper.PlayAudio(track.Id);
             }
