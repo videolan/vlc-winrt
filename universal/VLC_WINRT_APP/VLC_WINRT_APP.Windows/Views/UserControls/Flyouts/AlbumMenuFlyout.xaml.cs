4cec5da335afed5b11299575556d4b9ce47a3f96
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 00:57:01 +0100

    Share more xaml code with AlbumMenuFlyout

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/UserControls/Flyouts/AlbumMenuFlyout.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Controls;
+namespace VLC_WINRT_APP.Views.UserControls.Flyouts
+{
+    public sealed partial class AlbumMenuFlyout : Flyout
+    {
+        public AlbumMenuFlyout()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.WindowsPhone/Views/UserControls/Flyouts/AlbumMenuFlyout.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Controls;
+namespace VLC_WINRT_APP.Views.UserControls.Flyouts
+{
+    public sealed partial class AlbumMenuFlyout : MenuFlyout
+    {
+        public AlbumMenuFlyout()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
