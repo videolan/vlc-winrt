1e76703df2a43deeabcf472e2be54833eddb115a
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 00:58:13 +0100

    Add AlbumItem usercontrol

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/UserControls/AlbumItem.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace VLC_WINRT_APP.Views.UserControls
+{
+    public sealed partial class AlbumItem : UserControl
+    {
+        public AlbumItem()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RootAlbumItem_Loaded(object sender, RoutedEventArgs e)
+        {
+#if WINDOWS_APP
+            var grid = sender as Grid;
+            if (grid != null)
+                grid.Tapped += (o, args) => Flyout.ShowAttachedFlyout((Grid) sender);
+#endif
+        }
+    }
+}
