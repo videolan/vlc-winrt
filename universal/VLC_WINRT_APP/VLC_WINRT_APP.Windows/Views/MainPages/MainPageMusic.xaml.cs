8c2c19ed6b205eb46940056461df7d9833ffb4f2
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 01:02:49 +0100

    MainPageMusic is now, like in WP, just a navigation entry

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MainPages/MainPageMusic.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MainPages/MainPageMusic.xaml.cs
@@ -29,67 +29,14 @@ namespace VLC_WINRT_APP.Views.MainPages
         {
             this.InitializeComponent();
             this.NavigationCacheMode = NavigationCacheMode.Enabled;
-            this.SizeChanged += OnSizeChanged;
-        }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-        }
-
-        private void OnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs)
-        {
-            if (Window.Current.Bounds.Width < 400)
-            {
-                RootGrid.Margin = new Thickness(9, 0, 0, 0);
-                FirstRowDefinition.Height = new GridLength(0);
-            }
-            else
-            {
-                RootGrid.Margin = new Thickness(40, 0, 0, 0);
-                FirstRowDefinition.Height = new GridLength(42);
-            }
-        }
-
-
-
-
-        private void Panels_OnItemClick(object sender, ItemClickEventArgs e)
-        {
-            Model.Panel panel = e.ClickedItem as Model.Panel;
-            foreach (var panel1 in Locator.MusicLibraryVM.Panels)
-            {
-                panel1.Color = new SolidColorBrush(Colors.DimGray);
-            }
-            panel.Color = App.Current.Resources["MainColor"] as SolidColorBrush;
-
-            switch (panel.Index)
-            {
-                case 0:
-                    if(MusicPanesFrame.CurrentSourcePageType != typeof(ArtistsSemanticZoom))
-                        MusicPanesFrame.Navigate(typeof (ArtistsSemanticZoom));
-                    FavoriteAlbumsGridView.Visibility = Visibility.Collapsed;
-                    break;
-                case 1:
-                    if (MusicPanesFrame.CurrentSourcePageType != typeof(ArtistAlbumsTwoPanes))
-                        MusicPanesFrame.Navigate(typeof(ArtistAlbumsTwoPanes));
-                    FavoriteAlbumsGridView.Visibility = Visibility.Collapsed;
-                    break;
-                case 2:
-                    if (MusicPanesFrame.CurrentSourcePageType != typeof(SongsListView))
-                        MusicPanesFrame.Navigate(typeof(SongsListView));
-                    FavoriteAlbumsGridView.Visibility = Visibility.Collapsed;
-                    break;
-                case 3:
-                    FavoriteAlbumsGridView.Visibility = Visibility.Visible;
-                    break;
-            }
         }
 
         private void MusicPanesFrame_OnLoaded(object sender, RoutedEventArgs e)
         {
-            if(MusicPanesFrame.CurrentSourcePageType == null)
-                MusicPanesFrame.Navigate(typeof (ArtistsSemanticZoom));
+            if (MainPageMusicContentPresenter.CurrentSourcePageType == null)
+            {
+                Locator.MainVM.ChangeMainPageMusicViewCommand.Execute((int)Locator.SettingsVM.MusicView);
+            }
         }
     }
 }
