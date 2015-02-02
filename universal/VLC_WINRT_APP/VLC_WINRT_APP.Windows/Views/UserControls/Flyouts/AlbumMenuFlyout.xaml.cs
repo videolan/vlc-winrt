c2aa4a358a13eada1e5fe8c00a7d089dd9362d8e
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 15:35:50 +0100

    Add an appbar in AlbumPageflyout

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/UserControls/Flyouts/AlbumMenuFlyout.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/UserControls/Flyouts/AlbumMenuFlyout.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VLC_WINRT_APP.Helpers;
+using AppBar = CustomAppBarDesktop.AppBar;
+
 namespace VLC_WINRT_APP.Views.UserControls.Flyouts
 {
     public sealed partial class AlbumMenuFlyout : Flyout
@@ -6,6 +11,12 @@ namespace VLC_WINRT_APP.Views.UserControls.Flyouts
         public AlbumMenuFlyout()
         {
             this.InitializeComponent();
+        }
+
+        private void AppBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            var appBar = AppBarHelper.SetAlbumPageButtons(new List<ICommandBarElement>());
+            (sender as AppBar).PrimaryCommands = appBar;
         }
     }
 }
