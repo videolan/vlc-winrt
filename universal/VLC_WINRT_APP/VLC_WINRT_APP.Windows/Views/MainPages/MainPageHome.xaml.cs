cee9fc0fecedd731db1ee82c725800af18ab25dd
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 15:35:24 +0100

    Reset responsiveness of the MainPageHome hub

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MainPages/MainPageHome.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MainPages/MainPageHome.xaml.cs
@@ -55,18 +55,6 @@ namespace VLC_WINRT_APP.Views.MainPages
 
         private void Responsive()
         {
-            if (Window.Current.Bounds.Width < 400)
-            {
-                MainGrid.Margin = new Thickness(-30);
-                SnapHeaderGrid.Visibility = Visibility.Visible;
-                StandardHeaderGrid.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                MainGrid.Margin = new Thickness(0);
-                SnapHeaderGrid.Visibility = Visibility.Collapsed;
-                StandardHeaderGrid.Visibility = Visibility.Visible;
-            }
         }
 
         private void VideoItemWrapGrid_Loaded(object sender, RoutedEventArgs e)
@@ -82,43 +70,6 @@ namespace VLC_WINRT_APP.Views.MainPages
 
         private void Responsive(ItemsWrapGrid itemsWrap)
         {
-            int usefulWidth = (int)Window.Current.Bounds.Width;
-            Debug.WriteLine(usefulWidth);
-            int sidebar;
-            if (Window.Current.Bounds.Width < 400)
-            {
-                sidebar = 65;
-            }
-            else if (Window.Current.Bounds.Width < 1080)
-            {
-                sidebar = 180;
-            }
-            else
-            {
-                sidebar = 420;
-            }
-            usefulWidth -= sidebar;
-
-            if (usefulWidth < 400)
-            {
-                itemsWrap.ItemWidth = usefulWidth;
-                itemsWrap.ItemHeight = usefulWidth * 0.561;
-            }
-            else if (usefulWidth < 890)
-            {
-                itemsWrap.ItemWidth = usefulWidth / 2;
-                itemsWrap.ItemHeight = (usefulWidth / 2) * 0.561;
-            }
-            else if (usefulWidth < 1300)
-            {
-                itemsWrap.ItemWidth = usefulWidth / 3;
-                itemsWrap.ItemHeight = (usefulWidth / 3) * 0.561;
-            }
-            else
-            {
-                itemsWrap.ItemWidth = usefulWidth / 4;
-                itemsWrap.ItemHeight = (usefulWidth / 4) * 0.561;
-            }
         }
     }
 }
