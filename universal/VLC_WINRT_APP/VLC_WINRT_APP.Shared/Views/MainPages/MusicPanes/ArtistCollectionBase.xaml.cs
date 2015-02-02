c084d8669faa24d9c5812cc9acb28197ecaa8e36
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 16:04:40 +0100

    Add ArtistCollectionBase for displaying Artists

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MusicPanes/ArtistCollectionBase.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VLC_WINRT_APP.Model.Video;
+
+namespace VLC_WINRT_APP.Views.MainPages.MusicPanes
+{
+    public sealed partial class ArtistCollectionBase : UserControl
+    {
+        public ArtistCollectionBase()
+        {
+            this.InitializeComponent();
+        }
+        private void ArtistsWrapGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            TemplateSizer.ComputeAlbums(sender as ItemsWrapGrid);
+        }
+    }
+}
