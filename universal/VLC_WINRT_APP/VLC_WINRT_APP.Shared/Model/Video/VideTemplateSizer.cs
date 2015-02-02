7441699f9036cfdcec0e6bb6d4c290542711897c
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 18:47:26 +0100

    Change the way we set items size in Windows

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Model/Video/VideTemplateSizer.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Model/Video/VideTemplateSizer.cs
@@ -17,17 +17,20 @@ namespace VLC_WINRT_APP.Model.Video
             var splitScreen = 2;
             if (!DisplayHelper.IsPortrait())
                 splitScreen = 4;
+#if WINDOWS_APP
+            width = (width > 600) ? width : 470;
+            if (width == 470) splitScreen = 2;
+#endif
             var itemWidth = (width / splitScreen);
-            var itemHeight = (itemWidth * 9/16) + 78;
-            //var itemHeight = (itemWidth);
+            var itemHeight = (itemWidth * 9 / 16) + 78;
             wrapGrid.ItemWidth = itemWidth - 10;
             wrapGrid.ItemHeight = itemHeight;
         }
 
         public static void ComputeAlbums(ItemsWrapGrid wrapGrid, TemplateSize size = TemplateSize.Compact)
         {
-#if WINDOWS_PHONE_APP
             var width = Window.Current.Bounds.Width;
+#if WINDOWS_PHONE_APP
             if(width == 400)
                 size = TemplateSize.Normal;
             var splitScreen = (size == TemplateSize.Compact) ? 3 : 2;
@@ -38,8 +41,16 @@ namespace VLC_WINRT_APP.Model.Video
             wrapGrid.ItemWidth = itemWidth -7;
             wrapGrid.ItemHeight = itemHeight;
 #else
-            wrapGrid.ItemWidth = 200;
-            wrapGrid.ItemHeight = 266;
+            if (width > 600)
+            {
+                wrapGrid.ItemWidth = 200;
+                wrapGrid.ItemHeight = 266;
+            }
+            else
+            {
+                wrapGrid.ItemWidth = (width / 3) - 20;
+                wrapGrid.ItemHeight = wrapGrid.ItemWidth * 1.33;
+            }
 #endif
         }
     }
