a27c507d6d9321518687b84e2d38caa63eaa3e0b
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 01:48:38 +0100

    Remove useless menu

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/ViewModels/MainVM.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/ViewModels/MainVM.cs
@@ -186,7 +186,6 @@ namespace VLC_WINRT_APP.ViewModels
             removableName = resourceLoader.GetString("RemovableStorage");
             Panels.Add(new Panel(removableName, 3, 0.4, App.Current.Resources["RemovablesPath"].ToString()));
             //removableName = resourceLoader.GetString("SdCard");
-            Panels.Add(new Panel(resourceLoader.GetString("More"), 3, 0.4, App.Current.Resources["HamburgerPath"].ToString()));
             Panels.Add(new Panel(resourceLoader.GetString("MediaServers"), 4, 0.4, App.Current.Resources["ServerPath"].ToString()));
 #endif
             Initialize();
