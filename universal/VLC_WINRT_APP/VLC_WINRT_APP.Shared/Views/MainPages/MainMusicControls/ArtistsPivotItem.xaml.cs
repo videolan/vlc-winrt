7c51f559819bc386ba4f79f07086948d38dc190e
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 16:02:50 +0100

    Merge ArtistsPivotItem

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MainMusicControls/ArtistsPivotItem.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VLC_WINRT_APP.Model.Video;
+
+namespace VLC_WINRT_APP.Views.MainPages.MainMusicControls
+{
+    public sealed partial class ArtistsPivotItem : Page
+    {
+        public ArtistsPivotItem()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
