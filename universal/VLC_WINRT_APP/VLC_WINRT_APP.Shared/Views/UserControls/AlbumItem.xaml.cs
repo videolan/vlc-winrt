e2186cc711474966a148f0fe1e20196afefb03f1
Thomas Nigro <tn@thomasnigro.fr>
2015-02-02 15:31:25 +0100

    Use codebehind for registering interactions with album items

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/UserControls/AlbumItem.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/UserControls/AlbumItem.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Linq;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using VLC_WINRT_APP.ViewModels;
 
 namespace VLC_WINRT_APP.Views.UserControls
 {
@@ -15,7 +18,22 @@ namespace VLC_WINRT_APP.Views.UserControls
 #if WINDOWS_APP
             var grid = sender as Grid;
             if (grid != null)
-                grid.Tapped += (o, args) => Flyout.ShowAttachedFlyout((Grid) sender);
+            {
+                grid.Tapped += (o, args) =>
+                {
+                    var album = (this.DataContext as Model.Music.AlbumItem);
+                    Locator.MusicLibraryVM.CurrentArtist = Locator.MusicLibraryVM.Artists.FirstOrDefault(x => x.Id == album.ArtistId);
+                    Locator.MusicLibraryVM.CurrentAlbum = album;
+                    Flyout.ShowAttachedFlyout((Grid)sender);
+                };
+            }
+#endif
+        }
+
+        private void RootAlbumItem_Holding(object sender, HoldingRoutedEventArgs e)
+        {
+#if WINDOWS_PHONE_APP
+            Flyout.ShowAttachedFlyout((Grid)sender);
 #endif
         }
     }
