6d7750e79687faaf4b582d39470d8ed40819311e
Thomas Nigro <tn@thomasnigro.fr>
2015-02-04 18:57:46 +0100

    Add SliderControl

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/UserControls/SliderControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Controls;
+namespace VLC_WINRT_APP.Views.UserControls
+{
+    public sealed partial class SliderControl : UserControl
+    {
+        public SliderControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
