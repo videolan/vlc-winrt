57ac2b5e5da64cecd24fdda27fb9aee11ffc05dd
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-02-11 16:25:19 +0100

    direct2d: spacify

--- modules/libdirect2d_winrt_plugin.Shared/I420Effect.h
+++ modules/libdirect2d_winrt_plugin.Shared/I420Effect.h
@@ -24,29 +24,29 @@ DEFINE_GUID(CLSID_CustomI420Effect, 0xda637e40, 0x44d4, 0x4617, 0xa3, 0xd3, 0xa2
 
 typedef enum I420_PROP
 {
-	I420_PROP_DISPLAYEDFRAME_WIDTH = 0,
-	I420_PROP_DISPLAYEDFRAME_HEIGHT = 1,
-	I420_PROP_VISIBLE_WIDTH = 2,
+    I420_PROP_DISPLAYEDFRAME_WIDTH = 0,
+    I420_PROP_DISPLAYEDFRAME_HEIGHT = 1,
+    I420_PROP_VISIBLE_WIDTH = 2,
     I420_PROP_VISIBLE_HEIGHT = 3,
 };
 
 struct Vertex{
-	float x;
-	float y;
+    float x;
+    float y;
 };
 
 class I420Effect : public ID2D1EffectImpl, public ID2D1DrawTransform
 {
 public:
-	// Declare effect registration methods.
-	static HRESULT Register(_In_ ID2D1Factory1* pFactory);
-	static HRESULT __stdcall CreateRippleImpl(_Outptr_ IUnknown** ppEffectImpl);
+    // Declare effect registration methods.
+    static HRESULT Register(_In_ ID2D1Factory1* pFactory);
+    static HRESULT __stdcall CreateRippleImpl(_Outptr_ IUnknown** ppEffectImpl);
 
-	uint32 GetDisplayedFrameWidth() const;
-	HRESULT SetDisplayedFrameWidth(uint32 width);
+    uint32 GetDisplayedFrameWidth() const;
+    HRESULT SetDisplayedFrameWidth(uint32 width);
 
-	uint32 GetDisplayedFrameHeight() const;
-	HRESULT SetDisplayedFrameHeight(uint32 height);
+    uint32 GetDisplayedFrameHeight() const;
+    HRESULT SetDisplayedFrameHeight(uint32 height);
 
     uint32 GetVisibleWidth() const;
     HRESULT SetVisibleWidth(uint32 width);
@@ -54,63 +54,63 @@ public:
     uint32 GetVisibleHeight() const;
     HRESULT SetVisibleHeight(uint32 height);
 
-	// Declare ID2D1EffectImpl implementation methods.
-	IFACEMETHODIMP Initialize(_In_ ID2D1EffectContext* pContextInternal,_In_ ID2D1TransformGraph* pTransformGraph);
-	IFACEMETHODIMP PrepareForRender(D2D1_CHANGE_TYPE changeType);
-	IFACEMETHODIMP SetGraph(_In_ ID2D1TransformGraph* pGraph);
+    // Declare ID2D1EffectImpl implementation methods.
+    IFACEMETHODIMP Initialize(_In_ ID2D1EffectContext* pContextInternal,_In_ ID2D1TransformGraph* pTransformGraph);
+    IFACEMETHODIMP PrepareForRender(D2D1_CHANGE_TYPE changeType);
+    IFACEMETHODIMP SetGraph(_In_ ID2D1TransformGraph* pGraph);
 
-	// Declare ID2D1DrawTransform implementation methods.
-	IFACEMETHODIMP SetDrawInfo(_In_ ID2D1DrawInfo* pRenderInfo);
+    // Declare ID2D1DrawTransform implementation methods.
+    IFACEMETHODIMP SetDrawInfo(_In_ ID2D1DrawInfo* pRenderInfo);
 
-	// Declare ID2D1Transform implementation methods.
-	IFACEMETHODIMP MapOutputRectToInputRects(
-		_In_ const D2D1_RECT_L* pOutputRect,
-		_Out_writes_(inputRectCount) D2D1_RECT_L* pInputRects,
-		UINT32 inputRectCount
-		) const;
+    // Declare ID2D1Transform implementation methods.
+    IFACEMETHODIMP MapOutputRectToInputRects(
+        _In_ const D2D1_RECT_L* pOutputRect,
+        _Out_writes_(inputRectCount) D2D1_RECT_L* pInputRects,
+        UINT32 inputRectCount
+        ) const;
 
-	IFACEMETHODIMP MapInputRectsToOutputRect(
-		_In_reads_(inputRectCount) CONST D2D1_RECT_L* pInputRects,
-		_In_reads_(inputRectCount) CONST D2D1_RECT_L* pInputOpaqueSubRects,
-		UINT32 inputRectCount,
-		_Out_ D2D1_RECT_L* pOutputRect,
-		_Out_ D2D1_RECT_L* pOutputOpaqueSubRect
-		);
+    IFACEMETHODIMP MapInputRectsToOutputRect(
+        _In_reads_(inputRectCount) CONST D2D1_RECT_L* pInputRects,
+        _In_reads_(inputRectCount) CONST D2D1_RECT_L* pInputOpaqueSubRects,
+        UINT32 inputRectCount,
+        _Out_ D2D1_RECT_L* pOutputRect,
+        _Out_ D2D1_RECT_L* pOutputOpaqueSubRect
+        );
 
-	IFACEMETHODIMP MapInvalidRect(
-		UINT32 inputIndex,
-		D2D1_RECT_L invalidInputRect,
-		_Out_ D2D1_RECT_L* pInvalidOutputRect
-		) const;
+    IFACEMETHODIMP MapInvalidRect(
+        UINT32 inputIndex,
+        D2D1_RECT_L invalidInputRect,
+        _Out_ D2D1_RECT_L* pInvalidOutputRect
+        ) const;
 
-	// Declare ID2D1TransformNode implementation methods.
-	IFACEMETHODIMP_(UINT32) GetInputCount() const;
+    // Declare ID2D1TransformNode implementation methods.
+    IFACEMETHODIMP_(UINT32) GetInputCount() const;
 
-	// Declare IUnknown implementation methods.
-	IFACEMETHODIMP_(ULONG) AddRef();
-	IFACEMETHODIMP_(ULONG) Release();
-	IFACEMETHODIMP QueryInterface(_In_ REFIID riid, _Outptr_ void** ppOutput);
+    // Declare IUnknown implementation methods.
+    IFACEMETHODIMP_(ULONG) AddRef();
+    IFACEMETHODIMP_(ULONG) Release();
+    IFACEMETHODIMP QueryInterface(_In_ REFIID riid, _Outptr_ void** ppOutput);
 private:
 
-	I420Effect();
-	HRESULT UpdateConstants();
+    I420Effect();
+    HRESULT UpdateConstants();
 
-	Microsoft::WRL::ComPtr<ID2D1VertexBuffer>   m_vertexBuffer;
-	UINT                                        m_numVertices;
-	Microsoft::WRL::ComPtr<ID2D1DrawInfo>       m_drawInfo;
+    Microsoft::WRL::ComPtr<ID2D1VertexBuffer>   m_vertexBuffer;
+    UINT                                        m_numVertices;
+    Microsoft::WRL::ComPtr<ID2D1DrawInfo>       m_drawInfo;
 
-	LONG                                        m_refCount;
-	D2D1_SIZE_U									m_displayedFrame;
-	D2D1_SIZE_U									m_visibleSize;
+    LONG                                        m_refCount;
+    D2D1_SIZE_U									m_displayedFrame;
+    D2D1_SIZE_U									m_visibleSize;
     D2D1_RECT_L                                 m_inputRects[3];
 
-	float										m_dpi;
+    float										m_dpi;
 
-	struct
-	{
-		uint32 displayedFrameWidth;
-		uint32 displayedFrameHeight;
-		uint32 visibleFrameWidth;
-		uint32 visibleFrameHeight;
-	} m_constants;
+    struct
+    {
+        uint32 displayedFrameWidth;
+        uint32 displayedFrameHeight;
+        uint32 visibleFrameWidth;
+        uint32 visibleFrameHeight;
+    } m_constants;
 };
--- modules/libdirect2d_winrt_plugin.Shared/I420Effect_VS.hlsl
+++ modules/libdirect2d_winrt_plugin.Shared/I420Effect_VS.hlsl
@@ -1,55 +1,55 @@
 cbuffer Direct2DTransforms : register(b0)
 {
-	float2x1 sceneToOutputX;
-	float2x1 sceneToOutputY;
-	float2x1 sceneToInput0X;
-	float2x1 sceneToInput0Y;
+    float2x1 sceneToOutputX;
+    float2x1 sceneToOutputY;
+    float2x1 sceneToInput0X;
+    float2x1 sceneToInput0Y;
 };
 
 cbuffer constants : register(b1)
 {
-	// size in pixel of the frame to draw to the screen
-	uint2 displayedFrameSize;
+    // size in pixel of the frame to draw to the screen
+    uint2 displayedFrameSize;
 
-	// size in pixel of the original frame (native video source resolution)
-	uint2 originalFrameSize;
+    // size in pixel of the original frame (native video source resolution)
+    uint2 originalFrameSize;
 };
 
 struct VSOut
 {
-	float4 clipSpaceOutput  : SV_POSITION;
-	float4 sceneSpaceOutput : SCENE_POSITION;
-	float4 texelSpaceInput0 : TEXCOORD0;
+    float4 clipSpaceOutput  : SV_POSITION;
+    float4 sceneSpaceOutput : SCENE_POSITION;
+    float4 texelSpaceInput0 : TEXCOORD0;
 };
 
 struct VSIn
 {
-	// Vertices position
-	float2 position : MESH_POSITION;
+    // Vertices position
+    float2 position : MESH_POSITION;
 };
 
 VSOut main(VSIn input)
 {
-	VSOut output;
+    VSOut output;
 
-	// Compute Scene-space output (vertex simply passed-through here). 
-	output.sceneSpaceOutput.x = displayedFrameSize.x * input.position.x;
-	output.sceneSpaceOutput.y = displayedFrameSize.y * input.position.y;
-	output.sceneSpaceOutput.z = 0.0f;
-	output.sceneSpaceOutput.w = 1.0f;
+    // Compute Scene-space output (vertex simply passed-through here). 
+    output.sceneSpaceOutput.x = displayedFrameSize.x * input.position.x;
+    output.sceneSpaceOutput.y = displayedFrameSize.y * input.position.y;
+    output.sceneSpaceOutput.z = 0.0f;
+    output.sceneSpaceOutput.w = 1.0f;
 
-	// Generate standard Clip-space output coordinates.
-	output.clipSpaceOutput.x = (output.sceneSpaceOutput.x * sceneToOutputX[0] + sceneToOutputX[1]);
-	output.clipSpaceOutput.y = (output.sceneSpaceOutput.y * sceneToOutputY[0] + sceneToOutputY[1]);
+    // Generate standard Clip-space output coordinates.
+    output.clipSpaceOutput.x = (output.sceneSpaceOutput.x * sceneToOutputX[0] + sceneToOutputX[1]);
+    output.clipSpaceOutput.y = (output.sceneSpaceOutput.y * sceneToOutputY[0] + sceneToOutputY[1]);
 
-	output.clipSpaceOutput.z = output.sceneSpaceOutput.z;
-	output.clipSpaceOutput.w = output.sceneSpaceOutput.w;
+    output.clipSpaceOutput.z = output.sceneSpaceOutput.z;
+    output.clipSpaceOutput.w = output.sceneSpaceOutput.w;
 
-	// Generate standard Texel-space input coordinates.
+    // Generate standard Texel-space input coordinates.
     output.texelSpaceInput0.x = (originalFrameSize.x * input.position.x * sceneToInput0X[0]) + sceneToInput0X[1];
     output.texelSpaceInput0.y = (originalFrameSize.y * input.position.y * sceneToInput0Y[0]) + sceneToInput0Y[1];
-	output.texelSpaceInput0.z = 1 - input.position.y;
-	output.texelSpaceInput0.w = 1;
+    output.texelSpaceInput0.z = 1 - input.position.y;
+    output.texelSpaceInput0.w = 1;
 
-	return output;
+    return output;
 }
