a8567bf17d69a94a2ecf0577d37b1d1ef3f22430
Thomas Nigro <tn@thomasnigro.fr>
2015-02-17 10:16:58 +0100

    On Windows the BottomMiniPlayer is always visible

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/BottomMiniPlayerVisibilityConverter.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/BottomMiniPlayerVisibilityConverter.cs
@@ -11,11 +11,15 @@ namespace VLC_WINRT_APP.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
+#if WINDOWS_PHONE_APP
             if (value is Type && (Type) value != typeof (MusicPlayerPage))
             {
                 return Visibility.Visible;
             }
             return Visibility.Collapsed;
+#else
+            return Visibility.Visible;
+#endif
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
