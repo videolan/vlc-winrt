373437513f15436899c5dd9fa7de8f24f6454f55
Thomas Nigro <tn@thomasnigro.fr>
2015-02-18 14:38:12 +0100

    If interaction on AlbumFlyout is made by opening a flyout, don't close the source

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MusicPages/AlbumFlyout.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/MusicPages/AlbumFlyout.xaml.cs
@@ -3,6 +3,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using VLC_WINRT_APP.Helpers;
+using WinRTXamlToolkit.Controls.Extensions;
 using AppBar = CustomAppBarDesktop.AppBar;
 
 namespace VLC_WINRT_APP.Views.MusicPages
@@ -22,6 +23,17 @@ namespace VLC_WINRT_APP.Views.MusicPages
 
         private void RootGrid_OnTapped(object sender, TappedRoutedEventArgs e)
         {
+            var s = e.OriginalSource as FrameworkElement;
+            if (s != null)
+            {
+                var button = s.GetFirstAncestorOfType<Button>();
+                if (button != null)
+                {
+                    var flyout = button.Flyout;
+                    if (flyout != null)
+                        return;
+                }
+            }
             this.Hide();
         }
     }
