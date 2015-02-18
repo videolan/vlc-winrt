90c58190ce65ec6ea2ea23682b49e232b0a76411
Thomas Nigro <tn@thomasnigro.fr>
2015-02-18 14:33:37 +0100

    Songs collection is more responsive

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MainMusicControls/SongsPivotItem.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MainMusicControls/SongsPivotItem.xaml.cs
@@ -2,6 +2,7 @@
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
 using VLC_WINRT_APP.Helpers;
 
 namespace VLC_WINRT_APP.Views.MainPages.MainMusicControls
@@ -11,6 +12,38 @@ namespace VLC_WINRT_APP.Views.MainPages.MainMusicControls
         public SongsPivotItem()
         {
             this.InitializeComponent();
+        }
+
+        private void Collection_Loaded(object sender, RoutedEventArgs e)
+        {
+#if WINDOWS_APP
+            RootGrid.Margin = new Thickness(24, 0, 0, 0);
+#endif
+            Window.Current.SizeChanged += Current_SizeChanged;
+            this.Unloaded += OnUnloaded;
+            Responsive();
+        }
+
+        void Current_SizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            Responsive();
+        }
+
+
+        void Responsive()
+        {
+            if (this.ActualWidth > 700)
+            {
+                VisualStateUtilities.GoToState(this, "Horizontal", false);
+            }
+            else
+            {
+                VisualStateUtilities.GoToState(this, "Vertical", false);
+            }
+        }
+        private void OnUnloaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Window.Current.SizeChanged -= Current_SizeChanged;
         }
 
         private void SemanticZoom_OnViewChangeCompleted(object sender, SemanticZoomViewChangedEventArgs e)
