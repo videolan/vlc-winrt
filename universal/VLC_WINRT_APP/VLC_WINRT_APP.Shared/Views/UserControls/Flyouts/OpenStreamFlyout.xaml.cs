59846c0fa33839ca862b4c1b3536225035e065e8
Thomas Nigro <tn@thomasnigro.fr>
2015-02-18 14:25:38 +0100

    Move OpenStreamFlyout in a usercontrol

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/UserControls/Flyouts/OpenStreamFlyout.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Controls;
+namespace VLC_WINRT_APP.Views.UserControls.Flyouts
+{
+    public sealed partial class OpenStreamFlyout : UserControl
+    {
+        public OpenStreamFlyout()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
