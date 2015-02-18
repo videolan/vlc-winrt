8a108109b38e61741d77f0e72b4374da1d03a3a7
Thomas Nigro <tn@thomasnigro.fr>
2015-02-18 17:04:27 +0100

    Play a track from a playlist

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/Music/PlayTrackCollCommand.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Commands/Music/PlayTrackCollCommand.cs
@@ -1,6 +1,9 @@
 ﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
 using VLC_WINRT.Common;
 using VLC_WINRT_APP.Helpers.MusicPlayer;
+using VLC_WINRT_APP.Model.Music;
+using VLC_WINRT_APP.ViewModels;
 using VLC_WINRT_APP.ViewModels.MusicVM;
 using VLC_WINRT_APP.Views.MusicPages;
 
@@ -10,13 +13,19 @@ namespace VLC_WINRT_APP.Commands.Music
     {
         public override async void Execute(object parameter)
         {
-            TrackCollection trackCollection = null;
-            if (parameter is TrackCollection)
-            {
-                trackCollection = parameter as TrackCollection;
-            }
+            var trackCollection = Locator.MusicLibraryVM.CurrentTrackCollection;
             if (trackCollection == null || trackCollection.Playlist == null || !trackCollection.Playlist.Any()) return;
-            await PlayMusicHelper.AddTrackCollectionToPlaylistAndPlay(trackCollection.Playlist);
+            if (parameter is int)
+            {
+                await PlayMusicHelper.AddTrackCollectionToPlaylistAndPlay(trackCollection.Playlist, true, (int) parameter);
+            }
+            else if (parameter is ItemClickEventArgs)
+            {
+                var track = (TrackItem)((ItemClickEventArgs) parameter).ClickedItem;
+                var index = trackCollection.Playlist.IndexOf(track);
+                await PlayMusicHelper.AddTrackCollectionToPlaylistAndPlay(trackCollection.Playlist, true, index);
+            }
+            else await PlayMusicHelper.AddTrackCollectionToPlaylistAndPlay(trackCollection.Playlist);
             if (App.ApplicationFrame.CurrentSourcePageType != typeof (MusicPlayerPage))
                 App.ApplicationFrame.Navigate(typeof (MusicPlayerPage));
         }
