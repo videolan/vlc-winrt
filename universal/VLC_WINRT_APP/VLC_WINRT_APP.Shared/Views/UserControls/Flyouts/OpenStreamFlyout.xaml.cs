1621e472bec7e1876f3bbd0aba28d31f7a3cb1bf
Thomas Nigro <tn@thomasnigro.fr>
2015-02-24 15:24:16 +0100

    Fix bug with null stream url parameter

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/UserControls/Flyouts/OpenStreamFlyout.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/UserControls/Flyouts/OpenStreamFlyout.xaml.cs
@@ -1,5 +1,8 @@
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
+using VLC_WINRT_APP.ViewModels;
+using WinRTXamlToolkit.Controls.Extensions;
 
 namespace VLC_WINRT_APP.Views.UserControls.Flyouts
 {
@@ -11,6 +14,12 @@ namespace VLC_WINRT_APP.Views.UserControls.Flyouts
 #if WINDOWS_PHONE_APP
             this.Placement = FlyoutPlacementMode.Full;
 #endif
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var urlTextBox = ((sender as Button).Parent as Grid).GetFirstDescendantOfType<TextBox>();
+            if (urlTextBox != null) Locator.VideoLibraryVM.PlayNetworkMRL.Execute(urlTextBox.Text);
         }
     }
 }
