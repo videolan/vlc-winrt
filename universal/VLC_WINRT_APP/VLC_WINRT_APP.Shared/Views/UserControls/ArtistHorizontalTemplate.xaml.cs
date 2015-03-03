bc4bc4579b15719d7206d2d9c2f0235fd9aefa8e
Thomas Nigro <tn@thomasnigro.fr>
2015-03-03 17:32:10 +0100

    Add ArtistHorizontalTemplate, remove unused ArtistTemplates

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/UserControls/ArtistHorizontalTemplate.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace VLC_WINRT_APP.Views.UserControls
+{
+    public sealed partial class ArtistHorizontalTemplate : UserControl
+    {
+        public ArtistHorizontalTemplate()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
