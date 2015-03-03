b0186e3aafbd082b604ab115bbdf8f2dad53344b
Thomas Nigro <tn@thomasnigro.fr>
2015-03-03 20:00:44 +0100

    Hide BackgroundImage when user is not in MusicPlayerPage

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/MusicPlayerBackgroundVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using VLC_WINRT_APP.ViewModels;
+using VLC_WINRT_APP.Views.MusicPages;
+
+namespace VLC_WINRT_APP.Converters
+{
+    public class MusicPlayerBackgroundVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (Locator.MainVM.CurrentPage == typeof (MusicPlayerPage))
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
