ba13da1d7cacf6b642072bd9d4a3b28e9a699378
Thomas Nigro <tn@thomasnigro.fr>
2015-03-05 16:12:06 +0100

    Add listview itemclick handling

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MusicPanes/ArtistCollectionPanes/TracksListView.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MusicPanes/ArtistCollectionPanes/TracksListView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Windows.UI.Xaml.Controls;
+using VLC_WINRT_APP.Model.Music;
+using VLC_WINRT_APP.ViewModels.MusicVM;
 
 namespace VLC_WINRT_APP.Views.MainPages.MusicPanes.ArtistCollectionPanes
 {
@@ -7,6 +9,23 @@ namespace VLC_WINRT_APP.Views.MainPages.MusicPanes.ArtistCollectionPanes
         public TracksListView()
         {
             this.InitializeComponent();
+        }
+
+        private void ListViewBase_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            var vm = this.DataContext as MusicLibraryVM;
+            if (vm != null)
+            {
+                vm.PlayAllSongsCommand.Execute(e);
+            }
+            else
+            {
+                var item = this.DataContext as AlbumItem;
+                if (item != null)
+                {
+                    item.TrackClicked.Execute(e);
+                }
+            }
         }
     }
 }
