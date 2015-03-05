217630e7c65d019296b99d6b108906d383a9c69d
Thomas Nigro <tn@thomasnigro.fr>
2015-03-05 14:33:09 +0100

    Add PlaylistControl, refactor xaml code, update slidercontrol layout

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MusicPages/MusicNowPlayingControls/PlaylistControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Controls;
+namespace VLC_WINRT_APP.Views.MusicPages.MusicNowPlayingControls
+{
+    public sealed partial class PlaylistControl : Grid
+    {
+        public PlaylistControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
