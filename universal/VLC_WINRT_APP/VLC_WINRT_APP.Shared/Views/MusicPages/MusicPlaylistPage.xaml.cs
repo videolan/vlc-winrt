025ccfea44113ac337320f8db92e2be3eebcf983
Thomas Nigro <tn@thomasnigro.fr>
2015-03-05 14:32:23 +0100

    Add MusicPlaylistPage page

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MusicPages/MusicPlaylistPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿#if WINDOWS_PHONE_APP
+using Windows.Phone.UI.Input;
+#endif
+using Windows.UI.Xaml.Controls;
+
+namespace VLC_WINRT_APP.Views.MusicPages
+{
+    public sealed partial class MusicPlaylistPage : Page
+    {
+        public MusicPlaylistPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MusicPlaylistPage_Loaded;
+        }
+
+        private void MusicPlaylistPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+#if WINDOWS_PHONE_APP
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+#endif
+            this.Unloaded += MusicPlaylistPage_Unloaded;
+        }
+
+#if WINDOWS_PHONE_APP
+        private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            e.Handled=true;
+            App.ApplicationFrame.GoBack();
+        }
+#endif
+
+        private void MusicPlaylistPage_Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+#if WINDOWS_PHONE_APP
+            HardwareButtons.BackPressed -= HardwareButtons_BackPressed;
+#endif
+        }
+    }
+}
