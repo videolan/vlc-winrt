64fe093763b331738f0dd05815a32b70736994ba
Thomas Nigro <tn@thomasnigro.fr>
2015-03-11 19:17:53 +0100

    BottomMiniPlayer is not visible when in musicplayerpage

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/BottomMiniPlayerVisibilityConverter.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/BottomMiniPlayerVisibilityConverter.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 using VLC_WINRT_APP.Views.MusicPages;
@@ -11,15 +9,11 @@ namespace VLC_WINRT_APP.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-#if WINDOWS_PHONE_APP
             if (value is Type && (Type) value != typeof (MusicPlayerPage))
             {
                 return Visibility.Visible;
             }
             return Visibility.Collapsed;
-#else
-            return Visibility.Visible;
-#endif
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
