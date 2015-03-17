4df0cf070fe86f0bd68071a87c63563a72afc68c
Thomas Nigro <tn@thomasnigro.fr>
2015-03-17 17:39:02 +0100

    Add StreamMedia object for Streams

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Model/Stream/StreamMedia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SQLite;
+using VLC_WINRT_APP.Common;
+
+namespace VLC_WINRT_APP.Model.Stream
+{
+    class StreamMedia : BindableBase, IVLCMedia
+    {
+        private string _filePath;
+        private TimeSpan _duration;
+        private string _title;
+
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        [Unique]
+        public string Path
+        {
+            get { return _filePath; }
+            set { SetProperty(ref _filePath, value); }
+        }
+        public string Name
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public TimeSpan Duration
+        {
+            get { return _duration; }
+            set { SetProperty(ref _duration, value); }
+        }
+
+        public StreamMedia(string mrl)
+        {
+            Path = mrl;
+        }
+    }
+}
