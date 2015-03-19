49f94b18be46e551187593d7769007866cfa400b
Thomas Nigro <thomasnigro@wifi-172-30-26-187.wifi.univ-paris5.fr>
2015-03-19 12:15:58 +0100

    Fix layout problem in AlbumCollection Base
    Buttons were over the Albums Combobox on small screens/snap view

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MusicPanes/MusicPanesControls/AlbumsCollectionButtons.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MusicPanes/MusicPanesControls/AlbumsCollectionButtons.xaml.cs
@@ -1,12 +1,44 @@
 ﻿
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 namespace VLC_WINRT_APP.Views.MainPages.MusicPanes.MusicPanesControls
 {
-    public sealed partial class AlbumsCollectionButtons : Grid
+    public sealed partial class AlbumsCollectionButtons : UserControl
     {
         public AlbumsCollectionButtons()
         {
             this.InitializeComponent();
+            this.Loaded += AlbumsCollectionButtons_Loaded;
+        }
+
+        void AlbumsCollectionButtons_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Responsive(Window.Current.Bounds.Width);
+            Window.Current.SizeChanged += Current_SizeChanged;
+            this.Unloaded += AlbumsCollectionButtons_Unloaded;
+        }
+
+        void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            Responsive(e.Size.Width);
+        }
+
+        void AlbumsCollectionButtons_Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Window.Current.SizeChanged -= Current_SizeChanged;
+        }
+
+        void Responsive(double width)
+        {
+#if WINDOWS_PHONE_APP
+            if (width < 470)
+#else
+            if (width < 570)
+#endif
+                VisualStateUtilities.GoToState(this, "Minimal", false);
+            else
+                VisualStateUtilities.GoToState(this, "Normal", false);
         }
     }
 }
