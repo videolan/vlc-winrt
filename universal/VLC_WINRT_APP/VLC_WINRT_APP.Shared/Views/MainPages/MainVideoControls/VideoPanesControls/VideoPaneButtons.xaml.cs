2f7f2e4d2f4b63d5e5643ca0e6c2fb3bb07e984e
Thomas Nigro <tn@thomasnigro.fr>
2015-03-19 17:46:09 +0100

    Remove namespace

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MainVideoControls/VideoPanesControls/VideoPaneButtons.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MainVideoControls/VideoPanesControls/VideoPaneButtons.xaml.cs
@@ -1,20 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
-
+﻿using Windows.UI.Xaml.Controls;
 namespace VLC_WINRT_APP.Views.MainPages.MainVideoControls.VideoPanesControls
 {
     public sealed partial class VideoPaneButtons : UserControl
