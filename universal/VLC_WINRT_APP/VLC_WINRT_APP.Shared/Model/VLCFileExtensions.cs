9f5997933e659b731861cc431ff617943b074e40
Thomas Nigro <thomasnigro@MacBook-Pro-de-Thomas.local>
2015-03-20 15:46:39 +0100

    Remove .mkv and flac in the MFSupported
    So we don't try to fetch video thumbnails from MF

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Model/VLCFileExtensions.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Model/VLCFileExtensions.cs
@@ -15,9 +15,6 @@ namespace VLC_WINRT_APP.Model
             ".wma",
             ".aac",
             ".m4a",
-            // on w10, mkv and flac are supported...
-            ".flac",
-            ".mkv"
         };
 
         public static List<string> Supported = new List<string>()
