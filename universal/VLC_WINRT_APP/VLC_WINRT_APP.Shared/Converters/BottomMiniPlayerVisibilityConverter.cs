22f05251062f906db56a7cd763c1218f0bd507cb
Thomas Nigro <tn@thomasnigro.fr>
2015-03-23 16:16:39 +0100

    Don't display BottomMiniPlayer when user is playing a video

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/BottomMiniPlayerVisibilityConverter.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/BottomMiniPlayerVisibilityConverter.cs
@@ -2,6 +2,7 @@
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 using VLC_WINRT_APP.Views.MusicPages;
+using VLC_WINRT_APP.Views.VideoPages;
 
 namespace VLC_WINRT_APP.Converters
 {
@@ -9,7 +10,7 @@ namespace VLC_WINRT_APP.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value is Type && (Type) value != typeof (MusicPlayerPage))
+            if (value is Type && (Type) value != typeof (MusicPlayerPage) && (Type)value != typeof(VideoPlayerPage))
             {
                 return Visibility.Visible;
             }
