1e35c83ad12b39df1da5736f5bf9ca8d0fa6e4e3
Thomas Nigro <tn@thomasnigro.fr>
2015-03-24 05:24:29 +0100

    Add MusicViewVisibilityConverter

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/MusicViewVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using VLC_WINRT_APP.Model.Music;
+
+namespace VLC_WINRT_APP.Converters
+{
+    public class MusicViewVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is MusicView)
+            {
+                var view = (MusicView) value;
+                var viewName = view.ToString().ToLower();
+                if (viewName == (string) parameter)
+                    return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
