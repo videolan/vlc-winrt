2a36cecf455ad1de8b0c7b10fb1de367eae794f6
Thomas Nigro <tn@thomasnigro.fr>
2015-03-24 05:58:17 +0100

    Add TrackItemTemplateDetailed control

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/UserControls/TrackItemTemplateDetailed.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using VLC_WINRT_APP.Model.Music;
+
+namespace VLC_WINRT_APP.Views.UserControls
+{
+    public sealed partial class TrackItemTemplateDetailed : UserControl
+    {
+        public TrackItemTemplateDetailed()
+        {
+            this.InitializeComponent();
+        }
+        private void Grid_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+#if WINDOWS_APP
+            Flyout.ShowAttachedFlyout((Grid)sender);
+#endif
+        }
+
+        private void NameTextBlock_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var trackItem = this.DataContext as TrackItem;
+            var b = new Binding
+            {
+                Converter = App.Current.Resources["CurrentTrackEnhancifierConverter"] as IValueConverter,
+                ConverterParameter = ((TextBlock)sender).Foreground,
+                ConverterLanguage = "color",
+                Source = trackItem,
+                Path = new PropertyPath("IsCurrentPlaying"),
+            };
+            ((TextBlock)sender).SetBinding(TextBlock.ForegroundProperty, b);
+        }
+    }
+}
