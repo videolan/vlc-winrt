cb715738527a94bf46da4da1d2a2abb198e934a5
Thomas Nigro <tn@thomasnigro.fr>
2015-03-24 07:04:30 +0100

    Add LastFmScrobbler project
    (cherry picked from commit e0d66db38804253174268261769089c161ce80bc)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.LastFmScrobbler/LastFmScrobblerHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using IF.Lastfm.Core.Api;
+
+namespace VLC_WINRT_APP.LastFmScrobbler
+{
+    public sealed class LastFmScrobblerHelper
+    {
+        private LastAuth _auth;
+        public bool IsConnected { get; private set; }
+
+        public LastFmScrobblerHelper(string apiKey, string apiSig)
+        {
+            _auth = new LastAuth(apiKey, apiSig);
+        }
+
+        public IAsyncOperation<bool> ConnectOperation(string account, string password)
+        {
+            return Connect(account, password).AsAsyncOperation();
+        }
+
+        internal async Task<bool> Connect(string account, string password)
+        {
+            var lastResponse = await _auth.GetSessionTokenAsync(account, password);
+            IsConnected = lastResponse.Success && _auth.Authenticated;
+            return IsConnected;
+        }
+
+        public async void ScrobbleTrack(string artist, string album, string track)
+        {
+            var trackApi = new TrackApi(_auth);
+            var response = await trackApi.ScrobbleAsync(new Scrobble(artist, album, track, DateTimeOffset.Now));
+            if (response.Success)
+            {
+                Debug.WriteLine("Scrobble success!");
+            }
+            else
+            {
+                Debug.WriteLine("Scrobble failed!");
+            }
+        }
+    }
+}
