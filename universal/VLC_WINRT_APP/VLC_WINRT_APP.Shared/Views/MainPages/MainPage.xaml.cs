c826be12ed94a9e68e6ef337e847ff5a00bb07f3
Thomas Nigro <tn@thomasnigro.fr>
2015-03-25 20:23:29 +0100

    Move and enable the Settings commands for Windows

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MainPage.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MainPage.xaml.cs
@@ -87,46 +87,5 @@ namespace VLC_WINRT_APP.Views.MainPages
         {
             Window.Current.SizeChanged -= Current_SizeChanged;
         }
-
-        private void MainFrame_OnNavigated(object sender, NavigationEventArgs e)
-        {
-            Responsive();
-#if WINDOWS_APP
-            SettingsPane pane = SettingsPane.GetForCurrentView();
-            pane.CommandsRequested += SettingsCommandRequested;
-#endif
-        }
-
-#if WINDOWS_APP
-        private void SettingsCommandRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
-        {
-            ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView();
-            var privacyCommand = new SettingsCommand("privacy", resourceLoader.GetString("PrivacyStatement"),
-                async h => await Launcher.LaunchUriAsync(new Uri("http://videolan.org/vlc/privacy.html")));
-
-            var specialThanks = new SettingsCommand("specialThanks", resourceLoader.GetString("SpecialThanks"),
-                command =>
-                {
-                    App.ApplicationFrame.Navigate(typeof(SpecialThanks));
-                });
-
-            var settings = new SettingsCommand("settings", resourceLoader.GetString("Settings"),
-                command =>
-                {
-                    var settingsFlyout = new SettingsFlyout();
-                    settingsFlyout.Show();
-                });
-            var license = new SettingsCommand("license", "License", command =>
-            {
-                var licenseFlyout = new LicenseFlyout();
-                licenseFlyout.Show();
-            });
-            args.Request.ApplicationCommands.Clear();
-            args.Request.ApplicationCommands.Add(privacyCommand);
-            args.Request.ApplicationCommands.Add(specialThanks);
-            args.Request.ApplicationCommands.Add(settings);
-            args.Request.ApplicationCommands.Add(license);
-        }
-#endif
     }
 }
--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/UserControls/ShellContent.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Windows/Views/UserControls/ShellContent.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.ApplicationModel.Resources;
+using Windows.System;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using VLC_WINRT_APP.Views.VariousPages;
 
 namespace VLC_WINRT_APP.Views.UserControls
 {
@@ -8,5 +14,45 @@ namespace VLC_WINRT_APP.Views.UserControls
         {
             this.InitializeComponent();
         }
+
+        private void MainFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+#if WINDOWS_APP
+            SettingsPane pane = SettingsPane.GetForCurrentView();
+            pane.CommandsRequested += SettingsCommandRequested;
+#endif
+        }
+
+#if WINDOWS_APP
+        private void SettingsCommandRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView();
+            var privacyCommand = new SettingsCommand("privacy", resourceLoader.GetString("PrivacyStatement"),
+                async h => await Launcher.LaunchUriAsync(new Uri("http://videolan.org/vlc/privacy.html")));
+
+            var specialThanks = new SettingsCommand("specialThanks", resourceLoader.GetString("SpecialThanks"),
+                command =>
+                {
+                    App.ApplicationFrame.Navigate(typeof(SpecialThanks));
+                });
+
+            var settings = new SettingsCommand("settings", resourceLoader.GetString("Settings"),
+                command =>
+                {
+                    var settingsFlyout = new SettingsFlyout();
+                    settingsFlyout.Show();
+                });
+            var license = new SettingsCommand("license", "License", command =>
+            {
+                var licenseFlyout = new LicenseFlyout();
+                licenseFlyout.Show();
+            });
+            args.Request.ApplicationCommands.Clear();
+            args.Request.ApplicationCommands.Add(privacyCommand);
+            args.Request.ApplicationCommands.Add(specialThanks);
+            args.Request.ApplicationCommands.Add(settings);
+            args.Request.ApplicationCommands.Add(license);
+        }
+#endif
     }
 }
