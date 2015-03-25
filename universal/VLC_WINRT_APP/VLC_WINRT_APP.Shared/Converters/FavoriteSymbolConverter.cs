ddcfef589b399a77547d0d4c7dbcd663139f7dc8
Thomas Nigro <tn@thomasnigro.fr>
2015-03-25 20:22:13 +0100

    Add missing namespaces

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/FavoriteSymbolConverter.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/FavoriteSymbolConverter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
 using VLC_WINRT_APP.Helpers;
+using VLC_WINRT_APP.Utils;
 
 namespace VLC_WINRT_APP.Converters
 {
