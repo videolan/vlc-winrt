ca5b796f053eba4c3989450ef35ce9a3929a14ab
Thomas Nigro <tn@thomasnigro.fr>
2015-03-25 18:44:56 +0100

    Add BooleanToMainColor Converter

---
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Converters/BooleanToMainColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace VLC_WINRT_APP.Converters
+{
+    public class BooleanToMainColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if ((bool) value)
+            {
+                return App.Current.Resources["InactiveMainColor"];
+            }
+            return App.Current.Resources["MainColor"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
