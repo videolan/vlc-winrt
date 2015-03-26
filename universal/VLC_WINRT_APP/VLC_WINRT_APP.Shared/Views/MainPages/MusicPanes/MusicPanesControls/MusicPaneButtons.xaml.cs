a1f6125d4f48259a2738ea9037b726557e538680
Thomas Nigro <tn@thomasnigro.fr>
2015-03-26 17:44:57 +0000

    Add new buttons and searchbox

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MusicPanes/MusicPanesControls/MusicPaneButtons.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MusicPanes/MusicPanesControls/MusicPaneButtons.xaml.cs
@@ -1,12 +1,40 @@
-﻿using Windows.UI.Xaml;
+﻿using Microsoft.Xaml.Interactivity;
+using VLC_WINRT_APP.ViewModels;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 namespace VLC_WINRT_APP.Views.MainPages.MusicPanes.MusicPanesControls
 {
-    public sealed partial class MusicPaneButtons : Grid
+    public sealed partial class MusicPaneButtons : UserControl
     {
         public MusicPaneButtons()
         {
             this.InitializeComponent();
+            this.Loaded += VideoPaneButtons_Loaded;
+        }
+
+        void VideoPaneButtons_Loaded(object sender, RoutedEventArgs e)
+        {
+            Responsive(Window.Current.Bounds.Width);
+            Window.Current.SizeChanged += Current_SizeChanged;
+            this.Unloaded += MusicPaneButtons_Unloaded;
+        }
+
+        void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            Responsive(e.Size.Width);
+        }
+
+        void MusicPaneButtons_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.SizeChanged -= Current_SizeChanged;
+        }
+
+        void Responsive(double width)
+        {
+            if (width <= 650)
+                VisualStateUtilities.GoToState(this, "Minimal", false);
+            else
+                VisualStateUtilities.GoToState(this, "Normal", false);
         }
 
         private void RefreshButton_Loaded(object sender, RoutedEventArgs e)
@@ -14,6 +42,13 @@ namespace VLC_WINRT_APP.Views.MainPages.MusicPanes.MusicPanesControls
 #if WINDOWS_PHONE_APP
             (sender as AppBarButton).Visibility = Visibility.Collapsed;
 #endif
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(MusicSearchBox.Text) && !string.IsNullOrEmpty(Locator.MusicLibraryVM.SearchTag))
+                Locator.MainVM.ChangeMainPageMusicViewCommand.Execute((int)Locator.SettingsVM.MusicView);
+            Locator.MusicLibraryVM.SearchTag = MusicSearchBox.Text;
         }
     }
 }
