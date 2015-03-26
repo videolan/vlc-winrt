80032d7b06934b70f4f9244603fb418db4e86893
Thomas Nigro <tn@thomasnigro.fr>
2015-03-26 11:24:05 +0000

    Update VideoPaneButton layout, add SearchBox and RefreshButton

--- universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MainVideoControls/VideoPanesControls/VideoPaneButtons.xaml.cs
+++ universal/VLC_WINRT_APP/VLC_WINRT_APP.Shared/Views/MainPages/MainVideoControls/VideoPanesControls/VideoPaneButtons.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Microsoft.Xaml.Interactivity;
+using VLC_WINRT_APP.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
 namespace VLC_WINRT_APP.Views.MainPages.MainVideoControls.VideoPanesControls
 {
     public sealed partial class VideoPaneButtons : UserControl
@@ -6,6 +10,39 @@ namespace VLC_WINRT_APP.Views.MainPages.MainVideoControls.VideoPanesControls
         public VideoPaneButtons()
         {
             this.InitializeComponent();
+            this.Loaded += VideoPaneButtons_Loaded;
+        }
+
+        void VideoPaneButtons_Loaded(object sender, RoutedEventArgs e)
+        {
+            Responsive(Window.Current.Bounds.Width);
+            Window.Current.SizeChanged += Current_SizeChanged;
+            this.Unloaded += VideoPaneButtons_Unloaded;
+        }
+
+        void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            Responsive(e.Size.Width);
+        }
+
+        void VideoPaneButtons_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.SizeChanged -= Current_SizeChanged;
+        }
+
+        void Responsive(double width)
+        {
+            if (width <= 550)
+                VisualStateUtilities.GoToState(this, "Minimal", false);
+            else
+                VisualStateUtilities.GoToState(this, "Normal", false);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(VideoSearchBox.Text) && !string.IsNullOrEmpty(Locator.VideoLibraryVM.SearchTag))
+                Locator.MainVM.ChangeMainPageVideoViewCommand.Execute((int)Locator.SettingsVM.VideoView);
+            Locator.VideoLibraryVM.SearchTag = VideoSearchBox.Text;
         }
     }
 }
