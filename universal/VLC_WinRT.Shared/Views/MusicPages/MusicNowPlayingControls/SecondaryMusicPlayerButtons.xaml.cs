eddcfc472e638c554b4e6161316e0b0eb4f5320b
Thomas Nigro <tn@thomasnigro.fr>
2015-03-31 20:41:48 +0200

    Update MiniPlayer layout

--- universal/VLC_WinRT.Shared/Views/MusicPages/MusicNowPlayingControls/SecondaryMusicPlayerButtons.xaml.cs
+++ universal/VLC_WinRT.Shared/Views/MusicPages/MusicNowPlayingControls/SecondaryMusicPlayerButtons.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace VLC_WinRT.Views.MusicPages.MusicNowPlayingControls
 {
@@ -7,6 +8,27 @@ namespace VLC_WinRT.Views.MusicPages.MusicNowPlayingControls
         public SecondaryMusicPlayerButtons()
         {
             this.InitializeComponent();
+        }
+        
+        public static readonly DependencyProperty FontSizeProperty = DependencyProperty.Register(
+            "FontSize", typeof (double), typeof (SecondaryMusicPlayerButtons), new PropertyMetadata(30, FontSizePropertyChangedCallback));
+
+        private static void FontSizePropertyChangedCallback(DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var that = (SecondaryMusicPlayerButtons)dependencyObject;
+            var size = (double)dependencyPropertyChangedEventArgs.NewValue;
+            that.ShuffleFontIcon.FontSize = size;
+            that.ShareFontIcon.FontSize = size;
+            that.FavoriteFontIcon.FontSize = size;
+            that.PlaylistPath.Height = size - 2;
+            that.PlaylistPath.Width = size - 2;
+        }
+
+        public double FontSize
+        {
+            get { return (double) GetValue(FontSizeProperty); }
+            set { SetValue(FontSizeProperty, value); }
         }
     }
 }
--- universal/VLC_WinRT.Shared/Views/UserControls/BottomMiniPlayer.xaml.cs
+++ universal/VLC_WinRT.Shared/Views/UserControls/BottomMiniPlayer.xaml.cs
@@ -50,7 +50,7 @@ namespace VLC_WinRT.Views.UserControls
             RootGrid.Height = 60;
 
 #else
-            RootGrid.Height = 50;
+            RootGrid.Height = 85;
 #endif
             if (this.ActualWidth > 700)
                 VisualStateUtilities.GoToState(this, "FullWindows", false);
