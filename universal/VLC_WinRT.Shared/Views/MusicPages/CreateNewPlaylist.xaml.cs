923d82f513b21ca6febf54d1c2e5079dfac45b18
Thomas Nigro <tn@thomasnigro.fr>
2015-04-01 17:51:53 +0200

    Add forgotten file

---
+++ universal/VLC_WinRT.Shared/Views/MusicPages/CreateNewPlaylist.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VLC_WinRT.Helpers.MusicLibrary;
+
+namespace VLC_WinRT.Views.MusicPages
+{
+    public sealed partial class CreateNewPlaylist : UserControl
+    {
+        public CreateNewPlaylist()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void AddToCollection_Click(object sender, RoutedEventArgs e)
+        {
+            await MusicLibraryManagement.AddNewPlaylist(playlistName.Text);
+        }
+    }
+}
