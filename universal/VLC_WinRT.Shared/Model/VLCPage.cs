e4d4a8835b49065d52766c4568799e19ab2a9ec9
Thomas Nigro <tn@thomasnigro.fr>
2015-04-01 19:55:40 +0200

    Add VLCPage enum, and basic navigationservice

---
+++ universal/VLC_WinRT.Shared/Model/VLCPage.cs
@@ -0,0 +1,17 @@
+﻿namespace VLC_WinRT.Model
+{
+    public enum VLCPage
+    {
+        MainPageHome,
+        MainPageVideo,
+        MainPageMusic,
+        MainPageFileExplorer,
+        AlbumPage,
+        ArtistPage,
+        PlaylistPage,
+        CurrentPlaylistPage,
+        VideoPlayerPage,
+        MusicPlayerPage,
+        SettingsPage,
+    }
+}
---
+++ universal/VLC_WinRT.Shared/Services/RunTime/NavigationService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using VLC_WinRT.Model;
+#if WINDOWS_PHONE_APP
+using Windows.Phone.UI.Input;
+#endif
+
+namespace VLC_WinRT.Services.RunTime
+{
+    public class NavigationService
+    {
+        public VLCPage CurrentPage;
+
+        public NavigationService()
+        {
+#if WINDOWS_PHONE_APP
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+#endif    
+        }
+
+#if WINDOWS_PHONE_APP
+        private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            switch (CurrentPage)
+            {
+                case VLCPage.MainPageHome:
+                    break;
+                case VLCPage.MainPageVideo:
+                    break;
+                case VLCPage.MainPageMusic:
+                    break;
+                case VLCPage.MainPageFileExplorer:
+                    break;
+                case VLCPage.AlbumPage:
+                    break;
+                case VLCPage.ArtistPage:
+                    break;
+                case VLCPage.PlaylistPage:
+                    break;
+                case VLCPage.CurrentPlaylistPage:
+                    break;
+                case VLCPage.VideoPlayerPage:
+                    break;
+                case VLCPage.MusicPlayerPage:
+                    break;
+                case VLCPage.SettingsPage:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+#endif
+    }
+}
