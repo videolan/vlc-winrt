f26711d70a086f3b4054498b2137c3d6ff876e55
Thomas Nigro <tn@thomasnigro.fr>
2015-04-14 15:18:24 -0700

    Update PivotHeader visual

--- universal/VLC_WinRT.Shared/Views/UserControls/PivotHeaderControl.xaml.cs
+++ universal/VLC_WinRT.Shared/Views/UserControls/PivotHeaderControl.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Microsoft.Xaml.Interactivity;
+using VLC_WinRT.Model;
+using Windows.UI.Text;
+using System.Diagnostics;
 
 namespace VLC_WinRT.Views.UserControls
 {
@@ -12,6 +15,53 @@ namespace VLC_WinRT.Views.UserControls
             Responsive();
             Window.Current.SizeChanged += Current_SizeChanged;
         }
+
+        public Model.Panel Panel
+        {
+            get { return (Model.Panel)GetValue(PanelProperty); }
+            set
+            {
+                SetValue(PanelProperty, value);
+            }
+        }
+
+        void SetPanel(Model.Panel p)
+        {
+            Icon.Glyph = p.PathData;
+            Title.Text = p.Title;
+            SetCurrentPanelInterface();
+            p.PropertyChanged += Value_PropertyChanged;
+        }
+
+        void SetCurrentPanelInterface()
+        {
+            if (Panel.IsCurrent)
+            {
+                Title.Opacity = 1;
+                Icon.Opacity = 1;
+            }
+            else
+            {
+                Title.Opacity = 0.5;
+                Icon.Opacity = 0.5;
+            }
+        }
+
+        private void Value_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            SetCurrentPanelInterface();
+        }
+
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PanelProperty = DependencyProperty.Register("Panel", typeof(Model.Panel), typeof(PivotHeaderControl), new PropertyMetadata(default(Model.Panel), PanelPropertyChangedCallback));
+
+        private static void PanelPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var that = (PivotHeaderControl)dependencyObject;
+            that.SetPanel((Model.Panel)dependencyPropertyChangedEventArgs.NewValue);
+        }
+
         void Responsive()
         {
             if (Window.Current.Bounds.Width < 600)
