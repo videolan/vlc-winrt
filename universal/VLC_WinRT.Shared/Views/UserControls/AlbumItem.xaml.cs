35700cb2f50d37cd20a2c138693933c7a438268a
Thomas Nigro <tn@thomasnigro.fr>
2015-04-21 03:13:52 -0700

    Add Mouse Over interactions for AlbumItem

--- universal/VLC_WinRT.Shared/Views/UserControls/AlbumItem.xaml.cs
+++ universal/VLC_WinRT.Shared/Views/UserControls/AlbumItem.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Diagnostics;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 
@@ -10,11 +11,7 @@ namespace VLC_WinRT.Views.UserControls
         {
             this.InitializeComponent();
         }
-
-        private void RootAlbumItem_Loaded(object sender, RoutedEventArgs e)
-        {
-        }
-
+        
         private void RootAlbumItem_Holding(object sender, HoldingRoutedEventArgs e)
         {
             Flyout.ShowAttachedFlyout((Grid)sender);
@@ -23,6 +20,18 @@ namespace VLC_WinRT.Views.UserControls
         private void Grid_RightTapped(object sender, RightTappedRoutedEventArgs e)
         {
             Flyout.ShowAttachedFlyout((Grid)sender);
+        }
+
+        private void Cover_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.Pointer.PointerDeviceType != Windows.Devices.Input.PointerDeviceType.Mouse) return;
+            VisualStateManager.GoToState(this, "MouseOver", false);
+        }
+
+        private void Cover_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.Pointer.PointerDeviceType != Windows.Devices.Input.PointerDeviceType.Mouse) return;
+            VisualStateManager.GoToState(this, "Normal", false);
         }
     }
 }
