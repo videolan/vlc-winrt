4b1b60c414153879215676a286130dc2f3ab758f
Thomas Nigro <tn@thomasnigro.fr>
2015-04-23 15:43:16 +0200

    Add Magic Numbers file in Utilities

---
+++ universal/VLC_WinRT.Shared/Utils/Numbers.cs
@@ -0,0 +1,11 @@
+﻿namespace VLC_WinRT.Utils
+{
+    /// <summary>
+    /// Magic numbers belong here
+    /// </summary>
+    public static class Numbers
+    {
+        // Under this number, list should not display SemanticZoom (letters or anything), only a flat list
+        public static readonly int SemanticZoomItemCountThreshold = 25;
+    }
+}
