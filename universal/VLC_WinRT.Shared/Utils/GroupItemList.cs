30473d216a26e562b56ecc8a24a74f1d803cf9ef
Thomas Nigro <tn@thomasnigro.fr>
2015-05-18 19:49:32 +0200

    Add GroupItemList custom ObservableCollection

---
+++ universal/VLC_WinRT.Shared/Utils/GroupItemList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace VLC_WinRT.Utils
+{
+    public class GroupItemList<T> : ObservableCollection<T>
+    {
+        public GroupItemList(T item) : base(new[] { item })
+        {
+
+        }
+
+        public GroupItemList()
+        {
+
+        }
+
+        public string Key { get; set; }
+
+        public new IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)base.GetEnumerator();
+        }
+    }
+}
