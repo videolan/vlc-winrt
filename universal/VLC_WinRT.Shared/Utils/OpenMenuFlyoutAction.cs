170d825d63d546113ad73a5854d930d5952dd186
Thomas Nigro <tn@thomasnigro.fr>
2015-05-19 16:50:57 +0200

    Code cosmetic

--- universal/VLC_WinRT.Shared/Utils/OpenMenuFlyoutAction.cs
+++ universal/VLC_WinRT.Shared/Utils/OpenMenuFlyoutAction.cs
@@ -19,9 +19,7 @@ namespace VLC_WinRT.Utils
         {
             var senderElement = sender as FrameworkElement;
             var flyoutBase = FlyoutBase.GetAttachedFlyout(senderElement);
-
             flyoutBase.ShowAt(senderElement);
-
             return null;
         }
     }
