d06d0cf80b19e55240ee32f672d8deed6799f953
Thomas Nigro <tn@thomasnigro.fr>
2015-06-09 16:25:53 +0200

    Move SharedBackground from Shared project to PCL
    This is unfortunately needed because BackgroundAudioPlayer project must be a Windows Runtime Component and not a PCL. So, it can't handle classes like SQLite.cs which contain types PCL doesn't want or something like this.

---
+++ universal/VLC_WinRT.ShareBackground/Database/BackgroundTrackDatabase.cs
@@ -0,0 +1,76 @@
+﻿using SQLite.Net;
+using SQLite.Net.Platform.WinRT;
+using SQLite.Net.Async;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using VLC_WinRT.BackgroundAudioPlayer.Model;
+
+namespace VLC_WinRT.SharedBackground.Database
+{
+    public class BackgroundTrackDatabase
+    {
+        private SQLitePlatformWinRT platform = new SQLitePlatformWinRT();
+        private SQLiteConnectionWithLock connectionLock;
+        private static readonly string DbPath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "background.sqlite");
+
+        public BackgroundTrackDatabase()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            connectionLock = new SQLiteConnectionWithLock(platform, new SQLiteConnectionString(DbPath, false));
+            var connection = new SQLiteAsyncConnection(() => connectionLock);
+            connection.CreateTableAsync<BackgroundTrackItem>();
+        }
+
+        public void Drop()
+        {
+            connectionLock = new SQLiteConnectionWithLock(platform, new SQLiteConnectionString(DbPath, false));
+            var connection = new SQLiteAsyncConnection(() => connectionLock);
+            connection.DropTableAsync<BackgroundTrackItem>();
+        }
+
+        public Task Add(BackgroundTrackItem track)
+        {
+            connectionLock = new SQLiteConnectionWithLock(platform, new SQLiteConnectionString(DbPath, false));
+            var connection = new SQLiteAsyncConnection(() => connectionLock);
+            return connection.InsertAsync(track);
+        }
+
+        public Task AddBunchTracks(dynamic tracks)
+        {
+            connectionLock = new SQLiteConnectionWithLock(platform, new SQLiteConnectionString(DbPath, false));
+            var connection = new SQLiteAsyncConnection(() => connectionLock);
+            return connection.InsertAllAsync(tracks);
+        }
+
+        public List<BackgroundTrackItem> LoadPlaylist()
+        {
+            using (var c = new SQLiteConnection(platform, DbPath))
+            {
+                var list = new List<BackgroundTrackItem>(c.Table<BackgroundTrackItem>().ToList());
+                return list;
+            }
+        }
+
+
+        public Task Remove(BackgroundTrackItem track)
+        {
+            connectionLock = new SQLiteConnectionWithLock(platform, new SQLiteConnectionString(DbPath, false));
+            var connection = new SQLiteAsyncConnection(() => connectionLock);
+            return connection.DeleteAsync(track);
+        }
+
+        public void Clear()
+        {
+            using (var c = new SQLiteConnection(platform, DbPath))
+            {
+                c.Execute("DELETE FROM BackgroundTrackItem");
+            }
+        }
+    }
+}
