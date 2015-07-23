0b89ae7cfe07c1df6e7b53a81f7f5c2347c0a402
Thomas Nigro <tn@thomasnigro.fr>
2015-07-23 16:40:10 +0200

    Remove unused file

--- universal/VLC_WinRT.UI.Legacy/Views/MainPages/MusicPanes/ArtistCollectionBase.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/MainPages/MusicPanes/ArtistCollectionBase.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Microsoft.Xaml.Interactivity;
+using VLC_WinRT.ViewModels;
+using VLC_WinRT.Utils;
+using Windows.UI.Xaml.Data;
 
 namespace VLC_WinRT.Views.MainPages.MusicPanes
 {
@@ -9,6 +12,25 @@ namespace VLC_WinRT.Views.MainPages.MusicPanes
         public ArtistCollectionBase()
         {
             this.InitializeComponent();
+            this.Loaded += ArtistCollectionBase_Loaded;
+        }
+
+        async void ArtistCollectionBase_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Locator.MusicLibraryVM.MusicCollectionLoaded.Task;
+            if (Locator.MusicLibraryVM.Artists.Count > Numbers.SemanticZoomItemCountThreshold)
+            {
+                var b = new Binding();
+                b.Mode = BindingMode.OneWay;
+                b.Source = this.Resources["GroupArtists"] as CollectionViewSource;
+                ArtistListView.SetBinding(ListView.ItemsSourceProperty, b);
+                SemanticZoom.IsZoomOutButtonEnabled = true;
+            }
+        }
+
+        private void SemanticZoom_OnViewChangeCompleted(object sender, SemanticZoomViewChangedEventArgs e)
+        {
+            ArtistsZoomedOutView.ItemsSource = GroupArtists.View.CollectionGroups;
         }
     }
 }
