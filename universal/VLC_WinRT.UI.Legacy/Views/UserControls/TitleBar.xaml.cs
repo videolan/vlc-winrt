f4ee15830026c5d1fbd4aaffb8f51ea91bbb00bc
Thomas Nigro <tn@thomasnigro.fr>
2015-08-06 23:49:51 +0200

    fix HI-DPI bug

--- universal/VLC_WinRT.UI.Legacy/Views/UserControls/TitleBar.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/UserControls/TitleBar.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.Graphics.Display;
+using Windows.UI.Xaml.Controls;
 namespace VLC_WinRT.UI.Legacy.Views.UserControls
 {
     public sealed partial class TitleBar : UserControl
@@ -6,6 +8,7 @@ namespace VLC_WinRT.UI.Legacy.Views.UserControls
         public TitleBar()
         {
             this.InitializeComponent();
+            this.Height = Math.Floor(32 * (DisplayInformation.GetForCurrentView().LogicalDpi / 100));
         }
     }
 }
