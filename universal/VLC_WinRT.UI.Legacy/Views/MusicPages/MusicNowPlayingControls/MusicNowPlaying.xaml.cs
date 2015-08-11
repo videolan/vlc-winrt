e3f6dee014820db5dc48253e8af45c353bb6c651
Thomas Nigro <tn@thomasnigro.fr>
2015-08-11 17:24:36 +0200

    Simplify MusicPlayer UI, one layer deleted
    Fix #15217 #15231

--- universal/VLC_WinRT.UI.Legacy/Views/MusicPages/MusicNowPlayingControls/MusicNowPlaying.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/MusicPages/MusicNowPlayingControls/MusicNowPlaying.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using System.ComponentModel;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Microsoft.Xaml.Interactivity;
@@ -50,6 +51,42 @@ namespace VLC_WinRT.Views.MusicPages.MusicNowPlayingControls
             Locator.MediaPlaybackViewModel.PlayOrPauseCommand.Execute(null);
             await Locator.MediaPlaybackViewModel.CleanViewModel();
             Locator.NavigationService.GoBack_Default();
+        }
+
+        private void MusicNowPlaying_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MusicNowPlayingFlipView.SelectedIndex = Locator.MediaPlaybackViewModel.TrackCollection.CurrentTrack;
+                Locator.MediaPlaybackViewModel.TrackCollection.PropertyChanged += TrackCollectionOnPropertyChanged;
+                this.Unloaded += MusicNowPlayingOnUnloaded;
+                MusicNowPlayingFlipView.SelectionChanged += MusicNowPlaying_OnSelectionChanged;
+            }
+            catch { }
+        }
+
+        private void MusicNowPlayingOnUnloaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Locator.MediaPlaybackViewModel.TrackCollection.PropertyChanged -= TrackCollectionOnPropertyChanged;
+        }
+
+        private void TrackCollectionOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName == "CurrentTrack")
+            {
+                if (MusicNowPlayingFlipView.Items != null && (Locator.MediaPlaybackViewModel.TrackCollection.CurrentTrack == -1 || Locator.MediaPlaybackViewModel.TrackCollection.CurrentTrack < MusicNowPlayingFlipView.Items.Count))
+                    MusicNowPlayingFlipView.SelectedIndex = Locator.MediaPlaybackViewModel.TrackCollection.CurrentTrack;
+            }
+        }
+
+        private async void MusicNowPlaying_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Locator.MediaPlaybackViewModel.TrackCollection.CurrentTrack == -1) return;
+            // TODO: Code a better way to do this >_>' This is obviously messed up (The whole thing actually)
+            if (MusicNowPlayingFlipView.SelectedIndex == Locator.MediaPlaybackViewModel.TrackCollection.CurrentTrack + 1)
+                await Locator.MediaPlaybackViewModel.PlayNext();
+            else if (MusicNowPlayingFlipView.SelectedIndex == Locator.MediaPlaybackViewModel.TrackCollection.CurrentTrack - 1)
+                await Locator.MediaPlaybackViewModel.PlayPrevious();
         }
     }
 }
