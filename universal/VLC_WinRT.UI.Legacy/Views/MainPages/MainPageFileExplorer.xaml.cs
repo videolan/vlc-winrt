ab62d0c17df1043df6910fad83f91a9e0f53f5bd
Thomas Nigro <tn@thomasnigro.fr>
2015-08-14 19:30:21 +0200

    Update MainPageFileExplorer design

--- universal/VLC_WinRT.UI.Legacy/Views/MainPages/MainPageFileExplorer.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/MainPages/MainPageFileExplorer.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 using Microsoft.Xaml.Interactivity;
+using VLC_WinRT.ViewModels;
 
 namespace VLC_WinRT.Views.MainPages
 {
@@ -9,6 +11,49 @@ namespace VLC_WinRT.Views.MainPages
         public MainPageFileExplorer()
         {
             this.InitializeComponent();
+            this.Loaded += MainPageFileExplorer_Loaded;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Locator.Slideshow.GoDefaultPic();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            Locator.Slideshow.RestoreSlideshow();
+        }
+
+
+        private void MainPageFileExplorer_Loaded(object sender, RoutedEventArgs e)
+        {
+            Responsive();
+            this.SizeChanged += OnSizeChanged;
+            this.Unloaded += OnUnloaded;
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs)
+        {
+            Responsive();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            this.SizeChanged -= OnSizeChanged;
+        }
+
+        private void Responsive()
+        {
+            if (Window.Current.Bounds.Width < 550)
+            {
+                VisualStateUtilities.GoToState(this, "Narrow", false);
+            }
+            else
+            {
+                VisualStateUtilities.GoToState(this, "Wide", false);
+            }
         }
     }
 }
