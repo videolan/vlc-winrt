11c2e8f478658a2e2cf4259009f60250100f7e58
Thomas Nigro <tn@thomasnigro.fr>
2015-08-17 13:58:32 +0200

    Fix Invalid Cast Exception

--- universal/VLC_WinRT.Shared/Commands/VLCFileExplorer/PlayFolderCommand.cs
+++ universal/VLC_WinRT.Shared/Commands/VLCFileExplorer/PlayFolderCommand.cs
@@ -7,6 +7,7 @@ using VLC_WinRT.Helpers.MusicLibrary;
 using VLC_WinRT.Model.Video;
 using VLC_WinRT.Utils;
 using VLC_WinRT.Helpers;
+using VLC_WinRT.Model.FileExplorer;
 
 namespace VLC_WinRT.Commands.VLCFileExplorer
 {
@@ -14,26 +15,26 @@ namespace VLC_WinRT.Commands.VLCFileExplorer
     {
         public override async void Execute(object parameter)
         {
-            var items = (ObservableCollection<IStorageItem>)parameter;
-            var files = new List<StorageFile>();
+            var items = (ObservableCollection<IVLCStorageItem>)parameter;
+            var files = new List<VLCStorageFile>();
             foreach (var item in items)
             {
-                var file = item as StorageFile;
-                if (file != null && VLCFileExtensions.Supported.Contains(file.FileType.ToLower()))
-                    files.Add((StorageFile)item);
+                var file = item as VLCStorageFile;
+                if (file != null && VLCFileExtensions.Supported.Contains((file.StorageItem as StorageFile).FileType.ToLower()))
+                    files.Add((VLCStorageFile)item);
             }
             var playlist = new ObservableCollection<IVLCMedia>();
             foreach (var file in files)
             {
-                if(VLCFileExtensions.AudioExtensions.Contains(file.FileType.ToLower()))
+                if(VLCFileExtensions.AudioExtensions.Contains((file.StorageItem as StorageFile).FileType.ToLower()))
                 {
-                    var trackItem = await MusicLibraryManagement.GetTrackItemFromFile(file);
+                    var trackItem = await MusicLibraryManagement.GetTrackItemFromFile(file.StorageItem as StorageFile);
                     playlist.Add(trackItem);
                 }
-                else if(VLCFileExtensions.VideoExtensions.Contains(file.FileType.ToLower()))
+                else if(VLCFileExtensions.VideoExtensions.Contains((file.StorageItem as StorageFile).FileType.ToLower()))
                 {
                     var videoVm = new VideoItem();
-                    await videoVm.Initialize(file);
+                    await videoVm.Initialize((file.StorageItem as StorageFile));
                     playlist.Add(videoVm);
                 }
             }
