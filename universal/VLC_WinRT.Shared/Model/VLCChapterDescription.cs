65eae14368e1e3fd7277b4ae0bae677bdc415314
Thomas Nigro <tn@thomasnigro.fr>
2015-08-25 11:46:28 +0200

    Add VLC ChapterDescription wrapper

---
+++ universal/VLC_WinRT.Shared/Model/VLCChapterDescription.cs
@@ -0,0 +1,19 @@
+﻿using libVLCX;
+using System;
+
+namespace VLC_WinRT.Model
+{
+    public class VLCChapterDescription
+    {
+        public string Name { get; set; }
+        public TimeSpan StarTime { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public VLCChapterDescription(ChapterDescription chapter)
+        {
+            Name = chapter.name();
+            StarTime = TimeSpan.FromMilliseconds(chapter.startTime());
+            Duration = TimeSpan.FromMilliseconds(chapter.duration());
+        }
+    }
+}
