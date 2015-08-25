aee584379222d96fd025eba934efddc4bc12b2f3
Thomas Nigro <tn@thomasnigro.fr>
2015-08-25 11:46:06 +0200

    Add SurfaceZoom enum

---
+++ universal/VLC_WinRT.Shared/Model/Video/VLCSurfaceZoom.cs
@@ -0,0 +1,13 @@
+﻿namespace VLC_WinRT.Model.Video
+{
+    public enum VLCSurfaceZoom
+    {
+        SURFACE_BEST_FIT = 0,
+        SURFACE_FIT_HORIZONTAL = 1,
+        SURFACE_FIT_VERTICAL = 2,
+        SURFACE_FILL = 3,
+        SURFACE_16_9 = 4,
+        SURFACE_4_3 = 5,
+        SURFACE_ORIGINAL = 6,
+    }
+}
