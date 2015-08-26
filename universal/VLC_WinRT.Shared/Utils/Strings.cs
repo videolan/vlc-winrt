d96b84b6a0a8e0d33a300c29b29f9ee8e74e1a63
Thomas Nigro <tn@thomasnigro.fr>
2015-08-26 14:32:34 +0200

    Change font everywhere

--- universal/VLC_WinRT.Shared/Utils/Strings.cs
+++ universal/VLC_WinRT.Shared/Utils/Strings.cs
@@ -17,7 +17,7 @@ namespace VLC_WinRT.Utils
 
         public static readonly string FeedbackMailAdress = "modernvlc@outlook.com";
 
-        public static readonly string ModernFont = "ms-appx:SEGMDL2.TTF#Segoe MDL2 Assets";
+        public static readonly string ModernFont = "ms-appx:VLC.ttf#W10";
         public static readonly string MusicDatabase = Path.Combine(ApplicationData.Current.LocalFolder.Path, "mediavlc.sqlite");
         public static readonly string VideoDatabase = Path.Combine(ApplicationData.Current.LocalFolder.Path, "mediavlcVideos.sqlite");
         public static readonly string SettingsDatabase = Path.Combine(ApplicationData.Current.LocalFolder.Path, "settings.sqlite");
