e8089f4670bb39c2d3ed3ddf6edf48fa1beb74b6
Thomas Nigro <tn@thomasnigro.fr>
2015-08-26 16:58:45 +0200

    MainPageVideos now has the same Pivot has MainPageMusic

--- universal/VLC_WinRT.UI.Legacy/Views/MainPages/MainPageVideos.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/MainPages/MainPageVideos.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using VLC_WinRT.ViewModels;
+using Microsoft.Xaml.Interactivity;
 
 namespace VLC_WinRT.Views.MainPages
 {
@@ -20,6 +21,32 @@ namespace VLC_WinRT.Views.MainPages
         {
             InitializeComponent();
             this.NavigationCacheMode = NavigationCacheMode.Enabled;
+            this.Loaded += MainPageMusic_Loaded;
+        }
+
+        void MainPageMusic_Loaded(object sender, RoutedEventArgs e)
+        {
+            Responsive(Window.Current.Bounds.Width);
+            Window.Current.SizeChanged += Current_SizeChanged;
+            this.Unloaded += AlbumsCollectionButtons_Unloaded;
+        }
+
+        void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            Responsive(e.Size.Width);
+        }
+
+        void AlbumsCollectionButtons_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.SizeChanged -= Current_SizeChanged;
+        }
+
+        void Responsive(double width)
+        {
+            if (width <= 600)
+                VisualStateUtilities.GoToState(this, "Narrow", false);
+            else
+                VisualStateUtilities.GoToState(this, "Wide", false);
         }
 
         private void MainPageVideoContentPresenter_OnLoaded(object sender, RoutedEventArgs e)
