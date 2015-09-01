d968148224a517fe40719d348c33d781096ec2e2
Thomas Nigro <tn@thomasnigro.fr>
2015-09-01 16:47:32 +0200

    Add a non-SQL property describing the keys used for this KeyboardAction

--- universal/VLC_WinRT.Shared/Model/KeyboardAction.cs
+++ universal/VLC_WinRT.Shared/Model/KeyboardAction.cs
@@ -5,6 +5,8 @@ namespace VLC_WinRT.Model
 {
     public class KeyboardAction
     {
+        private string keyDes;
+
         [NotNull]
         public VirtualKey MainKey { get; set; }
 
@@ -12,5 +14,23 @@ namespace VLC_WinRT.Model
 
         [NotNull, PrimaryKey]
         public VLCAction Action { get; set; }
+
+        [Ignore]
+        public string KeyDescription
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(keyDes))
+                {
+                    keyDes= MainKey.ToString();
+                    if (SecondKey != VirtualKey.None)
+                    {
+                        keyDes += " + ";
+                        keyDes += SecondKey.ToString();
+                    }
+                }
+                return keyDes;
+            }
+        }
     }
 }
