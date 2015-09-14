9778f7ae6fddfeb58fe5a79ebabdd640019ce82a
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-09-14 17:35:41 +0200

    Thumbnailer: Fail early if no libvlc instance is available

--- wrapper/libVLCX/Thumbnailer.cpp
+++ wrapper/libVLCX/Thumbnailer.cpp
@@ -153,80 +153,80 @@ static void Unlock(void *opaque, void *picture, void *const *pixels){
 
 IAsyncOperation<PreparseResult^>^ Thumbnailer::TakeScreenshot(Platform::String^ mrl, int width, int height, int timeoutMs)
 {
+    if (!p_instance)
+        return nullptr;
     return concurrency::create_async([&] (concurrency::cancellation_token ct) {
         thumbnailer_sys_t *sys = new thumbnailer_sys_t();
         auto completionTask = concurrency::create_task(sys->screenshotCompleteEvent, ct);
-        if (p_instance){
-            size_t len2 = WideCharToMultiByte( CP_UTF8, 0, mrl->Data(), -1, nullptr, 0, nullptr, nullptr );
-            char* mrl_str = new char[len2];
-            WideCharToMultiByte( CP_UTF8, 0, mrl->Data(), -1, mrl_str, len2, nullptr, nullptr );
+        size_t len2 = WideCharToMultiByte( CP_UTF8, 0, mrl->Data(), -1, nullptr, 0, nullptr, nullptr );
+        char* mrl_str = new char[len2];
+        WideCharToMultiByte( CP_UTF8, 0, mrl->Data(), -1, mrl_str, len2, nullptr, nullptr );
 
-            libvlc_media_t* m = libvlc_media_new_location(this->p_instance, mrl_str);
-            delete[] mrl_str;
+        libvlc_media_t* m = libvlc_media_new_location(this->p_instance, mrl_str);
+        delete[] mrl_str;
 
-            /* Set media to fast with no options */
-            libvlc_media_add_option(m, ":no-audio");
-            libvlc_media_add_option(m, ":no-spu");
-            libvlc_media_add_option(m, ":no-osd");
-            libvlc_media_add_option(m, ":avcodec-hw=none");
-            libvlc_media_add_option( m, ":aout=none" );
+        /* Set media to fast with no options */
+        libvlc_media_add_option(m, ":no-audio");
+        libvlc_media_add_option(m, ":no-spu");
+        libvlc_media_add_option(m, ":no-osd");
+        libvlc_media_add_option(m, ":avcodec-hw=none");
+        libvlc_media_add_option( m, ":aout=none" );
 
-            libvlc_media_player_t* mp = libvlc_media_player_new_from_media(m);
-            libvlc_media_release(m);
-            sys->eventMgr = libvlc_media_player_event_manager(mp);
-            libvlc_event_attach(sys->eventMgr, libvlc_MediaPlayerEncounteredError, &CancelPreparse, sys);
-            // Workaround some short and weird samples can reach the end without getting a snapshot generated.
-            libvlc_event_attach(sys->eventMgr, libvlc_MediaPlayerEndReached, &CancelPreparse, sys);
-            // to know when seeking to the right position is done
-            libvlc_event_attach( sys->eventMgr, libvlc_MediaPlayerPositionChanged, &onSeekChanged, sys );
+        libvlc_media_player_t* mp = libvlc_media_player_new_from_media(m);
+        libvlc_media_release(m);
+        sys->eventMgr = libvlc_media_player_event_manager(mp);
+        libvlc_event_attach(sys->eventMgr, libvlc_MediaPlayerEncounteredError, &CancelPreparse, sys);
+        // Workaround some short and weird samples can reach the end without getting a snapshot generated.
+        libvlc_event_attach(sys->eventMgr, libvlc_MediaPlayerEndReached, &CancelPreparse, sys);
+        // to know when seeking to the right position is done
+        libvlc_event_attach( sys->eventMgr, libvlc_MediaPlayerPositionChanged, &onSeekChanged, sys );
 
-            /* Set the video format and the callbacks. */
+        /* Set the video format and the callbacks. */
 
-            unsigned int pitch = width * PIXEL_SIZE;
-            sys->thumbHeight = height;
-            sys->thumbWidth = width;
-            sys->thumbSize = pitch * sys->thumbHeight;
-            sys->thumbData.reset(new char[sys->thumbSize]);
-            sys->hLock = CreateEventEx(nullptr, nullptr, CREATE_EVENT_INITIAL_SET, EVENT_MODIFY_STATE);
-            sys->mp = mp;
+        unsigned int pitch = width * PIXEL_SIZE;
+        sys->thumbHeight = height;
+        sys->thumbWidth = width;
+        sys->thumbSize = pitch * sys->thumbHeight;
+        sys->thumbData.reset(new char[sys->thumbSize]);
+        sys->hLock = CreateEventEx(nullptr, nullptr, CREATE_EVENT_INITIAL_SET, EVENT_MODIFY_STATE);
+        sys->mp = mp;
 
-            libvlc_video_set_format(mp, "BGRA", sys->thumbWidth, sys->thumbHeight, pitch);
-            libvlc_video_set_callbacks(mp, Lock, Unlock, nullptr, (void*) sys);
-            sys->state = THUMB_SEEKING;
+        libvlc_video_set_format(mp, "BGRA", sys->thumbWidth, sys->thumbHeight, pitch);
+        libvlc_video_set_callbacks(mp, Lock, Unlock, nullptr, (void*) sys);
+        sys->state = THUMB_SEEKING;
 
-            // Create a local representation to allow it to be captured
-            auto sce = sys->screenshotCompleteEvent;
-            sys->cancellationTask = concurrency::create_task( [sce, timeoutMs] {
-                concurrency::wait( timeoutMs );
-                if( !concurrency::is_task_cancellation_requested() )
-                    sce.set( nullptr );
-            }, sys->timeoutCts.get_token() );
+        // Create a local representation to allow it to be captured
+        auto sce = sys->screenshotCompleteEvent;
+        sys->cancellationTask = concurrency::create_task( [sce, timeoutMs] {
+            concurrency::wait( timeoutMs );
+            if( !concurrency::is_task_cancellation_requested() )
+                sce.set( nullptr );
+        }, sys->timeoutCts.get_token() );
 
-            if (mp)
-            {
-                libvlc_media_player_play(mp);
-                //TODO: Specify position
-                libvlc_media_player_set_position(mp, SEEK_POSITION);
-            }
-
-            completionTask.then([mp, sys](PreparseResult^ res) {
-                if ( res != nullptr )
-                    res->length = libvlc_media_player_get_length(mp);
-                sys->timeoutCts.cancel();
-                // event attachment cleanup
-                libvlc_event_detach( sys->eventMgr, libvlc_MediaPlayerPositionChanged, &onSeekChanged, sys );
-                // We rendered at least one frame, no need to check for EOF anymore
-                libvlc_event_detach(sys->eventMgr, libvlc_MediaPlayerEndReached, &CancelPreparse, sys);
-                libvlc_event_detach(sys->eventMgr, libvlc_MediaPlayerEncounteredError, &CancelPreparse, sys);
-                // We only block the unlock function if state == THUMB_SEEKED. In the worst case, we are rendering
-                // a thumbnail now (even if we timed out but the rendering started just before the timeout task triggered)
-                // If so, this will probably take a bit more time, but will not block, so we're safe stopping the media player.
-                libvlc_media_player_stop(mp);
-                libvlc_media_player_release(mp);
-                CloseHandle(sys->hLock);
-                delete sys;
-            });
+        if (mp)
+        {
+            libvlc_media_player_play(mp);
+            //TODO: Specify position
+            libvlc_media_player_set_position(mp, SEEK_POSITION);
         }
+
+        completionTask.then([mp, sys](PreparseResult^ res) {
+            if ( res != nullptr )
+                res->length = libvlc_media_player_get_length(mp);
+            sys->timeoutCts.cancel();
+            // event attachment cleanup
+            libvlc_event_detach( sys->eventMgr, libvlc_MediaPlayerPositionChanged, &onSeekChanged, sys );
+            // We rendered at least one frame, no need to check for EOF anymore
+            libvlc_event_detach(sys->eventMgr, libvlc_MediaPlayerEndReached, &CancelPreparse, sys);
+            libvlc_event_detach(sys->eventMgr, libvlc_MediaPlayerEncounteredError, &CancelPreparse, sys);
+            // We only block the unlock function if state == THUMB_SEEKED. In the worst case, we are rendering
+            // a thumbnail now (even if we timed out but the rendering started just before the timeout task triggered)
+            // If so, this will probably take a bit more time, but will not block, so we're safe stopping the media player.
+            libvlc_media_player_stop(mp);
+            libvlc_media_player_release(mp);
+            CloseHandle(sys->hLock);
+            delete sys;
+        });
         return completionTask;
     });
 }
