036a522fcb2a04969dbfb2a89b6d3ac26f3d0659
Thomas Nigro <tn@thomasnigro.fr>
2015-09-23 16:39:07 +0200

    Move UCs to Shell folder, add MenuDropdown control

---
+++ universal/VLC_WinRT.UI.UWP/Views/UserControls/Shell/MenuDropdown.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour en savoir plus sur le modèle d'élément Contrôle utilisateur, consultez la page http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace VLC_WinRT.UI.UWP.Views.UserControls.Shell
+{
+    public sealed partial class MenuDropdown : UserControl
+    {
+        public MenuDropdown()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
