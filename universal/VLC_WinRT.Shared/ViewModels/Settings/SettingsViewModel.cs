95c312adfaa0528812c6bcd99f62886135fa176a
Thomas Nigro <tn@thomasnigro.fr>
2015-10-01 20:11:57 +0200

    Fix build

--- universal/VLC_WinRT.Shared/ViewModels/Settings/SettingsViewModel.cs
+++ universal/VLC_WinRT.Shared/ViewModels/Settings/SettingsViewModel.cs
@@ -9,6 +9,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.System;
