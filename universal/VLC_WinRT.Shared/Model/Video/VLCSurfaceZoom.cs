b566b44796a774f84d700c46613722b4a85c103a
Thomas Nigro <tn@thomasnigro.fr>
2015-10-02 22:37:10 +0200

    Comment not-implemented enum values

--- universal/VLC_WinRT.Shared/Model/Video/VLCSurfaceZoom.cs
+++ universal/VLC_WinRT.Shared/Model/Video/VLCSurfaceZoom.cs
@@ -5,10 +5,10 @@
         SURFACE_BEST_FIT = 0,
         SURFACE_FIT_HORIZONTAL = 1,
         SURFACE_FIT_VERTICAL = 2,
-        SURFACE_FILL = 3,
-        SURFACE_16_9 = 4,
-        SURFACE_4_3 = 5,
-        SURFACE_ORIGINAL = 6,
-        SURFACE_CUSTOM_ZOOM = 7,
+        //SURFACE_FILL = 3,
+        //SURFACE_16_9 = 4,
+        //SURFACE_4_3 = 5,
+        //SURFACE_ORIGINAL = 6,
+        //SURFACE_CUSTOM_ZOOM = 7,
     }
 }
