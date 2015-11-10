4899df4777ec40e761d9188763c95cec1b910740
Thomas Nigro <tn@thomasnigro.fr>
2015-11-10 18:13:15 +0100

    Remove old design code and duplicates

--- universal/VLC_WinRT.UI.Legacy/Views/MusicPages/ArtistPageControls/MainArtistHeader.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/MusicPages/ArtistPageControls/MainArtistHeader.xaml.cs
@@ -11,47 +11,10 @@ using WinRTXamlToolkit.Controls.Extensions;
 namespace VLC_WinRT.Views.MusicPages.ArtistPageControls
 {
     public sealed partial class MainArtistHeader : UserControl
-    {
-        ContentPresenter parent;
-
-        private static readonly ResourceLoader _resourcesLoader = ResourceLoader.GetForViewIndependentUse();
-
+    {   
         public MainArtistHeader()
         {
             this.InitializeComponent();
-        }
-
-        void SwitchBetweenViewsButton_Click(object sender, RoutedEventArgs e)
-        {
-            if(IsArtistAlbumsList(parent))
-                parent.Content = new ArtistTracksGroupedList();
-            else parent.Content = new ArtistAlbumsList();
-        }
-
-        private void SwitchBetweenViewsButton_Loaded(object sender, RoutedEventArgs e)
-        {
-            parent = GetContentPresenter();
-            if (IsArtistAlbumsList(parent))
-            {
-                SwitchBetweenViewsButton.Label = Strings.Tracks;
-                SwitchBetweenViewsGlyph.Glyph = App.Current.Resources["DropdownSymbol"].ToString();
-            }
-            else
-            {
-                SwitchBetweenViewsGlyph.Glyph = App.Current.Resources["AlbumDiscSymbol"].ToString();
-                SwitchBetweenViewsButton.Label = Strings.Albums;
-            }
-        }
-
-        bool IsArtistAlbumsList(ContentPresenter cP)
-        {
-            return cP.Content is ArtistAlbumsList;
-        }
-
-        ContentPresenter GetContentPresenter()
-        {
-            var artistPage = this.GetFirstAncestorOfType<ArtistPageBase>();
-            return artistPage.FindName("ArtistPageBaseContent") as ContentPresenter;
         }
     }
 }
