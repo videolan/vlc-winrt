7b870442eb262e0af217b4adc3c6487458d34036
Thomas Nigro <tn@thomasnigro.fr>
2015-11-10 14:14:51 +0100

    Update Sidebar UI

--- universal/VLC_WinRT.UI.Legacy/Views/UserControls/Shell/SideBar.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/UserControls/Shell/SideBar.xaml.cs
@@ -7,40 +7,6 @@ namespace VLC_WinRT.Views.UserControls
         public SideBar()
         {
             this.InitializeComponent();
-            this.Loaded += SideBar_Loaded;
-        }
-
-        private void SideBar_Loaded(object sender, RoutedEventArgs e)
-        {
-            Window.Current.SizeChanged += Current_SizeChanged;
-            this.Unloaded += SideBar_Unloaded;
-            Responsive();
-        }
-
-        private void SideBar_Unloaded(object sender, RoutedEventArgs e)
-        {
-            Window.Current.SizeChanged -= Current_SizeChanged;
-        }
-
-        private void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
-        {
-            Responsive();
-        }
-
-        void Responsive()
-        {
-            if (Window.Current.Bounds.Width < 550)
-            {
-                SidebarGrid.Width = 0;
-            }
-            else if (Window.Current.Bounds.Width < 800)
-            {
-                SidebarGrid.Width = 64;
-            }
-            else
-            {
-                SidebarGrid.Width = 250;
-            }
         }
     }
 }
