5734c4b67d42147aceed903847ef1503294d974f
Thomas Nigro <tn@thomasnigro.fr>
2015-11-30 19:59:53 +0100

    Use Dependency Property "trackitem" to display actual info
    This is supposed to improve XAML performances. In fact, we don't use
    proper bindings which work using reflection, so we save some CPU cycles.
    Also, this patch changes the design a bit (with an icon explaining the track
    is playing), and obviously fixes some old bugs due to previous poor
    performances and unreliable architecture).

--- universal/VLC_WinRT.UI.Legacy/Views/UserControls/TrackItemTemplateDetailed.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/UserControls/TrackItemTemplateDetailed.xaml.cs
@@ -1,13 +1,22 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using VLC_WinRT.Model.Music;
+using VLC_WinRT.Utils;
+using VLC_WinRT.ViewModels;
+using VLC_WinRT.ViewModels.MusicVM;
 
 namespace VLC_WinRT.Views.UserControls
 {
     public sealed partial class TrackItemTemplateDetailed : UserControl
     {
+        private Brush previousBrush = null;
         public TrackItemTemplateDetailed()
         {
             this.InitializeComponent();
@@ -22,6 +31,31 @@ namespace VLC_WinRT.Views.UserControls
         // Using a DependencyProperty as the backing store for IsFlyoutEnabled.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IsFlyoutEnabledProperty =
             DependencyProperty.Register("IsFlyoutEnabled", typeof(bool), typeof(TrackItemTemplateDetailed), new PropertyMetadata(true));
+        
+        public TrackItem Track
+        {
+            get { return (TrackItem)GetValue(TrackProperty); }
+            set { SetValue(TrackProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Track.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TrackProperty =
+            DependencyProperty.Register("Track", typeof(TrackItem), typeof(TrackItemTemplateDetailed), new PropertyMetadata(null, PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var that = (TrackItemTemplateDetailed)dependencyObject;
+            that.Init();
+        }
+
+        public void Init()
+        {
+            NameTextBlock.Text = Track.Name;
+            ArtistNameTextBlock.Text = Strings.HumanizedArtistName(Track.ArtistName);
+            AlbumNameTextBlock.Text = Strings.HumanizedAlbumName(Track.AlbumName);
+            Track.PropertyChanged += TrackItemOnPropertyChanged;
+            UpdateTrack();
+        }
 
         private void Grid_RightTapped(object sender, RightTappedRoutedEventArgs e)
         {
@@ -31,18 +65,36 @@ namespace VLC_WinRT.Views.UserControls
 
         private void NameTextBlock_OnLoaded(object sender, RoutedEventArgs e)
         {
-            var trackItem = this.DataContext as TrackItem;
-            var b = new Binding
-            {
-                Converter = App.Current.Resources["CurrentTrackEnhancifierConverter"] as IValueConverter,
-                ConverterParameter = ((TextBlock)sender).Foreground,
-                ConverterLanguage = "color",
-                Source = trackItem,
-                Path = new PropertyPath("IsCurrentPlaying"),
-            };
-            ((TextBlock)sender).SetBinding(TextBlock.ForegroundProperty, b);
+            this.Unloaded += TrackItemTemplateDetailed_Unloaded;
         }
 
+        private void TrackItemTemplateDetailed_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Track.PropertyChanged -= TrackItemOnPropertyChanged;
+        }
+
+        private void TrackItemOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName == nameof(TrackItem.IsCurrentPlaying))
+            {
+                UpdateTrack();
+            }
+        }
+
+        void UpdateTrack()
+        {
+            if (Track.IsCurrentPlaying)
+            {
+                previousBrush = NameTextBlock.Foreground;
+                NameTextBlock.Foreground = (Brush)App.Current.Resources["MainColor"];
+                MusicLogo.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MusicLogo.Visibility = Visibility.Collapsed;
+                if (previousBrush != null) NameTextBlock.Foreground = previousBrush;
+            }
+        }
         private void Grid_Holding(object sender, HoldingRoutedEventArgs e)
         {
             if (IsFlyoutEnabled)
