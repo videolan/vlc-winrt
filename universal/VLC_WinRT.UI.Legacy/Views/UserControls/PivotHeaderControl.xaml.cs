1e92629dc90510f37219fdc1a66381b080375a6d
Thomas Nigro <tn@thomasnigro.fr>
2015-12-01 17:09:39 +0100

    Use Dependency Property for PivotHeader binding

--- universal/VLC_WinRT.UI.Legacy/Views/UserControls/PivotHeaderControl.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/UserControls/PivotHeaderControl.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.Xaml.Interactivity;
 using VLC_WinRT.Model;
 using Windows.UI.Text;
 using System.Diagnostics;
+using VLC_WinRT.ViewModels;
 
 namespace VLC_WinRT.Views.UserControls
 {
@@ -12,13 +13,75 @@ namespace VLC_WinRT.Views.UserControls
         public PivotHeaderControl()
         {
             this.InitializeComponent();
+            this.Loaded += PivotHeaderControl_Loaded;
             Responsive();
             Window.Current.SizeChanged += Current_SizeChanged;
         }
         
+
+        public Model.Panel Panel
+        {
+            get { return (Model.Panel)GetValue(PanelProperty); }
+            set { SetValue(PanelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Panel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PanelProperty =
+            DependencyProperty.Register("Panel", typeof(Model.Panel), typeof(PivotHeaderControl), new PropertyMetadata(null, PropertyChangedCallback));
+
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var that = (PivotHeaderControl)dependencyObject;
+            that.Init();
+        }
+
+        public void Init()
+        {
+            Icon.Glyph = Panel.DefaultIcon;
+            Title.Text = Panel.Title;
+            UpdatePivot();
+        }
+
+        private void PivotHeaderControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Locator.MainVM.PropertyChanged += MainVM_PropertyChanged;
+            this.Unloaded += PivotHeaderControl_Unloaded;
+        }
+
+        private void PivotHeaderControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Locator.MainVM.PropertyChanged -= MainVM_PropertyChanged;
+        }
+
+        private void MainVM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(MainVM.CurrentPanel))
+            {
+                UpdatePivot();
+            }
+        }
+
+        void UpdatePivot()
+        {
+            if (Panel.Index == Locator.MainVM.CurrentPanel.Index)
+            {
+                Icon.Glyph = Panel.FilledIcon;
+            }
+            else
+            {
+                Icon.Glyph = Panel.DefaultIcon;
+            }
+        }
+
         void Responsive()
         {
-            if (Window.Current.Bounds.Width < 1000)
+            if (Window.Current.Bounds.Width < 700)
+            {
+                VisualStateUtilities.GoToState(this, "Snap", false);
+                this.Width = (Window.Current.Bounds.Width - 60) / Locator.MainVM.Panels.Count;
+            }
+            else if (Window.Current.Bounds.Width < 1000)
             {
                 VisualStateUtilities.GoToState(this, "HalfSnap", false);
             }
