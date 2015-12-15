5cad214d8dd6828edc6e41f144ca0288c8a05bf9
Thomas Nigro <tn@thomasnigro.fr>
2015-12-15 02:52:55 +0100

    Artist tempalte now uses Dependency Property

--- universal/VLC_WinRT.UI.Legacy/Views/UserControls/ArtistHorizontalTemplate.xaml.cs
+++ universal/VLC_WinRT.UI.Legacy/Views/UserControls/ArtistHorizontalTemplate.xaml.cs
@@ -1,4 +1,11 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VLC_WinRT.Model.Music;
+using VLC_WinRT.Utils;
+using VLC_WinRT.ViewModels;
 
 namespace VLC_WinRT.Views.UserControls
 {
@@ -7,6 +14,34 @@ namespace VLC_WinRT.Views.UserControls
         public ArtistHorizontalTemplate()
         {
             this.InitializeComponent();
+        }
+
+        public ArtistItem Artist
+        {
+            get { return (ArtistItem)GetValue(ArtistProperty); }
+            set { SetValue(ArtistProperty, value); }
+        }
+
+        public static readonly DependencyProperty ArtistProperty =
+            DependencyProperty.Register(nameof(Artist), typeof(ArtistItem), typeof(ArtistHorizontalTemplate), new PropertyMetadata(0, PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var that = (ArtistHorizontalTemplate)dependencyObject;
+            that.Init();
+        }
+
+
+        public void Init()
+        {
+            NameTextBlock.Text = Strings.HumanizedArtistName(Artist.Name);
+            var id = Artist.Id;
+            Task.Run(async () =>
+            {
+                var albumsCount = await Locator.MusicLibraryVM.MusicLibrary.LoadAlbumsCount(id);
+                await App.Dispatcher.RunAsync(CoreDispatcherPriority.Low,
+                        () => AlbumsCountTextBlock.Text = albumsCount + " " + Strings.Albums);
+            });
         }
     }
 }
