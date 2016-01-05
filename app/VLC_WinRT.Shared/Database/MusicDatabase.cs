40fbffbf69909eb0e9b68a8f09e882cf99b5bf90
Thomas Nigro <thomas.nigro@outlook.com>
2016-01-05 11:47:02 +0100

    Add Music Database mutex:

---
+++ app/VLC_WinRT.Shared/Database/MusicDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VLC_WinRT.Database
+{
+    public static class MusicDatabase
+    {
+        public static readonly SemaphoreSlim DatabaseOperation = new SemaphoreSlim(1);
+    }
+}
