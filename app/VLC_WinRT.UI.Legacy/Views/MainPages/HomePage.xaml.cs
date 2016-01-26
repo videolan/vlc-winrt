c0f5d680981a735a737981c80afefe8e3597da79
Thomas Nigro <tn@thomasnigro.fr>
2016-01-26 17:00:11 +0100

    Remove useless code

--- app/VLC_WinRT.UI.Legacy/Views/MainPages/HomePage.xaml.cs
+++ app/VLC_WinRT.UI.Legacy/Views/MainPages/HomePage.xaml.cs
@@ -21,14 +21,7 @@ namespace VLC_WinRT.UI.Legacy.Views.MainPages
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            Locator.MainVM.PropertyChanged += MainVM_PropertyChanged;
-            Navigate(Locator.MainVM.CurrentPanel.Target);
             this.SizeChanged += HomePage_SizeChanged;
-#if WINDOWS_PHONE_APP
-            VisualStateUtilities.GoToState(this, nameof(Phone), false);
-#elif WINDOWS_APP
-            VisualStateUtilities.GoToState(this, nameof(Standard), false);
-#endif
         }
 
         private void HomePage_SizeChanged(object sender, Windows.UI.Xaml.SizeChangedEventArgs e)
@@ -36,37 +29,6 @@ namespace VLC_WinRT.UI.Legacy.Views.MainPages
             Responsive();
         }
 
-        private void MainVM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(MainVM.CurrentPanel))
-            {
-                Navigate(Locator.MainVM.CurrentPanel.Target);
-            }
-        }
-
-        public void Navigate(VLCPage page)
-        {
-            CurrentHomePage = page;
-            switch (page)
-            {
-                case VLCPage.MainPageVideo:
-                    if (HomePageController.HomePageContentPresenter.Content is MainPageVideos) return;
-                    HomePageController.HomePageContentPresenter.Content = new MainPageVideos();
-                    break;
-                case VLCPage.MainPageMusic:
-                    if (HomePageController.HomePageContentPresenter.Content is MainPageMusic) return;
-                    HomePageController.HomePageContentPresenter.Content = new MainPageMusic();
-                    break;
-                case VLCPage.MainPageFileExplorer:
-                    if (HomePageController.HomePageContentPresenter.Content is MainPageFileExplorer) return;
-                    HomePageController.HomePageContentPresenter.Content = new MainPageFileExplorer();
-                    break;
-                case VLCPage.MainPageNetwork:
-                    if (HomePageController.HomePageContentPresenter.Content is MainPageNetwork) return;
-                    HomePageController.HomePageContentPresenter.Content = new MainPageNetwork();
-                    break;
-            }
-        }
         
         void Responsive()
         {
