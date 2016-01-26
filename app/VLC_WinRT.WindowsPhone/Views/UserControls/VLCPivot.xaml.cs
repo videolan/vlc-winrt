104267c485d876f8be7aca275ee742aca9c5f86b
Thomas Nigro <tn@thomasnigro.fr>
2016-01-26 17:03:05 +0100

    Use a custom cross-platform Pivot control

---
+++ app/VLC_WinRT.WindowsPhone/Views/UserControls/VLCPivot.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace VLC_WinRT.Views.UserControls
+{
+    public sealed partial class VLCPivot : UserControl
+    {
+        public VLCPivot()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
