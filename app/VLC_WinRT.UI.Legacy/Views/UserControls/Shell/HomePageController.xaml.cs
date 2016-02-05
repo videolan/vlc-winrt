fd25cee705032e41026b73b3f4bf785a7bcfd32d
Thomas Nigro <tn@thomasnigro.fr>
2016-02-05 18:37:08 +0100

    Fix weird Access Violation in Windows.View.UI.XAML on WP8.1
    It happened when setting the Content of a Frame to null
    It works on Windows 8.1, W10M and W10M.

--- app/VLC_WinRT.UI.Legacy/Views/UserControls/Shell/HomePageController.xaml.cs
+++ app/VLC_WinRT.UI.Legacy/Views/UserControls/Shell/HomePageController.xaml.cs
@@ -1,6 +1,8 @@
-﻿using VLC_WinRT.Model;
+﻿using System.Threading.Tasks;
+using VLC_WinRT.Model;
 using VLC_WinRT.UI.Legacy.Views.MainPages;
 using VLC_WinRT.UI.Legacy.Views.VariousPages;
+using VLC_WinRT.Utils;
 using VLC_WinRT.ViewModels;
 using VLC_WinRT.Views.MainPages;
 using Windows.UI.Xaml.Controls;
@@ -28,11 +30,11 @@ namespace VLC_WinRT.Views.UserControls
             }
         }
 
-        private void HomePageController_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private async void HomePageController_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             this.Unloaded += HomePageController_Unloaded;
             Locator.MainVM.PropertyChanged += MainVM_PropertyChanged;
-            Navigate(Locator.MainVM.CurrentPanel.Target);
+            await Navigate(Locator.MainVM.CurrentPanel.Target);
         }
 
         private void HomePageController_Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
@@ -40,49 +42,52 @@ namespace VLC_WinRT.Views.UserControls
             Locator.MainVM.PropertyChanged -= MainVM_PropertyChanged;
         }
 
-        private void MainVM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        private async void MainVM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             if (e.PropertyName == nameof(MainVM.CurrentPanel))
             {
-                Navigate(Locator.MainVM.CurrentPanel.Target);
+                await Navigate(Locator.MainVM.CurrentPanel.Target);
             }
         }
 
-        public void Navigate(VLCPage page)
+        public Task Navigate(VLCPage page)
         {
-            if (IsPivotItem)
+            return DispatchHelper.InvokeAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
-                var panel = (Model.Panel)this.DataContext;
-                if (panel.Target != page)
+                if (IsPivotItem)
                 {
-                    HomePageContentPresenter.Content = null;
-                    return;
+                    var panel = (Model.Panel)this.DataContext;
+                    if (panel.Target != page)
+                    {
+                        HomePageContentPresenter.Content = new Grid();
+                        return;
+                    }
                 }
-            }
-            CurrentHomePage = page;
-            switch (page)
-            {
-                case VLCPage.MainPageVideo:
-                    if (HomePageContentPresenter.Content is MainPageVideos) return;
-                    HomePageContentPresenter.Navigate(typeof(MainPageVideos));
-                    break;
-                case VLCPage.MainPageMusic:
-                    if (HomePageContentPresenter.Content is MainPageMusic) return;
-                    HomePageContentPresenter.Navigate(typeof(MainPageMusic));
-                    break;
-                case VLCPage.MainPageFileExplorer:
-                    if (HomePageContentPresenter.Content is MainPageFileExplorer) return;
-                    HomePageContentPresenter.Navigate(typeof(MainPageFileExplorer));
-                    break;
-                case VLCPage.MainPageNetwork:
-                    if (HomePageContentPresenter.Content is MainPageNetwork) return;
-                    HomePageContentPresenter.Navigate(typeof(MainPageNetwork));
-                    break;
-                case VLCPage.SearchPage:
-                    if (HomePageContentPresenter.Content is SearchPage) return;
-                    HomePageContentPresenter.Navigate(typeof(SearchPage));
-                    break;
-            }
+                CurrentHomePage = page;
+                switch (page)
+                {
+                    case VLCPage.MainPageVideo:
+                        if (HomePageContentPresenter.Content is MainPageVideos) return;
+                        HomePageContentPresenter.Navigate(typeof(MainPageVideos));
+                        break;
+                    case VLCPage.MainPageMusic:
+                        if (HomePageContentPresenter.Content is MainPageMusic) return;
+                        HomePageContentPresenter.Navigate(typeof(MainPageMusic));
+                        break;
+                    case VLCPage.MainPageFileExplorer:
+                        if (HomePageContentPresenter.Content is MainPageFileExplorer) return;
+                        HomePageContentPresenter.Navigate(typeof(MainPageFileExplorer));
+                        break;
+                    case VLCPage.MainPageNetwork:
+                        if (HomePageContentPresenter.Content is MainPageNetwork) return;
+                        HomePageContentPresenter.Navigate(typeof(MainPageNetwork));
+                        break;
+                    case VLCPage.SearchPage:
+                        if (HomePageContentPresenter.Content is SearchPage) return;
+                        HomePageContentPresenter.Navigate(typeof(SearchPage));
+                        break;
+                }
+            });
         }
     }
 }
