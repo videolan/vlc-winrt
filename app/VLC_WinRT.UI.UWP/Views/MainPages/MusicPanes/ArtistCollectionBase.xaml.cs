de095bb6e4e78e54415076f3df109cbe94ffbcdb
Thomas Nigro <tn@thomasnigro.fr>
2016-02-15 17:51:33 +0100

    Add platform-specific ArtistCollectionpage control

---
+++ app/VLC_WinRT.UI.UWP/Views/MainPages/MusicPanes/ArtistCollectionBase.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+using VLC_WinRT.Model.Video;
+using VLC_WinRT.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+
+namespace VLC_WinRT.Views.MainPages.MusicPanes
+{
+    public sealed partial class ArtistCollectionBase : Page
+    {
+        public ArtistCollectionBase()
+        {
+            this.InitializeComponent();
+            this.Loaded += ArtistCollectionBase_Loaded;
+            this.Unloaded += ArtistCollectionBase_Unloaded;
+        }
+
+        private void ArtistCollectionBase_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Locator.MusicLibraryVM.OnNavigatedFromArtists();
+        }
+
+        void ArtistCollectionBase_Loaded(object sender, RoutedEventArgs e)
+        {
+            Locator.MusicLibraryVM.OnNavigatedToArtists();
+        }
+
+        private void SemanticZoom_OnViewChangeCompleted(object sender, SemanticZoomViewChangedEventArgs e)
+        {
+            ArtistsZoomedOutView.ItemsSource = GroupArtists.View.CollectionGroups;
+        }
+    }
+}
---
+++ app/VLC_WinRT.UI.Win8/Views/MainPages/MusicPanes/ArtistCollectionBase.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+using VLC_WinRT.ViewModels;
+using VLC_WinRT.Utils;
+using Windows.UI.Xaml.Data;
+using ScrollWatcher;
+
+namespace VLC_WinRT.Views.MainPages.MusicPanes
+{
+    public sealed partial class ArtistCollectionBase : UserControl
+    {
+        public ArtistCollectionBase()
+        {
+            this.InitializeComponent();
+            this.Loaded += ArtistCollectionBase_Loaded;
+            this.Unloaded += ArtistCollectionBase_Unloaded;
+        }
+
+        private void ArtistCollectionBase_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (Locator.NavigationService.CurrentPage != Model.VLCPage.ArtistPage)
+            {
+                Locator.MusicLibraryVM.OnNavigatedFromArtists();
+            }
+        }
+
+        void ArtistCollectionBase_Loaded(object sender, RoutedEventArgs e)
+        {
+            Locator.MusicLibraryVM.OnNavigatedToArtists();
+        }
+
+        private void SemanticZoom_OnViewChangeCompleted(object sender, SemanticZoomViewChangedEventArgs e)
+        {
+            ArtistsZoomedOutView.ItemsSource = GroupArtists.View.CollectionGroups;
+        }
+    }
+}
