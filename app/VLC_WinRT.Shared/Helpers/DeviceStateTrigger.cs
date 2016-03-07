262a21366cb1b4aa6c3fcb0c82f42a94c6d030b7
Thomas Nigro <tn@thomasnigro.fr>
2016-03-07 14:47:25 +0100

    Add DeviceTypeHelper and DeviceStateTrigger

---
+++ app/VLC_WinRT.Shared/Helpers/DeviceStateTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+
+namespace VLC_WinRT.Helpers
+{
+    public class DeviceStateTrigger : StateTriggerBase
+    {
+        private DeviceTypeEnum _deviceType;
+        private bool _allExcept;
+
+        public bool AllExcept
+        {
+            get { return _allExcept; }
+            set
+            {
+                _allExcept = value;
+                if (_allExcept)
+                {
+                    var isCurrent = IsCurrentPlatform();
+                    SetActive(isCurrent);
+                }
+            }
+        }
+        public DeviceTypeEnum DeviceType
+        {
+            get
+            {
+                return _deviceType;
+            }
+            set
+            {
+                _deviceType = value;
+                SetActive(IsCurrentPlatform());
+            }
+        }
+
+        public bool IsCurrentPlatform()
+        {
+            return DeviceType == DeviceTypeHelper.GetDeviceType();
+        }
+    }
+}
