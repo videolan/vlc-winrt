ed473849806339f15d3cfcde0137e8ae078118a6
Thomas Nigro <tn@thomasnigro.fr>
2016-03-09 17:55:33 +0100

    Update ArtistCollectionBase responsive desig

--- app/VLC_WinRT.UI.UWP/Views/MainPages/MusicPanes/ArtistCollectionBase.xaml.cs
+++ app/VLC_WinRT.UI.UWP/Views/MainPages/MusicPanes/ArtistCollectionBase.xaml.cs
@@ -17,6 +17,7 @@ namespace VLC_WinRT.Views.MainPages.MusicPanes
             this.InitializeComponent();
             this.Loaded += ArtistCollectionBase_Loaded;
             this.Unloaded += ArtistCollectionBase_Unloaded;
+            this.SizeChanged += ArtistCollectionBase_SizeChanged;
         }
 
         private void ArtistCollectionBase_Unloaded(object sender, RoutedEventArgs e)
@@ -29,9 +30,38 @@ namespace VLC_WinRT.Views.MainPages.MusicPanes
             Locator.MusicLibraryVM.OnNavigatedToArtists();
         }
 
+        private void ArtistCollectionBase_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ResponsiveArtistsList();
+        }
+
+        void ResponsiveArtistsList()
+        {
+            var minItemWidth = 300;
+            var itemsWrapGrid = ArtistListView?.ItemsPanelRoot as ItemsWrapGrid;
+            if (itemsWrapGrid == null) return;
+            if (ArtistsGridColumnDefinition.ActualWidth * 2 > minItemWidth)
+            {
+                itemsWrapGrid.ItemWidth = minItemWidth;
+            }
+            else if (ArtistsGridColumnDefinition.ActualWidth * 1.75 > minItemWidth)
+            {
+                itemsWrapGrid.ItemWidth = ArtistListView.ActualWidth / 2;
+            }
+            else
+            {
+                itemsWrapGrid.ItemWidth = ArtistListView.ActualWidth;
+            }
+        }
+
         private void SemanticZoom_OnViewChangeCompleted(object sender, SemanticZoomViewChangedEventArgs e)
         {
             ArtistsZoomedOutView.ItemsSource = GroupArtists.View.CollectionGroups;
+        }
+
+        private void ItemsWrapGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            ResponsiveArtistsList();
         }
     }
 }
