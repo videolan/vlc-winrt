2fb20ddf9530785dc9d966d9662910c5ac478a4f
Thomas Nigro <tn@thomasnigro.fr>
2016-03-09 17:51:18 +0100

    Add new StateTrigger

---
+++ app/VLC_WinRT.Shared/Helpers/StateTriggers/AdaptativeAndValuesStateTrigger.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using WinRTXamlToolkit.Common;
+
+namespace VLC_WinRT.Helpers.StateTriggers
+{
+    public class AdaptativeAndValuesStateTrigger : StateTriggerBase
+    {
+        #region state value
+        public bool StateValue
+        {
+            get { return (bool)GetValue(StateValueProperty); }
+            set { SetValue(StateValueProperty, value); }
+        }
+        
+        public static readonly DependencyProperty StateValueProperty = DependencyProperty.Register(nameof(StateValue), typeof(bool), typeof(AdaptativeAndValuesStateTrigger), new PropertyMetadata(null, StateValuePropertyChangedCallback));
+
+        private static void StateValuePropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs args)
+        {
+            var that = d as AdaptativeAndValuesStateTrigger;
+            that.ComputeIsActive();
+        }
+        #endregion
+        #region ctor
+        public AdaptativeAndValuesStateTrigger()
+        {
+            //this.RegisterPropertyChangedCallback(MinWindowWidthProperty, OnMinWindowWidthPropertyChanged);
+
+            var window = CoreApplication.GetCurrentView()?.CoreWindow;
+            if (window != null)
+            {
+                var weakEvent = new WeakEventListener<AdaptativeAndValuesStateTrigger, CoreWindow, WindowSizeChangedEventArgs>(this)
+                {
+                    OnEventAction = (instance, s, e) => OnCoreWindowOnSizeChanged(s, e),
+                    OnDetachAction = (instance) => window.SizeChanged -= instance.OnEvent
+                };
+                window.SizeChanged += weakEvent.OnEvent;
+            }
+        }
+        #endregion
+        #region window value
+
+
+        public int MinWindowWidth
+        {
+            get { return (int)GetValue(MinWindowWidthProperty); }
+            set { SetValue(MinWindowWidthProperty, value); }
+        }
+        
+        public static readonly DependencyProperty MinWindowWidthProperty = DependencyProperty.Register(nameof(MinWindowWidth), typeof(int), typeof(AdaptativeAndValuesStateTrigger), new PropertyMetadata(0));
+
+
+        private void OnCoreWindowOnSizeChanged(CoreWindow sender, WindowSizeChangedEventArgs args)
+        {
+            ComputeIsActive();
+        }
+
+        private void OnMinWindowHeightPropertyChanged(DependencyObject sender, DependencyProperty dp)
+        {
+            var window = CoreApplication.GetCurrentView()?.CoreWindow;
+            if (window != null)
+            {
+                ComputeIsActive();
+            }
+        }
+
+        private void OnMinWindowWidthPropertyChanged(DependencyObject sender, DependencyProperty dp)
+        {
+            var window = CoreApplication.GetCurrentView()?.CoreWindow;
+            if (window != null)
+            {
+                ComputeIsActive();
+            }
+        }
+        #endregion
+
+        void ComputeIsActive()
+        {
+            var meetsWindowSizeThreshold = Window.Current.Bounds.Width >= MinWindowWidth;
+            IsActive = meetsWindowSizeThreshold && StateValue;
+        }
+
+        #region ITriggerValue
+
+        private bool _isActive;
+
+        /// <summary>
+        /// Gets a value indicating whether this trigger is active.
+        /// </summary>
+        /// <value><c>true</c> if this trigger is active; otherwise, <c>false</c>.</value>
+        public bool IsActive
+        {
+            get { return _isActive; }
+            private set
+            {
+                if (_isActive != value)
+                {
+                    _isActive = value;
+                    this.SetActive(value);
+                    IsActiveChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Occurs when the <see cref="IsActive" /> property has changed.
+        /// </summary>
+        public event EventHandler IsActiveChanged;
+
+        #endregion ITriggerValue
+    }
+}
---
+++ app/VLC_WinRT.Shared/Helpers/StateTriggers/DeviceStateTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+
+namespace VLC_WinRT.Helpers
+{
+    public class DeviceStateTrigger : StateTriggerBase
+    {
+        private DeviceTypeEnum _deviceType;
+        private bool _allExcept;
+
+        public bool AllExcept
+        {
+            get { return _allExcept; }
+            set
+            {
+                _allExcept = value;
+                ComputeActive();
+            }
+        }
+        public DeviceTypeEnum DeviceType
+        {
+            get
+            {
+                return _deviceType;
+            }
+            set
+            {
+                _deviceType = value;
+                ComputeActive();
+            }
+        }
+
+        public void ComputeActive()
+        {
+            var isCurrent = IsCurrentPlatform();
+            var active = (AllExcept) ? !isCurrent : isCurrent;
+            SetActive(active);
+        }
+
+        public bool IsCurrentPlatform()
+        {
+            return DeviceType == DeviceTypeHelper.GetDeviceType();
+        }
+    }
+}
