101e95e086e0250ececfa6d5c0bc725a054bcfd6
Thomas Nigro <tn@thomasnigro.fr>
2016-03-16 16:26:05 +0100

    Code cosmetic

--- app/VLC_WinRT.Shared/App.xaml.cs
+++ app/VLC_WinRT.Shared/App.xaml.cs
@@ -46,11 +46,7 @@ namespace VLC_WinRT
         public static Model.Music.AlbumItem SelectedAlbumItem;
         public static IContainer Container;
         public static BackgroundAudioHelper BackgroundAudioHelper = new BackgroundAudioHelper();
-
-        /// <summary>
-        ///     Initializes the singleton application object.  This is the first line of authored code
-        ///     executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
+        
         public App()
         {
             InitializeComponent();
@@ -63,14 +59,7 @@ namespace VLC_WinRT
         public static MainPage RootPage => Window.Current?.Content as MainPage;
 
         public static SplitShell SplitShell => RootPage.SplitShell;
-
-
-        /// <summary>
-        ///     Invoked when the application is launched normally by the end user.  Other entry points
-        ///     will be used when the application is launched to open a specific file, to display
-        ///     search results, and so forth.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
+        
         protected override async void OnLaunched(LaunchActivatedEventArgs args)
         {
             if (Window.Current.Content == null)
@@ -160,14 +149,7 @@ namespace VLC_WinRT
             }
 #endif
         }
-
-        /// <summary>
-        ///     Invoked when application execution is being suspended.  Application state is saved
-        ///     without knowing whether the application will be terminated or resumed with the contents
-        ///     of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
+        
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             SuspendingDeferral deferral = e.SuspendingOperation.GetDeferral();
