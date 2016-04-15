9d7c6a9353de20522cf79ffb63b0aa5c35325722
Thomas Nigro <thomas@videolabs.io>
2016-04-15 12:35:42 +0000

    Add MediaLibraryHelper

---
+++ app/VLC_WinRT.Shared/Helpers/MediaLibraryHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VLC_WinRT.Model;
+using Windows.Storage;
+using Windows.Storage.Search;
+
+namespace VLC_WinRT.Helpers
+{
+    public static class MediaLibraryHelper
+    {
+        public static async Task<IReadOnlyList<StorageFile>> GetSupportedFiles(StorageFolder folder)
+        {
+            IReadOnlyList<StorageFile> files = null;
+            try
+            {
+                var queryOptions = new QueryOptions { FolderDepth = FolderDepth.Deep };
+                foreach (var type in VLCFileExtensions.Supported)
+                    queryOptions.FileTypeFilter.Add(type);
+                var fileQueryResult = folder.CreateFileQueryWithOptions(queryOptions);
+                files = await fileQueryResult.GetFilesAsync();
+            }
+            catch(Exception e)
+            {
+            }
+            return files;
+        }
+
+
+    }
+}
