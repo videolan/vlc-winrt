4a87e5ea54e09eed54ae0a8a1ca33e7654bacf8b
Thomas Nigro <thomas@videolabs.io>
2016-04-19 15:55:47 +0000

    Add Init Pip command

---
+++ app/VLC_WinRT.Shared/Commands/VideoPlayer/InitPiPCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VLC_WinRT.Utils;
+using VLC_WinRT.ViewModels;
+
+namespace VLC_WinRT.Commands.VideoPlayer
+{
+    public class InitPiPCommand : AlwaysExecutableCommand
+    {
+        public override void Execute(object parameter)
+        {
+            Locator.NavigationService.Go(Locator.SettingsVM.HomePage);
+            App.RootPage.StartCompositionAnimationOnSwapChain(true);
+        }
+    }
+}
