5a718d8d4e51d4e99f7a437d532ff89143719df6
Thomas Nigro <tn@thomasnigro.fr>
2016-05-06 13:58:22 +0000

    Add AttemptCommand class

---
+++ app/VLC_WinRT.Shared/Utils/AttemptCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VLC_WinRT.Utils
+{
+    public abstract class AttemptCommand
+    {
+        public abstract Task<bool> Execute(object parameter);
+    }
+}
