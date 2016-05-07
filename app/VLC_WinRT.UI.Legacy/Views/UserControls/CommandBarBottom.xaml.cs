ff3747ef403824d28cc13e74bb8acff5b9cb0e26
Thomas Nigro <tn@thomasnigro.fr>
2016-05-07 17:17:23 +0000

    Update BottomCommandBar layout

--- app/VLC_WinRT.UI.Legacy/Views/UserControls/CommandBarBottom.xaml.cs
+++ app/VLC_WinRT.UI.Legacy/Views/UserControls/CommandBarBottom.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using VLC_WinRT.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -15,6 +16,53 @@ namespace VLC_WinRT.UI.Legacy.Views.UserControls
         public CommandBarBottom()
         {
             this.InitializeComponent();
+            this.Loaded += CommandBarBottom_Loaded;
+        }
+
+        private void CommandBarBottom_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdatePlayerVisibility();
+            Locator.MusicPlayerVM.PropertyChanged += MusicPlayerVM_PropertyChanged;
+        }
+
+        private void MusicPlayerVM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Locator.MusicPlayerVM.IsMiniPlayerVisible))
+            {
+                this.MiniPlayerVisibility = Locator.MusicPlayerVM.IsMiniPlayerVisible;
+            }
+        }
+
+        public Visibility MiniPlayerVisibility
+        {
+            get { return (Visibility)GetValue(MiniPlayerVisibilityProperty); }
+            set { SetValue(MiniPlayerVisibilityProperty, value); }
+        }
+
+        public static readonly DependencyProperty MiniPlayerVisibilityProperty =
+            DependencyProperty.Register(nameof(MiniPlayerVisibility), typeof(Visibility), typeof(CommandBarBottom), new PropertyMetadata(Visibility.Collapsed, PlayerVisibilityChanged));
+
+        private static void PlayerVisibilityChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var that = (CommandBarBottom)obj;
+            that.UpdatePlayerVisibility();
+        }
+
+        public void UpdatePlayerVisibility()
+        {
+            PlayPreviousButton.Visibility =
+                PlayButton.Visibility =
+                PauseButton.Visibility =
+                PlayNextButton.Visibility =
+                ShuffleButton.Visibility =
+                RepeatButton.Visibility =
+                ShuffleMiniButton.Visibility =
+                MiniWindowButton.Visibility =
+                MiniPlayerVisibility;
+
+            this.ClosedDisplayMode = 
+                MiniPlayerVisibility == Visibility.Visible ? 
+                    AppBarClosedDisplayMode.Compact : AppBarClosedDisplayMode.Minimal;
         }
     }
 }
