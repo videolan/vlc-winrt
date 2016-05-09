aa825d30c5246694c7b83f3e046705d91d91955a
Thomas Nigro <thomas@videolabs.io>
2016-05-09 13:19:08 +0000

    Add parameters in DialogHelper

--- app/VLC_WinRT.Shared/Helpers/UIHelpers/DialogHelper.cs
+++ app/VLC_WinRT.Shared/Helpers/UIHelpers/DialogHelper.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using libVLCX;
 
 namespace VLC_WinRT.Helpers.UIHelpers
 {
     public static class DialogHelper
     {
-        public static void DisplayDialog(string title, string desc, string username = null, bool remember = false)
+        public static void DisplayDialog(string title, string desc)
         {
 
         }
 
-        public static void DisplayDialog(string title, string desc, uint questionType, string cancel, string action1, string action2)
+        public static void DisplayDialog(string title, string desc, Dialog dialog, string username = null, bool remember = false)
+        {
+
+        }
+
+        public static void DisplayDialog(string title, string desc, Dialog dialog, uint questionType, string cancel, string action1, string action2)
         {
 
         }
