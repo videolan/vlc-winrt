3a85d79ccbb68ff3efe0f4f682cdfb316fb70d43
Thomas Nigro <thomas@videolabs.io>
2016-05-09 12:55:17 +0000

    Add DialogHelper

---
+++ app/VLC_WinRT.Shared/Helpers/UIHelpers/DialogHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VLC_WinRT.Helpers.UIHelpers
+{
+    public static class DialogHelper
+    {
+        public static void DisplayDialog(string title, string desc, string username = null, string password = null, bool remember = false)
+        {
+
+        }
+
+        public static void DisplayDialog(string title, string desc, uint questionType, string cancel, string action1, string action2)
+        {
+
+        }
+    }
+}
---
+++ app/VLC_WinRT.UI.UWP/Views/UserControls/Shell/VLCDialog.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using VLC_WinRT.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+namespace VLC_WinRT.UI.UWP.Views.UserControls.Shell
+{
+    public sealed partial class VLCDialog : ContentDialog
+    {
+        public VLCDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PasswordBox1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Locator.MainVM.KeyboardListenerService.CanListen = false;
+        }
+
+        private void PasswordBox1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            Locator.MainVM.KeyboardListenerService.CanListen = true;
+        }
+    }
+}
