e75cc41fc4703ddaccb4c49c25e691982a723442
Jean-Baptiste Kempf <jb@videolan.org>
2016-06-23 18:52:33 +0200

    RemoveFolderFromLibraries: remove ifdef
    This is not valid for UWP. It probably breaks the WP8.1, we will fix
    later.

--- app/VLC_WinRT.Shared/Commands/Settings/RemoveFolderFromVideoLibrary.cs
+++ app/VLC_WinRT.Shared/Commands/Settings/RemoveFolderFromVideoLibrary.cs
@@ -9,11 +9,9 @@ namespace VLC_WinRT.Commands.Settings
     {
         public override async void Execute(object parameter)
         {
-#if WINDOWS_APP
             var lib = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Videos);
             lib.RequestRemoveFolderAsync(parameter as StorageFolder);
             await Locator.SettingsVM.GetVideoLibraryFolders();
-#endif
         }
     }
 }
