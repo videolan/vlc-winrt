3dc6669077190d568164dec12c59c32685a46b90
Thomas Nigro <tn@thomasnigro.fr>
2016-07-12 22:47:42 +0200

    Update MainPageXBOX : MediaCenter mode

--- app/VLC_WinRT.UI.Legacy/Views/MainPages/MainPageXBOX.xaml.cs
+++ app/VLC_WinRT.UI.Legacy/Views/MainPages/MainPageXBOX.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using VLC_WinRT.Helpers;
 using VLC_WinRT.ViewModels;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
 
 namespace VLC_WinRT.UI.Legacy.Views.MainPages
 {
@@ -14,10 +17,29 @@ namespace VLC_WinRT.UI.Legacy.Views.MainPages
             PanelsListView.Focus(Windows.UI.Xaml.FocusState.Keyboard);
         }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            App.SplitShell.FlyoutAsHeader = true;
+            base.OnNavigatedTo(e);
+            PanelsListView.ItemsSource = Locator.MainVM.Panels;
+            PanelsListView.SelectedItem = Locator.MainVM.CurrentPanel;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            App.SplitShell.FlyoutAsHeader = false;
+            base.OnNavigatingFrom(e);
+        }
+        
         private void ListView_ItemClick(object sender, ItemClickEventArgs e)
         {
             var model = e.ClickedItem as Model.Panel;
             Locator.NavigationService.Go(model.Target);
+        }
+
+        private void ItemsWrapGrid_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ((PanelsListView.ItemsPanelRoot as ItemsWrapGrid).Children[PanelsListView.SelectedIndex] as ListViewItem).Focus(Windows.UI.Xaml.FocusState.Programmatic);
         }
     }
 }
