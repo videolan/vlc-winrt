598f53117dda025d656d47d72d4a26246b295634
Adrien Maglo <magsoft@videolan.org>
2016-08-26 20:48:21 +0200

    FolderDataTemplate: export it in an indenpendant file

---
+++ app/VLC.UI/Views/UserControls/FolderDataTemplate.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using VLC.Views.UserControls.Flyouts;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace VLC.Views.UserControls
+{
+    public sealed partial class FolderDataTemplate : UserControl
+    {
+        public FolderDataTemplate()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
