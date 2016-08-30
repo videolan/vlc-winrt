801187f5a729e349a25a10b64066210a65da5828
Thomas Nigro <tn@thomasnigro.fr>
2016-08-30 15:01:18 +0200

    Add GamepadService

---
+++ app/VLC.Core/Services/RunTime/GamepadService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VLC.ViewModels;
+using Windows.Gaming.Input;
+
+namespace VLC.Services.RunTime
+{
+    public class GamepadService : IDisposable
+    {
+        public event EventHandler<Gamepad> GamepadUpdated;
+        public GamepadService()
+        {
+            Gamepad.GamepadAdded += Gamepad_GamepadUpdated;
+            Gamepad.GamepadRemoved += Gamepad_GamepadUpdated;
+        }
+        
+        private void Gamepad_GamepadUpdated(object sender, Gamepad e)
+        {
+            GamepadUpdated?.Invoke(this, e);
+        }
+        
+        public void Dispose()
+        {
+            Gamepad.GamepadAdded -= Gamepad_GamepadUpdated;
+            Gamepad.GamepadRemoved -= Gamepad_GamepadUpdated;
+        }
+    }
+}
