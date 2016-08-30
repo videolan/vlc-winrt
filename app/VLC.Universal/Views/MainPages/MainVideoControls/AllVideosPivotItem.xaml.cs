9ce8deb543445276034aebc89b37578b46ee97e5
Adrien Maglo <magsoft@videolan.org>
2016-08-30 13:13:05 +0200

    AllVideosPivotItem: enable the VideoInformationFlyout on Xbox

--- app/VLC.Universal/Views/MainPages/MainVideoControls/AllVideosPivotItem.xaml.cs
+++ app/VLC.Universal/Views/MainPages/MainVideoControls/AllVideosPivotItem.xaml.cs
@@ -3,11 +3,17 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using VLC.Model.Video;
 using VLC.ViewModels;
+using Windows.UI.Xaml.Input;
+using Windows.System;
+using VLC.Views.UserControls.Flyouts;
+using VLC.Model;
 
 namespace VLC.Views.MainPages.MainVideoControls
 {
     public sealed partial class AllVideosPivotItem : Page
     {
+        private ListViewItem focussedListViewItem;
+
         public AllVideosPivotItem()
         {
             this.InitializeComponent();
@@ -28,6 +34,26 @@ namespace VLC.Views.MainPages.MainVideoControls
         private void VideosWrapGrid_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             TemplateSizer.ComputeCompactVideo(sender as ItemsWrapGrid, this.ActualWidth);
+        }
+
+        private void AllVideosListView_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            ListView list = (ListView)sender;
+            if (Locator.NavigationService.CurrentPage == VLCPage.MainPageVideo
+                && e.Key == VirtualKey.GamepadView)
+            {
+                var menu = new VideoInformationFlyout(AllVideosListView.ItemFromContainer(focussedListViewItem));
+                menu.ShowAt(focussedListViewItem);
+            }
+        }
+
+        private void AllVideosListView_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ListView list = (ListView)sender;
+            if (FocusManager.GetFocusedElement() as ListViewItem != null)
+            {
+                focussedListViewItem = (ListViewItem)FocusManager.GetFocusedElement();
+            }
         }
     }
 }
