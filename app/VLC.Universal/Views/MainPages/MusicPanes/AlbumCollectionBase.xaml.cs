acf8f4331288ebf8112412724ed49e6ae6806f48
Adrien Maglo <magsoft@videolan.org>
2016-08-30 14:19:14 +0200

    AlbumCollectionBase: enable the AlbumMenuFlyout on Xbox

--- app/VLC.Universal/Views/MainPages/MusicPanes/AlbumCollectionBase.xaml.cs
+++ app/VLC.Universal/Views/MainPages/MusicPanes/AlbumCollectionBase.xaml.cs
@@ -2,11 +2,17 @@
 using Windows.UI.Xaml.Controls;
 using VLC.Model.Video;
 using VLC.ViewModels;
+using Windows.UI.Xaml.Input;
+using VLC.Model;
+using Windows.System;
+using VLC.Views.UserControls.Flyouts;
 
 namespace VLC.Views.MainPages.MusicPanes
 {
     public sealed partial class AlbumCollectionBase : UserControl
     {
+        ListViewItem focussedListViewItem;
+
         public AlbumCollectionBase()
         {
             this.InitializeComponent();
@@ -29,6 +35,26 @@ namespace VLC.Views.MainPages.MusicPanes
         private void AlbumCollectionBase_Loaded(object sender, RoutedEventArgs e)
         {
             Locator.MusicLibraryVM.OnNavigatedToAlbums();
+        }
+
+        private void AlbumsZoomedInView_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ListView list = (ListView)sender;
+            if (FocusManager.GetFocusedElement() as ListViewItem != null)
+            {
+                focussedListViewItem = (ListViewItem)FocusManager.GetFocusedElement();
+            }
+        }
+
+        private void AlbumsZoomedInView_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            ListView list = (ListView)sender;
+            if (Locator.NavigationService.CurrentPage == VLCPage.MainPageMusic
+                && e.Key == VirtualKey.GamepadView)
+            {
+                var menu = new AlbumMenuFlyout(AlbumsZoomedInView.ItemFromContainer(focussedListViewItem));
+                menu.ShowAt(focussedListViewItem);
+            }
         }
     }
 }
