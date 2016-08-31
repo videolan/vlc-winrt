031b5f4c0197011bfcc3d1397ee7863559d8b619
Thomas Nigro <tn@thomasnigro.fr>
2016-08-31 17:36:42 +0200

    Merge VLCPivot in HomePage

--- app/VLC.Universal/Views/MainPages/HomePage.xaml.cs
+++ app/VLC.Universal/Views/MainPages/HomePage.xaml.cs
@@ -3,32 +3,82 @@ using VLC.Helpers;
 using VLC.Model;
 using VLC.ViewModels;
 using VLC.Views.MainPages;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Navigation;
 
 namespace VLC.UI.Legacy.Views.MainPages
 {
     public sealed partial class HomePage : Page
     {
-        public VLCPage CurrentHomePage
-        {
-            get; private set;
-        }
         public HomePage()
         {
             this.InitializeComponent();
+            this.SizeChanged += HomePage_SizeChanged;
+        }
+
+        private void HomePage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Responsive();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            AppViewHelper.SetAppView(true);
+            if (Locator.SettingsVM.MediaCenterMode)
+            {
+                (FindName(nameof(MediaCenterController)) as FrameworkElement).Visibility = Visibility.Visible;
+                (FindName(nameof(Pivot)) as FrameworkElement).Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                (FindName(nameof(MediaCenterController)) as FrameworkElement).Visibility = Visibility.Collapsed;
+                (FindName(nameof(Pivot)) as FrameworkElement).Visibility = Visibility.Visible;
+                AppViewHelper.SetAppView(true);
+            }
         }
 
         protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
         {
             base.OnNavigatingFrom(e);
-            AppViewHelper.SetAppView(false);
+            if (!Locator.SettingsVM.MediaCenterMode)
+                AppViewHelper.SetAppView(false);
+        }
+
+        private void TitleBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            AppViewHelper.SetTitleBar(TitleBar);
+            Responsive();
+        }
+
+        private void Responsive()
+        {
+            if (Window.Current.Bounds.Width < 850)
+            {
+                VLCIcon.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                VLCIcon.Visibility = Visibility.Visible;
+            }
+
+
+            if (AppViewHelper.TitleBarRightOffset == 0)
+                return;
+
+            var pivotHeader = WinRTXamlToolkit.Controls.Extensions.VisualTreeHelperExtensions.GetFirstDescendantOfType<PivotHeaderPanel>(Pivot);
+            if (pivotHeader == null)
+                return;
+
+            if (Window.Current.Bounds.Width < 850)
+            {
+                pivotHeader.Margin = new Thickness(0, 16, 0, 0);
+            }
+            else
+            {
+                pivotHeader.Margin = new Thickness();
+            }
         }
     }
 }
