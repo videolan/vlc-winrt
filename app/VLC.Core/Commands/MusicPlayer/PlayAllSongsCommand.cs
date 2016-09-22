968925dd5d41d5472541e01fd4f50afafd95b266
Adrien Maglo <magsoft@videolan.org>
2016-09-22 13:40:51 +0200

    PlayAllSongsCommand: reset the playlist before adding new items
    Fix #66

--- app/VLC.Core/Commands/MusicPlayer/PlayAllSongsCommand.cs
+++ app/VLC.Core/Commands/MusicPlayer/PlayAllSongsCommand.cs
@@ -25,7 +25,7 @@ namespace VLC.Commands.MusicPlayer
             }
             if (index > -1)
             {
-                await Locator.MediaPlaybackViewModel.PlaybackService.SetPlaylist(tracks, false, true, tracks[index]);
+                await Locator.MediaPlaybackViewModel.PlaybackService.SetPlaylist(tracks, true, true, tracks[index]);
             }
         }
     }
