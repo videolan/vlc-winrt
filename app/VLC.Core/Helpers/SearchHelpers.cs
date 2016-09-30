df5730f6db6f63b4f35b52fcd6bb7488eded83f6
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-09-30 15:43:41 +0200

    SearchHelpers: Remove a bunch of unused methods

--- app/VLC.Core/Helpers/SearchHelpers.cs
+++ app/VLC.Core/Helpers/SearchHelpers.cs
@@ -15,38 +15,6 @@ namespace VLC.Helpers
 {
     public static class SearchHelpers
     {        
-        public static async Task<ObservableCollection<SearchResult>> SearchArtists(string tag)
-        {
-            var results = new ObservableCollection<SearchResult>();
-            var artistItems = SearchArtistItems(tag);
-            foreach (var artistItem in artistItems)
-            {
-                results.Add(new SearchResult(artistItem.Name, ApplicationData.Current.LocalFolder.Path + "\\artistPic\\" + artistItem.Id + ".jpg",
-                    VLCItemType.Artist, artistItem.Id));
-            }
-            return results;
-        }
-
-        public static ObservableCollection<SearchResult> SearchTracks(string tag)
-        {
-            var results = new ObservableCollection<SearchResult>();
-            return results;
-        }
-
-        public static async Task<ObservableCollection<SearchResult>> SearchAlbumsGeneric(string tag)
-        {
-            var results = new ObservableCollection<SearchResult>();
-            var albumItems = SearchAlbumItems(tag);
-            foreach (AlbumItem albumItem in albumItems)
-            {
-                results.Add(new SearchResult(albumItem.Name,
-                    ApplicationData.Current.LocalFolder.Path + "\\albumPic\\" + albumItem.Id + ".jpg",
-                    VLCItemType.Album,
-                    albumItem.Id));
-            }
-            return results;
-        }
-
         public static async Task<List<AlbumItem>> SearchAlbums(string tag, List<AlbumItem> results)
         {
             var albums = SearchAlbumItems(tag);
@@ -59,22 +27,6 @@ namespace VLC.Helpers
             {
                 if (!albums.Contains(result))
                     results.Remove(result);
-            }
-            return results;
-        }
-
-        public static async Task<ObservableCollection<SearchResult>> SearchVideosGeneric(string tag, ObservableCollection<SearchResult> results)
-        {
-            var videoVms = SearchVideoItems(tag);
-            foreach (VideoItem vm in videoVms)
-            {
-                results.Add(new SearchResult(vm.Name, ApplicationData.Current.LocalFolder.Path + "\\videoPic\\" + vm.Name + ".jpg", VLCItemType.Video));
-            }
-
-            var showsVms = SearchShowItems(tag);
-            foreach (var showsVm in showsVms)
-            {
-                results.Add(new SearchResult(showsVm.Name, ApplicationData.Current.LocalFolder.Path + "\\videoPic\\" + showsVm.Name + ".jpg", VLCItemType.VideoShow));
             }
             return results;
         }
@@ -96,24 +48,6 @@ namespace VLC.Helpers
             return results;
         }
 
-        public static async Task SearchMusic(string tag, ObservableCollection<SearchResult> results)
-        {
-            var albums = await SearchAlbumsGeneric(tag);
-            foreach (var album in albums.Where(album => !results.Contains(album)))
-            {
-                results.Add(album);
-            }
-            foreach (var result in results.ToList().Where(result => !albums.Contains(result)))
-            {
-                results.Remove(result);
-            }
-        }
-
-        public static List<ArtistItem> SearchArtistItems(string tag)
-        {
-            return Locator.MediaLibrary.LoadArtists(x => x.Name.Contains(tag, StringComparison.CurrentCultureIgnoreCase));
-        }
-
         public static List<AlbumItem> SearchAlbumItems(string tag)
         {
             return Locator.MediaLibrary.Contains(nameof(AlbumItem.Name), tag);
@@ -122,11 +56,6 @@ namespace VLC.Helpers
         public static List<VideoItem> SearchVideoItems(string tag)
         {
             return Locator.MediaLibrary.ContainsVideo(nameof(VideoItem.Name), tag);
-        }
-
-        public static IEnumerable<VideoItem> SearchShowItems(string tag)
-        {
-            return Locator.MediaLibrary.Shows.SelectMany(show => show.Episodes).Where(x => x.Name.Contains(tag, StringComparison.CurrentCultureIgnoreCase));
         }
     }
 }
