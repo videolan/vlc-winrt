ecb71326204b25d354134ab9fdf615d76f29df67
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-10-03 10:47:03 +0200

    Add a PlayingStateToGlyph converter

---
+++ app/VLC.Core/Converters/PlayingStateToGlyphConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace VLC.Converters
+{
+    public class PlayingStateToGlyphConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var isPlaying = (bool)value;
+            if (isPlaying)
+                return Application.Current.Resources["PauseSymbol"];
+            return Application.Current.Resources["PlaySymbol"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
