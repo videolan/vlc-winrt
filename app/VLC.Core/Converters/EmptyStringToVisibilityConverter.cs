90520357df5fd01f70f8aa879c1feef041a92f75
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-10-04 02:21:21 +0200

    Remove hard coded reference to Microsoft.Toolkit.Uwp.UI.Controls
    Use a nuget package instead

---
+++ app/VLC.Core/Converters/EmptyStringToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿/**********************************************************************
+ * VLC for WinRT
+ **********************************************************************
+ * Copyright © 2013-2014 VideoLAN and Authors
+ *
+ * Licensed under GPLv2+ and MPLv2
+ * Refer to COPYING file of the official project for license
+ **********************************************************************/
+
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace VLC.Converters
+{
+    public class EmptyStringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null && value is string && ((string)value).Length > 0)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
