61de8ed2b42efcfd062cd11031cc363286c23dcb
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-11-28 15:35:13 +0100

    Prevent potential NRE when browsing folders

--- app/VLC.Core/Commands/VLCFileExplorer/IStorageItemClickedCommand.cs
+++ app/VLC.Core/Commands/VLCFileExplorer/IStorageItemClickedCommand.cs
@@ -11,7 +11,11 @@ namespace VLC.Commands.VLCFileExplorer
     {
         public override async void Execute(object parameter)
         {
-            IVLCStorageItem storageItem = ((ItemClickEventArgs)parameter).ClickedItem as IVLCStorageItem;
+            var clickedItem = ((ItemClickEventArgs)parameter).ClickedItem;
+            // For some reason, when clicking early while the page is loading, it seems we can click "null items"
+            if (clickedItem == null)
+                return;
+            IVLCStorageItem storageItem = clickedItem as IVLCStorageItem;
             if (Locator.NavigationService.CurrentPage == VLCPage.MainPageFileExplorer)
                 await Locator.FileExplorerVM.CurrentStorageVM.NavigateTo(storageItem);
         }
