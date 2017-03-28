671a3f38d4335e575a3be8ae054d6f7368f407ce
Adrien Maglo <magsoft@videolan.org>
2017-03-28 02:52:57 -0700

    ShowEpisodesView: display the VideoInformationFlyout for the episodes
    Fixes #155.
    Episodes can now be deleted.

--- app/VLC.Universal/Views/VideoPages/TVShowsViews/ShowEpisodesView.xaml.cs
+++ app/VLC.Universal/Views/VideoPages/TVShowsViews/ShowEpisodesView.xaml.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using VLC.Model;
 using VLC.Model.Video;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using VLC.UI.Views.UserControls.Flyouts;
+using VLC.ViewModels;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 namespace VLC.UI.Views.VideoPages.TVShowsViews
 {
     public sealed partial class ShowEpisodesView : Page
     {
+        private ListViewItem focussedListViewItem;
+
         public ShowEpisodesView()
         {
             this.InitializeComponent();
@@ -21,6 +21,26 @@ namespace VLC.UI.Views.VideoPages.TVShowsViews
         private void VideosWrapGrid_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             TemplateSizer.ComputeCompactVideo(sender as ItemsWrapGrid, this.ActualWidth);
+        }
+
+        private void EpisodesListView_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ListView list = (ListView)sender;
+            if (FocusManager.GetFocusedElement() as ListViewItem != null)
+            {
+                focussedListViewItem = (ListViewItem)FocusManager.GetFocusedElement();
+            }
+        }
+
+        private void EpisodesListView_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            ListView list = (ListView)sender;
+            if (Locator.NavigationService.CurrentPage == VLCPage.TvShowView
+                && e.Key == VirtualKey.GamepadView)
+            {
+                var menu = new VideoInformationFlyout(EpisodesListView.ItemFromContainer(focussedListViewItem));
+                menu.ShowAt(focussedListViewItem);
+            }
         }
     }
 }
