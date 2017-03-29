02687bb3f344ec3ea95dc45491ff115d5bec122a
Adrien Maglo <magsoft@videolan.org>
2017-03-29 10:54:23 -0700

    CreateNewPlaylist: fix the double validation of the form when pressing enter on the Xbox virtual keyboard.

--- app/VLC.Universal/Views/MusicPages/CreateNewPlaylist.xaml.cs
+++ app/VLC.Universal/Views/MusicPages/CreateNewPlaylist.xaml.cs
@@ -2,6 +2,7 @@
 using Windows.UI.Xaml.Controls;
 using VLC.Helpers.MusicLibrary;
 using VLC.ViewModels;
+using System.Threading.Tasks;
 
 namespace VLC.UI.Views.MusicPages
 {
@@ -14,14 +15,23 @@ namespace VLC.UI.Views.MusicPages
 
         private async void AddToCollection_Click(object sender, RoutedEventArgs e)
         {
-            await Locator.MediaLibrary.AddNewPlaylist(playlistName.Text);
-            Locator.NavigationService.GoBack_HideFlyout();
+            await addToCollectionAndClose();
         }
 
-        private void playlistName_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        private async void playlistName_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
         {
-            if (e.Key == Windows.System.VirtualKey.Enter)
-                AddToCollection_Click(null, null);
+            if (e.Key == Windows.System.VirtualKey.Enter
+                && e.KeyStatus.RepeatCount == 1)
+            {
+                await addToCollectionAndClose();
+                e.Handled = true;
+            }
+        }
+
+        private async Task addToCollectionAndClose()
+        {
+            await Locator.MediaLibrary.AddNewPlaylist(playlistName.Text);
+            Locator.NavigationService.GoBack_HideFlyout();
         }
     }
 }
