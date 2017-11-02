6b4b388313a3e02a4aadb248da0f08766cd9b0ad
Martin Finkel <finkel2804@gmail.com>
2017-11-02 07:59:09 +0100

    Fix #204

--- app/VLC.Universal/Views/MainPages/MainPageNetwork.xaml.cs
+++ app/VLC.Universal/Views/MainPages/MainPageNetwork.xaml.cs
@@ -2,15 +2,19 @@
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
-using VLC.Services.RunTime;
 using VLC.ViewModels;
 using Windows.UI.Xaml.Media;
+using VLC.Commands.StreamsLibrary;
+using VLC.Utils;
 
 namespace VLC.UI.Views.MainPages
 {
     public sealed partial class MainPageNetwork : Page
     {
+        private ListViewItem _focusedElement;
+
         public MainPageNetwork()
         {
             this.InitializeComponent();
@@ -36,11 +40,36 @@ namespace VLC.UI.Views.MainPages
             {
                 Locator.StreamsVM.PlayStreamCommand.Execute(MrlTextBox.Text);
             }
+            else if (args.VirtualKey == VirtualKey.GamepadView && _focusedElement != null)
+            {
+                var flyout = new MenuFlyout
+                {
+                    Items =
+                    {
+                        new MenuFlyoutItem
+                        {
+                            Text = Strings.DeleteSelected,
+                            Command = new DeleteStreamCommand(),
+                            CommandParameter = _focusedElement.Content
+                        }
+                    }
+                };
+                flyout.ShowAt(_focusedElement);
+            }
         }
 
         private void MrlTextBox_Paste(object sender, TextControlPasteEventArgs e)
         {
             MrlTextBox.Foreground = App.Current.Resources["MainColor"] as SolidColorBrush;
+        }
+
+
+        private void NetworkItemGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (FocusManager.GetFocusedElement() is ListViewItem)
+            {
+                _focusedElement = (ListViewItem)FocusManager.GetFocusedElement();
+            }
         }
     }
 }
